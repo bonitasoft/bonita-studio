75b4c0434e6afd7e3b20c352eb29e99753c34a8f
Aurelien Pupier <aaurelien.pupier@bonitasoft.com>
2013-01-03 14:17:30 +0000

    improve logging

--- plugins/org.bonitasoft.studio.repository.themes/src/org/bonitasoft/studio/repository/themes/LookNFeelRepositoryStore.java
+++ plugins/org.bonitasoft.studio.repository.themes/src/org/bonitasoft/studio/repository/themes/LookNFeelRepositoryStore.java
@@ -315,8 +315,10 @@ public class LookNFeelRepositoryStore extends AbstractRepositoryStore<LookNFeelF
             } catch (MalformedURLException e) {
                 //            	ApplicationLookNFeelFileStore fileStore =  new ApplicationLookNFeelFileStore(fileName, this) ;
                 //            	fileStore.save(null);
+            	BonitaStudioLog.error(e) ;
                 return null;
             }catch (Exception e1) {
+            	BonitaStudioLog.error("Exception when searching for file with name: "+ fileName, "org.bonitasoft.studio.repository.themes");
                 BonitaStudioLog.error(e1) ;
                 return null ;
             }
