ef1633c18c8e2de604c45dbddb6faa7c8bccd1b4
Arelie Zara <aurelie.zara@bonitasoft.com>
2013-01-04 09:52:11 +0000

    bug:STUDIO-395 Configure tycho to use Sonar

--- plugins/org.bonitasoft.studio.engine/src/org/bonitasoft/studio/engine/operation/ApplicationURLBuilder.java
+++ plugins/org.bonitasoft.studio.engine/src/org/bonitasoft/studio/engine/operation/ApplicationURLBuilder.java
@@ -74,8 +74,8 @@ public class ApplicationURLBuilder {
         }
 
         final String loginURL = BOSWebServerManager.getInstance().generateLoginURL(userName, password) ;
-        final String runUrl = "http://"+ host+":"+ port + APPLI_PATH + token +"#?name="+process.getName()+"&version="+process.getVersion()+"&id=" + processId+"&_p=StartProcess&_pf="+USER_PROFILE_ID+"&"+LOCALE_PARAM+"="+locale;
-
+     //   final String runUrl = "http://"+ host+":"+ port + APPLI_PATH + token +"#?name="+process.getName()+"&version="+process.getVersion()+"&id=" + processId+"&_p=StartProcess&_pf="+USER_PROFILE_ID+"&"+LOCALE_PARAM+"="+locale;
+        final String runUrl = "http://"+ host+":"+ port + APPLI_PATH + token +"ui=form#form="+process.getName()+"--"+process.getVersion()+"$entry&process="+processId+"&mode=app";
         return new URL(loginURL+"&redirectUrl="+URLEncoder.encode(runUrl, "UTF-8"));
     }
 
