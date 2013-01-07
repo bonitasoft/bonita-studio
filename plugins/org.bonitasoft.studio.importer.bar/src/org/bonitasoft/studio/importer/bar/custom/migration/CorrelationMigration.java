99bf0580c0da855ea2138cc62c57a224b2ba486a
Arelie Zara <aurelie.zara@bonitasoft.com>
2013-01-07 16:53:59 +0000

    bug:STUDIO-780 Write rule to migrate Attachment data into Document

---
+++ plugins/org.bonitasoft.studio.importer.bar/src/org/bonitasoft/studio/importer/bar/custom/migration/CorrelationMigration.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.bonitasoft.studio.importer.bar.custom.migration;
+
+import org.eclipse.emf.edapt.migration.CustomMigration;
+import org.eclipse.emf.edapt.migration.Instance;
+import org.eclipse.emf.edapt.migration.Metamodel;
+import org.eclipse.emf.edapt.migration.MigrationException;
+import org.eclipse.emf.edapt.migration.Model;
+
+/**
+ * @author aurelie zara
+ *
+ */
+public class CorrelationMigration extends CustomMigration {
+
+	@Override
+	public void migrateBefore(Model model, Metamodel metamodel)throws MigrationException{
+//		for (Instance correlationInstance : model.getAllInstances("process.Correlation")){
+//			final Instance correlationAssociationInstance = correlationInstance.get("correlationAssociation");
+//			final Instance correlationKeyInstance = correlationAssociationInstance.get("correlationKey");
+//			final Instance correlationExpressionInstance = correlationAssociationInstance.get("correlationExpression");
+//			 
+//		}
+	}
+}
