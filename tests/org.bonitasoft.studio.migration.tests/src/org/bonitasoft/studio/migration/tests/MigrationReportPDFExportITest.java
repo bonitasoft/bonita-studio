e0fefdbb33e9079fc92e94183fe360936c203d08
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-01-09 14:56:13 +0000

    Share project "org.bonitasoft.studio.migration.tests" into "https://svn.bonitasoft.org"

---
+++ tests/org.bonitasoft.studio.migration.tests/src/org/bonitasoft/studio/migration/tests/MigrationReportPDFExportITest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2012 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.migration.tests;
+
+import java.net.URL;
+
+import org.bonitasoft.studio.common.jface.FileActionDialog;
+import org.bonitasoft.studio.common.perspectives.BonitaPerspectivesUtils;
+import org.bonitasoft.studio.common.repository.Repository;
+import org.bonitasoft.studio.common.repository.operation.ImportBosArchiveOperation;
+import org.bonitasoft.studio.model.process.Pool;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swtbot.eclipse.gef.finder.SWTBotGefTestCase;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
+import org.eclipse.swtbot.swt.finder.waits.Conditions;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Romain Bioteau
+ *
+ */
+public class MigrationReportPDFExportITest extends SWTBotGefTestCase {
+	
+	@BeforeClass
+	public static void disablePopup(){
+		FileActionDialog.setDisablePopup(true);
+	}
+	
+	@Test
+	public void testAutomaticPerspectiveSwitch() throws Exception{
+		FileActionDialog.setDisablePopup(true);
+		final URL url = MigrationReportPDFExportITest.class.getResource("TestMigrationReport-1.0.bos");
+		ImportBosArchiveOperation op = new ImportBosArchiveOperation();
+		op.setArchiveFile(FileLocator.toFileURL(url).getFile());
+		op.run(Repository.NULL_PROGRESS_MONITOR);
+		
+		assertEquals("Invalid perspective for process with migration report","org.bonitasoft.studio.migration.perspective.process",BonitaPerspectivesUtils.getPerspectiveId(bot.activeEditor().getReference().getEditor(false)));
+	}
+
+	@Test
+	public void testCompleteReport() throws Exception{
+		FileActionDialog.setDisablePopup(true);
+		final URL url = MigrationReportPDFExportITest.class.getResource("TestMigrationReport-1.0.bos");
+		ImportBosArchiveOperation op = new ImportBosArchiveOperation();
+		op.setArchiveFile(FileLocator.toFileURL(url).getFile());
+		op.run(Repository.NULL_PROGRESS_MONITOR);
+		
+		bot.viewById("org.bonitasoft.studio.migration.view").bot().button("Complete").click();
+		bot.waitUntil(Conditions.shellIsActive("Complete import"));
+		bot.checkBox().deselect();
+		bot.button(IDialogConstants.YES_LABEL).click();
+		
+		assertEquals("Invalid perspective for process without migration report","org.bonitasoft.studio.perspective.process",BonitaPerspectivesUtils.getPerspectiveId(bot.activeEditor().getReference().getEditor(false)));
+		EObject mainProcess = (EObject) bot.gefEditor(bot.activeEditor().getTitle()).mainEditPart().part().getModel();
+		assertEquals("Report model should have been deleted",2,mainProcess.eResource().getContents().size());
+	}
+	
+	@Test
+	public void testEditorSelectionSynchronization() throws Exception{
+		FileActionDialog.setDisablePopup(true);
+		final URL url = MigrationReportPDFExportITest.class.getResource("TestMigrationReport-1.0.bos");
+		ImportBosArchiveOperation op = new ImportBosArchiveOperation();
+		op.setArchiveFile(FileLocator.toFileURL(url).getFile());
+		op.run(Repository.NULL_PROGRESS_MONITOR);
+		
+		bot.viewById("org.bonitasoft.studio.migration.view").toolbarToggleButton("Link with Editor").select();
+		bot.table().select(1);
+		SWTBotGefEditPart part = bot.gefEditor(bot.activeEditor().getTitle()).selectedEditParts().get(0);
+		assertTrue(((IGraphicalEditPart)part.part()).resolveSemanticElement() instanceof Pool);
+	}
+	
+	
+	
+}
