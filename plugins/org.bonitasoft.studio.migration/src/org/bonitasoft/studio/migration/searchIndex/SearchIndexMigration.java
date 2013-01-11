18d3424ae9edf0154b4706e87d9901623aa351a9
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-01-11 10:07:01 +0000

    fix migration tests

--- plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/searchIndex/SearchIndexMigration.java
+++ plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/searchIndex/SearchIndexMigration.java
@@ -5,11 +5,7 @@ package org.bonitasoft.studio.migration.searchIndex;
 
 import org.bonitasoft.studio.common.ExpressionConstants;
 import org.bonitasoft.studio.migration.utils.StringToExpressionConverter;
-import org.bonitasoft.studio.model.expression.Expression;
-import org.bonitasoft.studio.model.expression.ExpressionFactory;
-import org.bonitasoft.studio.model.process.ProcessFactory;
 import org.bonitasoft.studio.model.process.ProcessPackage;
-import org.bonitasoft.studio.model.process.SearchIndex;
 import org.eclipse.emf.edapt.migration.CustomMigration;
 import org.eclipse.emf.edapt.migration.Instance;
 import org.eclipse.emf.edapt.migration.Metamodel;
@@ -35,7 +31,7 @@ public class SearchIndexMigration extends CustomMigration {
 	}
 
 	private Instance createSearchIndex(Model model){
-		Instance searchIndex = model.newInstance(ProcessPackage.Literals.SEARCH_INDEX);
+		Instance searchIndex = model.newInstance("process.SearchIndex");
 		Instance nameExpression = StringToExpressionConverter.createExpressionInstance(model, "", "", String.class.getName(), ExpressionConstants.CONSTANT_TYPE, true);
 		searchIndex.set("name", nameExpression);
 		Instance valueExpression = StringToExpressionConverter.createExpressionInstance(model, "", "",String.class.getName(), ExpressionConstants.CONSTANT_TYPE, true);
