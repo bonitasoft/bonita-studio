69df2d054164aca4d344a261de46fc24f3f5dc11
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-01-11 08:32:35 +0000

    migration searchindex

--- plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/searchIndex/SearchIndexMigration.java
+++ plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/searchIndex/SearchIndexMigration.java
@@ -3,13 +3,12 @@
  */
 package org.bonitasoft.studio.migration.searchIndex;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bonitasoft.studio.common.ExpressionConstants;
+import org.bonitasoft.studio.migration.utils.StringToExpressionConverter;
 import org.bonitasoft.studio.model.expression.Expression;
 import org.bonitasoft.studio.model.expression.ExpressionFactory;
 import org.bonitasoft.studio.model.process.ProcessFactory;
+import org.bonitasoft.studio.model.process.ProcessPackage;
 import org.bonitasoft.studio.model.process.SearchIndex;
 import org.eclipse.emf.edapt.migration.CustomMigration;
 import org.eclipse.emf.edapt.migration.Instance;
@@ -22,32 +21,25 @@ import org.eclipse.emf.edapt.migration.Model;
  *
  */
 public class SearchIndexMigration extends CustomMigration {
+	
 	private final int MAX=5;
-
 
 	@Override
 	public void migrateAfter(Model model, Metamodel metamodel)
 			throws MigrationException {
 		for (Instance pool:model.getAllInstances("process.Pool")){
 			for (int i=0;i<MAX;i++){
-				pool.add("searchIndexs", createSearchIndex());
+				pool.add("SearchIndexs", createSearchIndex(model));
 			}
 		}
 	}
 
-	private SearchIndex createSearchIndex(){
-		SearchIndex searchIndex = ProcessFactory.eINSTANCE.createSearchIndex();
-		Expression name = ExpressionFactory.eINSTANCE.createExpression();
-		name.setContent("");
-		name.setType(ExpressionConstants.CONSTANT_TYPE);
-		name.setReturnType(String.class.getName());
-		name.setReturnTypeFixed(true);
-		searchIndex.setName(name);
-		Expression value = ExpressionFactory.eINSTANCE.createExpression();
-		value.setContent("");
-		value.setReturnType(String.class.getName());
-		value.setReturnTypeFixed(true);
-		searchIndex.setValue(value);
+	private Instance createSearchIndex(Model model){
+		Instance searchIndex = model.newInstance(ProcessPackage.Literals.SEARCH_INDEX);
+		Instance nameExpression = StringToExpressionConverter.createExpressionInstance(model, "", "", String.class.getName(), ExpressionConstants.CONSTANT_TYPE, true);
+		searchIndex.set("Name", nameExpression);
+		Instance valueExpression = StringToExpressionConverter.createExpressionInstance(model, "", "",String.class.getName(), ExpressionConstants.CONSTANT_TYPE, true);
+		searchIndex.set("Value",valueExpression);
 		return searchIndex;
 	}
 }
