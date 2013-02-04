cf51ec8c00ea3d0fcdf400d2807747c6e6e2d13c
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-02-04 11:10:19 +0000

    update pattern viewer exp resolution

--- plugins/org.bonitasoft.studio.expression.editor/src/org/bonitasoft/studio/expression/editor/viewer/PatternExpressionViewer.java
+++ plugins/org.bonitasoft.studio.expression.editor/src/org/bonitasoft/studio/expression/editor/viewer/PatternExpressionViewer.java
@@ -40,8 +40,11 @@ import org.eclipse.jface.fieldassist.ControlDecoration;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.FindReplaceDocumentAdapter;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextListener;
 import org.eclipse.jface.text.TextEvent;
 import org.eclipse.jface.text.TextViewer;
@@ -63,190 +66,192 @@ import org.eclipse.swt.widgets.Event;
  */
 public class PatternExpressionViewer extends Composite {
 
-    private TextViewer viewer;
-    private final IExpressionNatureProvider expressionNatureProvider = new ExpressionContentProvider();
-    private ExpressionContentAssistProcessor contentAssisProcessor;
-    private final Set<ViewerFilter> filters = new HashSet<ViewerFilter>();
-    private Expression patternExpression;
-    private List<Expression> filteredExpressions;
-    private PatternLineStyleListener patternLineStyle;
-    private ControlDecoration hintDecoration;
+	private TextViewer viewer;
+	private final IExpressionNatureProvider expressionNatureProvider = new ExpressionContentProvider();
+	private ExpressionContentAssistProcessor contentAssisProcessor;
+	private final Set<ViewerFilter> filters = new HashSet<ViewerFilter>();
+	private Expression patternExpression;
+	private List<Expression> filteredExpressions;
+	private PatternLineStyleListener patternLineStyle;
+	private ControlDecoration hintDecoration;
 
 
-    public PatternExpressionViewer(Composite parent, int style) {
-        super(parent, style);
-        setLayout(GridLayoutFactory.fillDefaults().numColumns(1).margins(0, 0).extendedMargins(10, 20, 0, 0).create()) ;
-        createTextViewer();
-    }
+	public PatternExpressionViewer(Composite parent, int style) {
+		super(parent, style);
+		setLayout(GridLayoutFactory.fillDefaults().numColumns(1).margins(0, 0).extendedMargins(10, 20, 0, 0).create()) ;
+		createTextViewer();
+	}
 
 
-    protected void createTextViewer() {
-        viewer = new TextViewer(this, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL) ;
-        viewer.getControl().setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+	protected void createTextViewer() {
+		viewer = new TextViewer(this, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL) ;
+		viewer.getControl().setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
 
-        final ControlDecoration helpDecoration = new ControlDecoration(viewer.getControl(), SWT.TOP | SWT.RIGHT);
-        helpDecoration.hide();
-        helpDecoration.setImage(JFaceResources.getImage(Dialog.DLG_IMG_HELP));
-        helpDecoration.setDescriptionText(Messages.patternViewerHelp);
-        helpDecoration.setMarginWidth(2);
-        helpDecoration.setShowOnlyOnFocus(true);
+		final ControlDecoration helpDecoration = new ControlDecoration(viewer.getControl(), SWT.TOP | SWT.RIGHT);
+		helpDecoration.hide();
+		helpDecoration.setImage(JFaceResources.getImage(Dialog.DLG_IMG_HELP));
+		helpDecoration.setDescriptionText(Messages.patternViewerHelp);
+		helpDecoration.setMarginWidth(2);
+		helpDecoration.setShowOnlyOnFocus(true);
 
-        hintDecoration = new ControlDecoration(viewer.getControl(), SWT.TOP | SWT.LEFT);
-        hintDecoration.setImage(Pics.getImage(PicsConstants.hint));
-        hintDecoration.setMarginWidth(2);
-        hintDecoration.setShowHover(true);
-        hintDecoration.setShowOnlyOnFocus(true);
-        hintDecoration.hide();
+		hintDecoration = new ControlDecoration(viewer.getControl(), SWT.TOP | SWT.LEFT);
+		hintDecoration.setImage(Pics.getImage(PicsConstants.hint));
+		hintDecoration.setMarginWidth(2);
+		hintDecoration.setShowHover(true);
+		hintDecoration.setShowOnlyOnFocus(true);
+		hintDecoration.hide();
 
-        patternLineStyle = new PatternLineStyleListener();
-        viewer.getTextWidget().addLineStyleListener(patternLineStyle) ;
-        viewer.setDocument(new Document());
-        viewer.addTextListener(new ITextListener() {
+	
+		viewer.setDocument(new Document());
+		patternLineStyle = new PatternLineStyleListener(viewer.getDocument());
+		viewer.getTextWidget().addLineStyleListener(patternLineStyle) ;
+		viewer.addTextListener(new ITextListener() {
 
-            @Override
-            public void textChanged(TextEvent event) {
-                viewer.getTextWidget().notifyListeners(SWT.Modify,new Event());
-            }
-        });
-        contentAssisProcessor = new ExpressionContentAssistProcessor(viewer.getDocument()) ;
-        final ContentAssistant assistant = new ContentAssistant();
-        assistant.setContentAssistProcessor(contentAssisProcessor,IDocument.DEFAULT_CONTENT_TYPE);
-        assistant.setShowEmptyList(true);
-        assistant.install(viewer);
-        assistant.enableAutoActivation(true);
-        viewer.getControl().addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
+			@Override
+			public void textChanged(TextEvent event) {
+				viewer.getTextWidget().notifyListeners(SWT.Modify,new Event());
+			}
+		});
+		contentAssisProcessor = new ExpressionContentAssistProcessor(viewer.getDocument()) ;
+		final ContentAssistant assistant = new ContentAssistant();
+		assistant.setContentAssistProcessor(contentAssisProcessor,IDocument.DEFAULT_CONTENT_TYPE);
+		assistant.setShowEmptyList(true);
+		assistant.install(viewer);
+		assistant.enableAutoActivation(true);
+		viewer.getControl().addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
 
-                switch (e.keyCode) {
-                    case SWT.F1:
-                        assistant.showPossibleCompletions();
-                        break;
-                    default:
-                        //ignore everything else
-                }
-            }
+				switch (e.keyCode) {
+				case SWT.F1:
+					assistant.showPossibleCompletions();
+					break;
+				default:
+					//ignore everything else
+				}
+			}
 
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if((e.stateMask == SWT.CTRL || e.stateMask == SWT.COMMAND) && e.keyCode == SWT.SPACE){
-                    assistant.showPossibleCompletions();
-                }
-            }
-        });
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if((e.stateMask == SWT.CTRL || e.stateMask == SWT.COMMAND) && e.keyCode == SWT.SPACE){
+					assistant.showPossibleCompletions();
+				}
+			}
+		});
 
-        viewer.getTextWidget().addModifyListener(new ModifyListener() {
+		viewer.getTextWidget().addModifyListener(new ModifyListener() {
 
-            @Override
-            public void modifyText(ModifyEvent e) {
-                patternExpression.getReferencedElements().clear();
-                String content = viewer.getDocument().get();
-                if(content != null && !content.isEmpty()){
-                    for(Expression exp : filteredExpressions){
-                        String currentContent = content;
-                        int indexOf = currentContent.indexOf(exp.getName());
-                        boolean added = false ;
-                        while (indexOf != -1 && !added) {
-                            if(PatternLineStyleListener.isValidKeyWord(exp.getName(), currentContent, indexOf) && isNotIncludedInWord(exp.getName(), indexOf, currentContent, patternExpression.getReferencedElements() )){
-                                patternExpression.getReferencedElements().add(EcoreUtil.copy(exp));
-                                added = true;
-                            }
-                            if(!added){
-                                currentContent = currentContent.substring(indexOf+exp.getName().length());
-                                indexOf = currentContent.indexOf(exp.getName());
-                            }
-                        }
-                    }
-                }
-            }
-        });
-        helpDecoration.show();
-    }
-    
-    /** Check if a word is contained as a sub-word in each element of a list of expressions
-     * 
-     * @param word word to search in the expressions of the list
-     * @param startIndexWord index of the word in the message
-     * @param message the currentContent
-     * @param eList list of Expressions in the current Content
-     * @return 
-     */
-    public boolean isNotIncludedInWord(String word,  int startIndexWord, String message, EList<EObject> eList){
-    	for(Object exp : eList){
-    		if(exp instanceof Expression){
-    			String expWord = ((Expression)exp).getName();
-    			int offset = 0;
-    			while(message.indexOf( expWord, offset)!=-1){
-        			int startIndexReference = message.indexOf(expWord, offset);
-        			int endIndexReference = startIndexReference+expWord.length();
-        			if(startIndexWord-offset >= startIndexReference && startIndexWord < endIndexReference){
-        				return false;
-        			}
-        			offset=endIndexReference;
-    			}
-    		}
-    	}
-    	return true;
-    }
+			@Override
+			public void modifyText(ModifyEvent e) {
+				patternExpression.getReferencedElements().clear();
+				String content = viewer.getDocument().get();
+				if(content != null && !content.isEmpty()){
+					final FindReplaceDocumentAdapter finder = new FindReplaceDocumentAdapter(viewer.getDocument());
+					final Set<String> addedExp = new HashSet<String>();
+					for(Expression exp : filteredExpressions){
+						IRegion index;
+						try {
+							index = finder.find(0,exp.getName(), true, true, true, false);
+							if (index != null) {
+								if(!addedExp.contains(exp.getName())){
+									patternExpression.getReferencedElements().add(EcoreUtil.copy(exp));
+									addedExp.add(exp.getName());
+								}
+							}
+						} catch (final BadLocationException e1) {
+							// Just ignore them
+						}
+					}
+				}
+			}
+		});
+		helpDecoration.show();
+	}
 
-    public void setContextInput(EObject input){
-        manageNatureProviderAndAutocompletionProposal(input) ;
-    }
+	/** Check if a word is contained as a sub-word in each element of a list of expressions
+	 * 
+	 * @param word word to search in the expressions of the list
+	 * @param startIndexWord index of the word in the message
+	 * @param message the currentContent
+	 * @param eList list of Expressions in the current Content
+	 * @return 
+	 */
+	public boolean isNotIncludedInWord(String word,  int startIndexWord, String message, EList<EObject> eList){
+		for(Object exp : eList){
+			if(exp instanceof Expression){
+				String expWord = ((Expression)exp).getName();
+				int offset = 0;
+				while(message.indexOf( expWord, offset)!=-1){
+					int startIndexReference = message.indexOf(expWord, offset);
+					int endIndexReference = startIndexReference+expWord.length();
+					if(startIndexWord-offset >= startIndexReference && startIndexWord < endIndexReference){
+						return false;
+					}
+					offset=endIndexReference;
+				}
+			}
+		}
+		return true;
+	}
 
-    public void setPatternExpression(final Expression patternExpression){
-        Assert.isLegal(patternExpression.getType() != null && patternExpression.getType().equals(ExpressionConstants.PATTERN_TYPE)) ;
-        this.patternExpression = patternExpression;
-    }
+	public void setContextInput(EObject input){
+		manageNatureProviderAndAutocompletionProposal(input) ;
+	}
 
-
-    protected void manageNatureProviderAndAutocompletionProposal(Object input) {
-        if(expressionNatureProvider != null){
-            expressionNatureProvider.setContext((EObject) input);
-        }
-        filteredExpressions =  getFilteredExpressions() ;
-        Set<Expression> expressionSet = new HashSet<Expression>(filteredExpressions);
-        contentAssisProcessor.setExpressions(expressionSet);
-        patternLineStyle.setExpressions(expressionSet);
-    }
-
-    private List<Expression> getFilteredExpressions() {
-        List<Expression> filteredExpressions = new ArrayList<Expression>() ;
-        Expression[] expressions = expressionNatureProvider.getExpressions();
-        EObject input =  expressionNatureProvider.getContext() ;
-        if(expressions != null){
-            filteredExpressions.addAll(Arrays.asList(expressions)) ;
-            if(input != null){
-                for(Expression exp : expressions) {
-                    for(ViewerFilter filter : filters){
-                        if(filter != null && !filter.select(viewer, input, exp)){
-                            filteredExpressions.remove(exp) ;
-                        }
-                    }
-                }
-            }
-        }
-        Collections.sort(filteredExpressions, new Comparator<Expression>() {
-
-            @Override
-            public int compare(Expression exp0, Expression exp1) {
-                return -Integer.valueOf(exp0.getName().length()).compareTo(Integer.valueOf(exp1.getName().length()));
-            }
-
-        });
-        return filteredExpressions ;
-    }
-
-    public void addFilter(ViewerFilter viewerFilter) {
-        filters.add(viewerFilter) ;
-    }
-
-    public StyledText getTextControl(){
-        return viewer.getTextWidget() ;
-    }
+	public void setPatternExpression(final Expression patternExpression){
+		Assert.isLegal(patternExpression.getType() != null && patternExpression.getType().equals(ExpressionConstants.PATTERN_TYPE)) ;
+		this.patternExpression = patternExpression;
+	}
 
 
-    public void setHint(String hint) {
-        hintDecoration.setDescriptionText(hint);
-        hintDecoration.show();
-    }
+	protected void manageNatureProviderAndAutocompletionProposal(Object input) {
+		if(expressionNatureProvider != null){
+			expressionNatureProvider.setContext((EObject) input);
+		}
+		filteredExpressions =  getFilteredExpressions() ;
+		Set<Expression> expressionSet = new HashSet<Expression>(filteredExpressions);
+		contentAssisProcessor.setExpressions(expressionSet);
+		patternLineStyle.setExpressions(expressionSet);
+	}
+
+	private List<Expression> getFilteredExpressions() {
+		List<Expression> filteredExpressions = new ArrayList<Expression>() ;
+		Expression[] expressions = expressionNatureProvider.getExpressions();
+		EObject input =  expressionNatureProvider.getContext() ;
+		if(expressions != null){
+			filteredExpressions.addAll(Arrays.asList(expressions)) ;
+			if(input != null){
+				for(Expression exp : expressions) {
+					for(ViewerFilter filter : filters){
+						if(filter != null && !filter.select(viewer, input, exp)){
+							filteredExpressions.remove(exp) ;
+						}
+					}
+				}
+			}
+		}
+		Collections.sort(filteredExpressions, new Comparator<Expression>() {
+
+			@Override
+			public int compare(Expression exp0, Expression exp1) {
+				return -Integer.valueOf(exp0.getName().length()).compareTo(Integer.valueOf(exp1.getName().length()));
+			}
+
+		});
+		return filteredExpressions ;
+	}
+
+	public void addFilter(ViewerFilter viewerFilter) {
+		filters.add(viewerFilter) ;
+	}
+
+	public StyledText getTextControl(){
+		return viewer.getTextWidget() ;
+	}
+
+
+	public void setHint(String hint) {
+		hintDecoration.setDescriptionText(hint);
+		hintDecoration.show();
+	}
 }
--- plugins/org.bonitasoft.studio.expression.editor/src/org/bonitasoft/studio/expression/editor/viewer/PatternLineStyleListener.java
+++ plugins/org.bonitasoft.studio.expression.editor/src/org/bonitasoft/studio/expression/editor/viewer/PatternLineStyleListener.java
@@ -22,6 +22,10 @@ import java.util.Vector;
 
 import org.bonitasoft.studio.common.ExpressionConstants;
 import org.bonitasoft.studio.model.expression.Expression;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.FindReplaceDocumentAdapter;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.LineStyleEvent;
 import org.eclipse.swt.custom.LineStyleListener;
@@ -35,73 +39,79 @@ import org.eclipse.swt.widgets.Display;
  */
 public class PatternLineStyleListener implements LineStyleListener {
 
-    private Set<Expression> expressions = new HashSet<Expression>();
+	private Set<Expression> expressions = new HashSet<Expression>();
+	private FindReplaceDocumentAdapter finder;
 
-    /* (non-Javadoc)
-     * @see org.eclipse.swt.custom.LineStyleListener#lineGetStyle(org.eclipse.swt.custom.LineStyleEvent)
-     */
-    @Override
-    public void lineGetStyle(LineStyleEvent event) {
-        Vector<StyleRange> styles = new Vector<StyleRange>();
+	public PatternLineStyleListener(IDocument document) {
+		finder = new FindReplaceDocumentAdapter(document);
+	}
 
-        addExpressionStyles( event,  styles, ExpressionConstants.VARIABLE_TYPE, SWT.COLOR_DARK_RED);
-        addExpressionStyles( event,  styles, ExpressionConstants.PARAMETER_TYPE, SWT.COLOR_DARK_GREEN);
-        addExpressionStyles( event,  styles, ExpressionConstants.FORM_FIELD_TYPE, SWT.COLOR_DARK_CYAN);
-        event.styles = styles.toArray(new StyleRange[styles.size()]);
-    }
+	/* (non-Javadoc)
+	 * @see org.eclipse.swt.custom.LineStyleListener#lineGetStyle(org.eclipse.swt.custom.LineStyleEvent)
+	 */
+	@Override
+	public void lineGetStyle(LineStyleEvent event) {
+		Vector<StyleRange> styles = new Vector<StyleRange>();
 
-    /**
-     * 
-     * @param event
-     * @param styles
-     * @param type
-     * @param color
-     */
-    protected void addExpressionStyles(LineStyleEvent event, Vector<StyleRange> styles, String type, int color){
-        for (Expression exp : expressions) {
-            if(type.equals(exp.getType())){
-                String content = event.lineText;
-                int indexOf = content.indexOf(exp.getName());
-                int offset = indexOf;
-                while (indexOf != -1) {
-                    if(isValidKeyWord(exp.getName(), content, indexOf)){
-                        styles.add(new StyleRange(event.lineOffset + offset, exp.getName().length(), Display.getDefault().getSystemColor(color), null,SWT.BOLD));
-                    }
-                    content = content.substring(indexOf+exp.getName().length());
-                    indexOf = content.indexOf(exp.getName());
-                    offset = indexOf+exp.getName().length() + offset;
-                }
-            }
-        }
-    }
-    
+		addExpressionStyles( event,  styles, ExpressionConstants.VARIABLE_TYPE, SWT.COLOR_DARK_RED);
+		addExpressionStyles( event,  styles, ExpressionConstants.PARAMETER_TYPE, SWT.COLOR_DARK_GREEN);
+		addExpressionStyles( event,  styles, ExpressionConstants.FORM_FIELD_TYPE, SWT.COLOR_DARK_CYAN);
+		event.styles = styles.toArray(new StyleRange[styles.size()]);
+	}
 
-    public static boolean isValidKeyWord(String expName, String content, int indexOf) {
-        return isStartingWord(indexOf) || isNotEscapeWord(content, indexOf);
-    }
+	/**
+	 * 
+	 * @param event
+	 * @param styles
+	 * @param type
+	 * @param color
+	 */
+	protected void addExpressionStyles(LineStyleEvent event, Vector<StyleRange> styles, String type, int color){
+		for (Expression exp : expressions) {
+			if(type.equals(exp.getType())){
+				try {
+					int i = 0 ;
+					IRegion index = null;
+					index = finder.find(i, exp.getName(), true, true, true, false);
+					while(index != null){
+						styles.add(new StyleRange(index.getOffset(), index.getLength(), Display.getDefault().getSystemColor(color), null,SWT.BOLD));
+						i = i + index.getLength();
+						index = finder.find(i, exp.getName(), true, true, true, false);
+					}
+				} catch (BadLocationException e) {
+					// Ignore
+				}
+			}
+		}
+	}
 
-    protected static boolean isNextCharacterAWhiteSpace(String expName, String content, int indexOf) {
-        return Character.isWhitespace(content.charAt(indexOf+expName.length()));
-    }
 
-    protected static boolean isFinishingWord(String expName, String content, int indexOf) {
-        return indexOf+expName.length()+1 > content.length();
-    }
+	public static boolean isValidKeyWord(String expName, String content, int indexOf) {
+		return isStartingWord(indexOf) || isNotEscapeWord(content, indexOf);
+	}
 
-    protected static boolean isPreviousCharacterAWhiteSpace(String content, int indexOf) {
-        return Character.isWhitespace(content.charAt(indexOf-1));
-    }
+	protected static boolean isNextCharacterAWhiteSpace(String expName, String content, int indexOf) {
+		return Character.isWhitespace(content.charAt(indexOf+expName.length()));
+	}
 
-    protected static boolean isNotEscapeWord(String content, int indexOf) {
-        return content.charAt(indexOf-1) != '\\';
-    }
+	protected static boolean isFinishingWord(String expName, String content, int indexOf) {
+		return indexOf+expName.length()+1 > content.length();
+	}
 
-    protected static boolean isStartingWord(int indexOf) {
-        return indexOf-1 < 0;
-    }
+	protected static boolean isPreviousCharacterAWhiteSpace(String content, int indexOf) {
+		return Character.isWhitespace(content.charAt(indexOf-1));
+	}
 
-    public void setExpressions(Set<Expression> expressions) {
-        this.expressions = expressions ;
-    }
+	protected static boolean isNotEscapeWord(String content, int indexOf) {
+		return content.charAt(indexOf-1) != '\\';
+	}
+
+	protected static boolean isStartingWord(int indexOf) {
+		return indexOf-1 < 0;
+	}
+
+	public void setExpressions(Set<Expression> expressions) {
+		this.expressions = expressions ;
+	}
 
 }
