7f54e20d982a6537f0f76b3fa4ffeb90b9e7d593
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-03-12 15:04:28 +0000

    use long text data

--- plugins/org.bonitasoft.studio.engine/src/org/bonitasoft/studio/engine/export/switcher/DataSwitch.java
+++ plugins/org.bonitasoft.studio.engine/src/org/bonitasoft/studio/engine/export/switcher/DataSwitch.java
@@ -18,6 +18,7 @@ package org.bonitasoft.studio.engine.export.switcher;
 
 import org.bonitasoft.engine.bpm.model.DataDefinitionBuilder;
 import org.bonitasoft.engine.bpm.model.FlowElementBuilder;
+import org.bonitasoft.engine.bpm.model.TextDataDefinitionBuilder;
 import org.bonitasoft.engine.expression.Expression;
 import org.bonitasoft.studio.model.process.BooleanType;
 import org.bonitasoft.studio.model.process.Data;
@@ -52,12 +53,12 @@ public class DataSwitch extends ProcessSwitch<DataDefinitionBuilder> {
 
     @Override
     public DataDefinitionBuilder caseStringType(final StringType type) {
-        return builder.addStringData(data.getName(), expr);
+    	return builder.addTextData(data.getName(), expr);
     }
 
     @Override
     public DataDefinitionBuilder caseLongType(LongType object) {
-        return  builder.addLongData(data.getName(), expr);
+        return builder.addLongData(data.getName(), expr);
     }
 
     @Override
