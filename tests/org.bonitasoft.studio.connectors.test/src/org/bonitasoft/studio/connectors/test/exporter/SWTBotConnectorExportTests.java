d90dcd41388292a486119e434d8e343de6f77ee0
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-03-14 10:29:36 +0000

    update email connector version

--- tests/org.bonitasoft.studio.connectors.test/src/org/bonitasoft/studio/connectors/test/exporter/SWTBotConnectorExportTests.java
+++ tests/org.bonitasoft.studio.connectors.test/src/org/bonitasoft/studio/connectors/test/exporter/SWTBotConnectorExportTests.java
@@ -79,14 +79,14 @@ public class SWTBotConnectorExportTests extends SWTBotGefTestCase {
 
     @Test
     public void testBonitaConnectorExport() throws Exception {
-        final String emailConnector = "email-impl (1.0.1) -- org.bonitasoft.connectors.email.EmailConnector";
+        final String emailConnector = "email-impl (1.0.2) -- org.bonitasoft.connectors.email.EmailConnector";
         final String fileName = "email-impl-1.0.1.zip";
         exportConnector(emailConnector, fileName, true, false);
     }
 
     @Test
     public void testRewriteBonitaConnectorExport() throws Exception {
-        final String emailConnector = "email-impl (1.0.1) -- org.bonitasoft.connectors.email.EmailConnector";
+        final String emailConnector = "email-impl (1.0.2) -- org.bonitasoft.connectors.email.EmailConnector";
         final String fileName = "email-impl-1.0.1.zip";
         exportConnector(emailConnector, fileName, true, false);
         exportConnector(emailConnector, fileName, true, false);
