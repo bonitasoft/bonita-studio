611ebb4f7542e9190222d694484d2c7e18017072
Aurelien Pupier <aaurelien.pupier@bonitasoft.com>
2013-03-27 10:26:38 +0000

    avoid NPE

--- plugins/org.bonitasoft.studio.validation/src/org/bonitasoft/studio/validation/constraints/ValidationContentProvider.java
+++ plugins/org.bonitasoft.studio.validation/src/org/bonitasoft/studio/validation/constraints/ValidationContentProvider.java
@@ -81,31 +81,35 @@ public class ValidationContentProvider implements IStructuredContentProvider{
 			final IEditorInput input  = ((FormDiagramEditor) inputElement).getEditorInput();
 			if(input instanceof URIEditorInput){
 				String file = ((URIEditorInput) input).getURI().toPlatformString(true);
-				int idxSlash = file.lastIndexOf("/");
-				DiagramRepositoryStore store = (DiagramRepositoryStore) RepositoryManager.getInstance().getRepositoryStore(DiagramRepositoryStore.class);
-				DiagramFileStore dfs = null;
-				if(idxSlash <0){
-					dfs = store.getChild(file);
-				}else{
-					dfs = store.getChild(file.substring(idxSlash+1));
-				}
-				if(dfs!=null && dfs.getResource().exists()){
-					try {
-						final List<IMarker> markerList = new ArrayList<IMarker>();
-						IMarker[] markerTab = (IMarker[]) dfs.getResource().findMarkers("org.bonitasoft.studio.diagram.form.diagnostic", false, IResource.DEPTH_INFINITE);
-						for(int i=0; i<markerTab.length; i++ ){
-							Marker m = (Marker)markerTab[i];
-							String location = (String) m.getAttribute("location");
-							if(location.matches(".*::"+input.getName()+"::.*")){
-								markerList.add(m);
-							}
-						}
-						return markerList.toArray();
-
-					} catch (CoreException e) {
-						BonitaStudioLog.error(e);
-						return noElements;
+				if(file != null){
+					int idxSlash = file.lastIndexOf("/");
+					DiagramRepositoryStore store = (DiagramRepositoryStore) RepositoryManager.getInstance().getRepositoryStore(DiagramRepositoryStore.class);
+					DiagramFileStore dfs = null;
+					if(idxSlash <0){
+						dfs = store.getChild(file);
+					}else{
+						dfs = store.getChild(file.substring(idxSlash+1));
 					}
+					if(dfs!=null && dfs.getResource().exists()){
+						try {
+							final List<IMarker> markerList = new ArrayList<IMarker>();
+							IMarker[] markerTab = (IMarker[]) dfs.getResource().findMarkers("org.bonitasoft.studio.diagram.form.diagnostic", false, IResource.DEPTH_INFINITE);
+							for(int i=0; i<markerTab.length; i++ ){
+								Marker m = (Marker)markerTab[i];
+								String location = (String) m.getAttribute("location");
+								if(location.matches(".*::"+input.getName()+"::.*")){
+									markerList.add(m);
+								}
+							}
+							return markerList.toArray();
+
+						} catch (CoreException e) {
+							BonitaStudioLog.error(e);
+							return noElements;
+						}
+					}
+				} else {
+					BonitaStudioLog.log("File of FormDiagramEditor input not found."+ ((FormDiagramEditor) inputElement).getTitle());
 				}
 
 				return noElements;
