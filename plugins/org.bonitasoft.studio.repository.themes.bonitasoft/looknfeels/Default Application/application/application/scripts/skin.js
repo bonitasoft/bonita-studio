47ffea61cafef7e6b2ca1a36f8b677d1103867af
Florine Boudin <florine.boudin@bonitasoft.com>
2013-03-29 11:03:02 +0000

    update look n feel of Web

--- plugins/org.bonitasoft.studio.repository.themes.bonitasoft/looknfeels/Default Application/application/application/scripts/skin.js
+++ plugins/org.bonitasoft.studio.repository.themes.bonitasoft/looknfeels/Default Application/application/application/scripts/skin.js
@@ -1,4 +1,19 @@
 $(function() {
 	$('input').customInput();
-	$('input').css("border","1px solid red !important");
+	$(".bonita_form_field.list_container").each(function(i,e){
+		resizeSelect(e);
+	});
 });
+function resizeSelect(e){
+	var listContainer = $(e);
+	var arrowBgWidth = 35;
+	var containerWidth = listContainer.outerWidth();
+	var list = listContainer.find('select:first');
+	var listWidth = list.outerWidth();
+	if(listWidth>arrowBgWidth){
+		listContainer.css("width", listWidth+arrowBgWidth);
+	}else{
+		listContainer.css("width", arrowBgWidth);
+	}
+	list.css("width",  $(e).outerWidth()+arrowBgWidth);
+}
