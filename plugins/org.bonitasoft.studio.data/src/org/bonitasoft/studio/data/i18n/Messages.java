56cacb5a7f10c288895747621984ccc265ccc357
Aurelien Pupier <aaurelien.pupier@bonitasoft.com>
2013-04-10 12:11:22 +0000

    [STUDIo-1645]remove unused keys

--- plugins/org.bonitasoft.studio.data/src/org/bonitasoft/studio/data/i18n/Messages.java
+++ plugins/org.bonitasoft.studio.data/src/org/bonitasoft/studio/data/i18n/Messages.java
@@ -26,89 +26,52 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 
 
-    public static String literalLabel;
-    public static String literalAlreadyExistsTitle;
-    public static String literalAlreadyExistsMessage;
-    public static String createLiteralTitle;
     public static String addDataWizardTitle;
     public static String addDataWizardDescription;
-    public static String addDataWizardMessage;
-    public static String newEnumDescription;
     public static String browseClasses;
     public static String classLabel;
     public static String selectXSDToImport;
     public static String Add;
-    public static String attachementLabel;
     public static String datatypeLabel;
-    public static String cancelEnum;
-    public static String okEnum;
-    public static String deleteEnum;
     public static String down;
     public static String up;
-    public static String addLiteralLabel;
     public static String removeLiteralLabel;
-    public static String createNewTypeLabel;
-    public static String dataNameIsnotAvailable;
     public static String dataAlreadyExist;
     public static String defaultValueLabel;
-    public static String editTypeButtonLabel;
-    public static String datatypeLiteralsLabel;
     public static String dateFormatLabel;
-    public static String dataNameLabel;
     public static String dataDescriptionLabel;
     public static String addDataCommandLabel;
-    public static String xmlDefaultValueTooLongTitle;
-    public static String xmlDefaultValueTooLongMessage;
     public static String AddXSDSchema;
     public static String selectXMLNamespace;
     public static String selectXMLElement;
-    public static String multiplicity_single;
-    public static String multiplicity_multiple;
-    public static String multiplicity;
     public static String transientLabel;
     public static String transientHint;
     public static String importXsd_title;
     public static String importXsd_msg;
-    public static String createDataLabel;
-    public static String attachmentMaxSizeWarning;
     public static String dataTechnicalTypeLabel;
     public static String variableType;
     public static String name;
     public static String returnType;
-    public static String dateWidgetReturnTypeWarning;
     public static String javaType;
     public static String browseJava;
     public static String attachementRepository;
 
-    public static String filterLabel;
     public static String addData;
     public static String removeData;
     public static String updateData;
-    public static String createDataType;
-    public static String promoteData;
-    public static String promoteData_tooltip;
-    public static String updateDataCommandLabel;
-    public static String updateDataWizardTitle;
-    public static String updateDataWizardDescription;
-    public static String updateDataWizardMessage;
     public static String selectOnlyOneElementMessage;
     public static String selectOnlyOneElementTitle;
-    public static String addBasicDataTypeCommandLabel;
 
     // Deletion
     public static String deleteDataDialogTitle;
     public static String deleteDialogConfirmMessage;
-    public static String removeDataCommandLabel;
-    public static String clear;
 
     public static String moveData;
     public static String moveData_tooltip;
     public static String chooseTargetStepOrProcess;
 
-    public static String moveDataCommandLabel;
     public static String moveDataWizardTitle;
     public static String moveDataWizardMessage;
-    public static String moveDataWizardDescription;
 
     public static String PromoteDataWarningTitle;
     public static String PromoteDataWarningMessage;
