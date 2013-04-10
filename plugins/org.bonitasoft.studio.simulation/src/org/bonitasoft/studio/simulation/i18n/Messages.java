a4b2dbe41ca469694355df40c37876ea2e09a63c
Aurelien Pupier <aaurelien.pupier@bonitasoft.com>
2013-04-10 16:09:41 +0000

    [STUDIO-1645]remove unused keys

--- plugins/org.bonitasoft.studio.simulation/src/org/bonitasoft/studio/simulation/i18n/Messages.java
+++ plugins/org.bonitasoft.studio.simulation/src/org/bonitasoft/studio/simulation/i18n/Messages.java
@@ -26,40 +26,23 @@ import org.eclipse.osgi.util.NLS;
  */
 public class Messages extends NLS {
     private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
-    public static String simulationErrorDialogTitle;
     public static String SimulationResourceRepositoryName;
     public static String EditCalendarWizard_Title;
-    public static String EditCalendarWizard_WrongHoursFormat;
-    public static String EditCalendarWizardPage_DayDescription;
-    public static String EditCalendarWizardPage_DayGroup;
-    public static String EditCalendarWizardPage_DayHelp;
     public static String EditCalendarWizardPage_Message;
-    public static String EditCalendarWizardPage_MonthDesc;
     public static String EditCalendarWizardPage_Title;
-    public static String EditCalendarWizardPage_WeekDesc;
-    public static String EditCalendarWizardPage_WeekGroup;
-    public static String EditCalendarWizardPage_YearGroup;
     public static String EditSimulationResourceWizard_WindowTitle;
     public static String ManageResourcesWizardPage_lblName_text;
-    public static String ManageResourcesWizardPage_lblType_text;
     public static String ManageResourcesWizardPage_lblQuantity_text;
     public static String ManageResourcesWizardPage_lblFixedCost_text;
     public static String ManageResourcesWizardPage_lblTimeCost_text;
     public static String add;
     public static String edit;
     public static String remove;
-    public static String ManageResourcesWizardPage_lblTimeUnit_text;
-    public static String ManageResourcesWizardPage_lblCodtUnit_text;
-
-    public static String EditSimulationResourceWizardPage_Message;
-    public static String EditSimulationResourceWizardPage_Title;
 
     public static String EditSimulationResourceWizardPage_lblName_text;
-    public static String EditSimulationResourceWizardPage_lblType_text;
     public static String EditSimulationResourceWizardPage_lblQuantity_text;
     public static String EditSimulationResourceWizardPage_lblFixedCost_text;
     public static String EditSimulationResourceWizardPage_lblTimeCost_text;
-    public static String EditSimulationResourceWizardPage_lblTimeUnit_text;
     public static String EditSimulationResourceWizardPage_lblCostUnit_text;
     public static String EditSimulationResourceWizard_ErrorEmptyName;
     public static String EditSimulationResourceWizard_ErrorFixedCost;
@@ -75,14 +58,8 @@ public class Messages extends NLS {
     public static String week;
     public static String month;
     public static String year;
-    public static String EditSimulationResourceWizardPage_lblCalendar_text;
-    public static String EditSimulationResourceWizard_btnCalendar;
-    public static String ManageResourcesWizardPage_lblMaxQuantity_text;
-    public static String EditSimulationResourceWizard_ErrorMaxQuantity;
     public static String mustBeAPercentage;
     public static String EditSimulationLoadProfileWizardPage_begin;
-    public static String EditSimulationLoadProfileWizardPage_calendar;
-    public static String EditSimulationLoadProfileWizardPage_editCalendar;
     public static String EditSimulationLoadProfileWizardPage_end;
     public static String EditSimulationLoadProfileWizardPage_injectionPeriods;
     public static String EditSimulationLoadProfileWizardPage_name;
@@ -107,10 +84,7 @@ public class Messages extends NLS {
     public static String SimulationDataType_boolean;
     public static String SimulationDataType_number;
     public static String SimulationDataType_literal;
-    public static String TabbedSimulationPropertySection_Data;
-    public static String TabbedSimulationPropertySection_General;
     public static String dataNameLabel;
-    public static String dataDescriptionLabel;
     public static String datatypeLabel;
     public static String SimulationBoolean;
     public static String SimulationNumberData;
@@ -119,14 +93,12 @@ public class Messages extends NLS {
     public static String AddSimulationDataWizardPage_min;
     public static String AddSimulationDataWizardPage_probability;
     public static String AddSimulationDataWizardPage_ProbabilityOfTrueLabel;
-    public static String AddSimulationDataWizardPage_repartition;
     public static String process;
     public static String RunningSimulation;
     public static String Expression;
     public static String AddSimulationDataWizardPage_title;
     public static String AddSimulationDataWizardPage_msg;
     public static String EditSimulationLoadProfileWizardPage_directRepartitionType_hint;
-    public static String resourceAssignment_title;
     public static String loopCondition;
     public static String addAPeriod;
     public static String simulation_Error;
@@ -143,8 +115,6 @@ public class Messages extends NLS {
     public static String per;
     public static String EditSimulationResourceCalendarWizardPage_Title;
     public static String EditSimulationResourceCalendarWizardPage_Message;
-    public static String EditSimulationResourceCostWizardPage_Title;
-    public static String EditSimulationResourceCostWizardPage_Message;
     public static String Monday;
     public static String Tuesday;
     public static String Wednesday;
@@ -154,7 +124,6 @@ public class Messages extends NLS {
     public static String Sunday;
     public static String from;
     public static String to;
-    public static String resourceTypeHint;
     public static String targetQuantityHint;
     public static String quantityHint;
     public static String costPerUseHint;
@@ -182,8 +151,6 @@ public class Messages extends NLS {
     public static String ExecutionTime;
     public static String ExecutionTimePlus;
     public static String resourceAssignmentLabel;
-    public static String simulationFinished;
-    public static String openReportQuestion;
     public static String generatingReport;
     public static String mustBeANumber;
     public static String EditSimulationResourceWizard_ErrorTooManyTargetQuantity;
@@ -203,15 +170,12 @@ public class Messages extends NLS {
     public static String SimulationLoadProfileRepositoryName;
     public static String EditSimulationResourceWizard_ErrorQuantityLessThanZero;
     public static String createSimulationData;
-    public static String importSimulationArtifacts_title;
     public static String ResourceCost;
     public static String resourceDefaultName;
     public static String loadProfileDefaultName;
     public static String NoExecutionTime;
     public static String deleteQuestionTitle;
     public static String deleteQuestionMessage;
-    public static String initializingSimResourceRepository;
-    public static String initializingSimLoadProfileRepository;
     public static String removeResourceTitle;
     public static String removeResourceMessage;
     public static String unlimited;
@@ -224,6 +188,7 @@ public class Messages extends NLS {
 	public static String simulationTaskDataDescription;
 	public static String simulationProcessGeneralDescription;
 	public static String simulationProcessDataDescription;
+	public static String EditSimulationResourceWizard_ErrorMaxQuantity;
 
 
 
