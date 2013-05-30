d95ff3a1a39bfb7d4d8efd9d31a10fc8c188f396
Arelie Zara <aurelie.zara@bonitasoft.com>
2013-05-30 09:22:55 +0000

    correction of bug : when deselecting "default flow", conditionViewer was still disabled.

--- plugins/org.bonitasoft.studio.properties/src/org/bonitasoft/studio/properties/sections/general/TransitionCondition.java
+++ plugins/org.bonitasoft.studio.properties/src/org/bonitasoft/studio/properties/sections/general/TransitionCondition.java
@@ -471,6 +471,8 @@ public class TransitionCondition implements IExtensibleGridPropertySectionContri
             dataBindingContext.bindValue(SWTObservables.observeEnabled(useExpressionCondition),
                     EMFEditObservables.observeValue(editingDomain, transition, ProcessPackage.Literals.SEQUENCE_FLOW__IS_DEFAULT),new UpdateValueStrategy().setConverter(conditionConverter),new UpdateValueStrategy().setConverter(conditionConverter));
             
+            dataBindingContext.bindValue(SWTObservables.observeSelection(useExpressionCondition),SWTObservables.observeEnabled(conditionViewer.getControl()));
+            
             dataBindingContext.bindValue(SWTObservables.observeEnabled(useDecisionTable),
                     EMFEditObservables.observeValue(editingDomain, transition, ProcessPackage.Literals.SEQUENCE_FLOW__IS_DEFAULT),new UpdateValueStrategy().setConverter(conditionConverter),new UpdateValueStrategy().setConverter(conditionConverter));
             
