e0935f05abd42de464703c95affdac16abf65480
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-06-03 09:16:29 +0000

    add a decorator on form transiant data icons

--- plugins/org.bonitasoft.studio.data/src/org/bonitasoft/studio/data/provider/DataExpressionProvider.java
+++ plugins/org.bonitasoft.studio.data/src/org/bonitasoft/studio/data/provider/DataExpressionProvider.java
@@ -22,8 +22,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.bonitasoft.studio.common.DatasourceConstants;
 import org.bonitasoft.studio.common.ExpressionConstants;
 import org.bonitasoft.studio.common.emf.tools.ModelHelper;
+import org.bonitasoft.studio.data.DataPlugin;
 import org.bonitasoft.studio.data.i18n.Messages;
 import org.bonitasoft.studio.expression.editor.provider.IExpressionEditor;
 import org.bonitasoft.studio.expression.editor.provider.IExpressionProvider;
@@ -40,6 +42,8 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
+import org.eclipse.jface.viewers.DecorationOverlayIcon;
+import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.swt.graphics.Image;
 
 /**
@@ -48,106 +52,119 @@ import org.eclipse.swt.graphics.Image;
  */
 public class DataExpressionProvider implements IExpressionProvider {
 
-    private final ComposedAdapterFactory adapterFactory;
-    private final AdapterFactoryLabelProvider adapterLabelProvider;
+	private final ComposedAdapterFactory adapterFactory;
+	private final AdapterFactoryLabelProvider adapterLabelProvider;
 
 
-    public DataExpressionProvider(){
-        adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
-        adapterLabelProvider  = new AdapterFactoryLabelProvider(adapterFactory) ;
-    }
+	public DataExpressionProvider(){
+		adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+		adapterLabelProvider  = new AdapterFactoryLabelProvider(adapterFactory) ;
+	}
 
-    @Override
-    public Set<Expression> getExpressions(EObject context) {
-        Set<Expression> result = new HashSet<Expression>() ;
+	@Override
+	public Set<Expression> getExpressions(EObject context) {
+		Set<Expression> result = new HashSet<Expression>() ;
 
-        Form form = null ;
-        PageFlow pf= null;
-        if(context instanceof Widget){
-            form =  ModelHelper.getForm((Widget) context) ;
-        }else if(context instanceof Form){
-            form = (Form) context ;
-        }else if(context instanceof PageFlow){
-            pf = (PageFlow) context ;
-        }
+		Form form = null ;
+		PageFlow pf= null;
+		if(context instanceof Widget){
+			form =  ModelHelper.getForm((Widget) context) ;
+		}else if(context instanceof Form){
+			form = (Form) context ;
+		}else if(context instanceof PageFlow){
+			pf = (PageFlow) context ;
+		}
 
-        if(form != null){
-            final EObject formContainer = form.eContainer();
-            if(formContainer != null){
-                if(form.eContainmentFeature() != null && (formContainer instanceof PageFlow || formContainer instanceof ViewPageFlow) ){
-                    for(Data d :  getDataInForm(form, formContainer)){
-                        result.add(createExpression(d)) ;
-                    }
-                }
-            }
-        } else if(pf!= null){
-        	for(Data d :  ModelHelper.getAccessibleData(pf)){
-        		result.add(createExpression(d)) ;
-        	}
-        } else if(context instanceof EObject){
-            for(Data d : ModelHelper.getAccessibleData(context,true) ){
-                result.add(createExpression(d)) ;
-            }
-        }
+		if(form != null){
+			final EObject formContainer = form.eContainer();
+			if(formContainer != null){
+				if(form.eContainmentFeature() != null && (formContainer instanceof PageFlow || formContainer instanceof ViewPageFlow) ){
+					for(Data d :  getDataInForm(form, formContainer)){
+						result.add(createExpression(d)) ;
+					}
+				}
+			}
+		} else if(pf!= null){
+			for(Data d :  ModelHelper.getAccessibleData(pf)){
+				result.add(createExpression(d)) ;
+			}
+		} else if(context instanceof EObject){
+			for(Data d : ModelHelper.getAccessibleData(context,true) ){
+				result.add(createExpression(d)) ;
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    protected List<Data> getDataInForm(Form form, final EObject formContainer) {
-        return ModelHelper.getAccessibleDataInForms(formContainer, form.eContainmentFeature());
-    }
-    
-    @Override
-    public String getExpressionType() {
-        return ExpressionConstants.VARIABLE_TYPE;
-    }
+	protected List<Data> getDataInForm(Form form, final EObject formContainer) {
+		return ModelHelper.getAccessibleDataInForms(formContainer, form.eContainmentFeature());
+	}
 
-    @Override
-    public Image getIcon(Expression expression) {
-        if(expression.getReferencedElements().isEmpty()){
-            return null ;
-        }
+	@Override
+	public String getExpressionType() {
+		return ExpressionConstants.VARIABLE_TYPE;
+	}
 
-        EObject reference = expression.getReferencedElements().get(0);
-        return adapterLabelProvider.getImage(reference) ;
-    }
+	@Override
+	public Image getIcon(Expression expression) {
+		if(expression.getReferencedElements().isEmpty()){
+			return null ;
+		}
 
-    @Override
-    public String getProposalLabel(Expression expression) {
-        return expression.getName();
-    }
+		EObject reference = expression.getReferencedElements().get(0);
+		boolean formTransient = false;
+		if(reference instanceof Data){
+			formTransient = ((Data) reference).getDatasourceId() != null && DatasourceConstants.PAGEFLOW_DATASOURCE.equals(((Data) reference).getDatasourceId());
+		}
+		final Image icon = adapterLabelProvider.getImage(reference) ;
+		if(formTransient){
+			Image img = DataPlugin.getDefault().getImageRegistry().get("decoratedImageFor"+reference.eClass().getName());
+			if(img == null){
+				img = new DecorationOverlayIcon(icon,Pics.getImageDescriptor("form_decorator.png", DataPlugin.getDefault()),IDecoration.BOTTOM_LEFT).createImage();
+				DataPlugin.getDefault().getImageRegistry().put("decoratedImageFor"+reference.eClass().getName(), img);
+			}
+			return img;
+		}
+		return icon;
+	}
+
+	@Override
+	public String getProposalLabel(Expression expression) {
+		return expression.getName();
+	}
 
 
 
-    private Expression createExpression(Data d) {
-        Expression exp = ExpressionFactory.eINSTANCE.createExpression() ;
-        exp.setType(getExpressionType()) ;
-        exp.setContent(d.getName()) ;
-        exp.setName(d.getName()) ;
-        exp.setReturnType(org.bonitasoft.studio.common.DataUtil.getTechnicalTypeFor(d)) ;
-        exp.getReferencedElements().add(EcoreUtil.copy(d)) ;
-        return exp;
-    }
+	private Expression createExpression(Data d) {
+		Expression exp = ExpressionFactory.eINSTANCE.createExpression() ;
+		exp.setType(getExpressionType()) ;
+		exp.setContent(d.getName()) ;
+		exp.setName(d.getName()) ;
+		exp.setReturnType(org.bonitasoft.studio.common.DataUtil.getTechnicalTypeFor(d)) ;
+		exp.getReferencedElements().add(EcoreUtil.copy(d)) ;
+		return exp;
+	}
 
-    @Override
-    public boolean isRelevantFor(EObject context) {
-        return true;
-    }
+	@Override
+	public boolean isRelevantFor(EObject context) {
+		return true;
+	}
 
-    @Override
-    public Image getTypeIcon() {
-        return Pics.getImage(PicsConstants.data);
-    }
+	@Override
+	public Image getTypeIcon() {
+		return Pics.getImage(PicsConstants.data);
+	}
 
-    @Override
-    public String getTypeLabel() {
-        return Messages.variableType;
-    }
+	@Override
+	public String getTypeLabel() {
+		return Messages.variableType;
+	}
 
-    @Override
-    public IExpressionEditor getExpressionEditor(Expression expression,EObject context) {
-        return new DataExpressionEditor();
-    }
+	@Override
+	public IExpressionEditor getExpressionEditor(Expression expression,EObject context) {
+		return new DataExpressionEditor();
+	}
 
 
 
