c199b7069b42869769755cf254c603fd57f2a138
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-06-06 14:01:45 +0000

    STUDIO-2343

--- plugins/org.bonitasoft.studio.diagram.form.properties/src/org/bonitasoft/studio/properties/form/sections/actions/contributions/OutputSectionContribution.java
+++ plugins/org.bonitasoft.studio.diagram.form.properties/src/org/bonitasoft/studio/properties/form/sections/actions/contributions/OutputSectionContribution.java
@@ -28,15 +28,20 @@ import org.bonitasoft.studio.model.expression.Expression;
 import org.bonitasoft.studio.model.expression.ExpressionFactory;
 import org.bonitasoft.studio.model.expression.Operation;
 import org.bonitasoft.studio.model.expression.Operator;
+import org.bonitasoft.studio.model.form.FileWidget;
 import org.bonitasoft.studio.model.form.Form;
 import org.bonitasoft.studio.model.form.FormField;
 import org.bonitasoft.studio.model.form.FormPackage;
 import org.bonitasoft.studio.model.form.ViewForm;
 import org.bonitasoft.studio.model.form.Widget;
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.conversion.Converter;
 import org.eclipse.emf.databinding.EMFDataBindingContext;
+import org.eclipse.emf.databinding.edit.EMFEditObservables;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.ISelection;
@@ -49,111 +54,120 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
  */
 public class OutputSectionContribution implements IExtensibleGridPropertySectionContribution {
 
-    protected Widget element;
-    protected TransactionalEditingDomain editingDomain;
-    protected EMFDataBindingContext dataBinding;
-    private OperationViewer operationViewer;
+	protected Widget element;
+	protected TransactionalEditingDomain editingDomain;
+	protected EMFDataBindingContext dataBinding;
+	private OperationViewer operationViewer;
+
+	public void createControl(Composite composite, TabbedPropertySheetWidgetFactory widgetFactory, ExtensibleGridPropertySection extensibleGridPropertySection) {
+		composite.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create());
+		composite.setLayout(GridLayoutFactory.fillDefaults().numColumns(1).create());
+
+		AvailableExpressionTypeFilter expressionFilter =  new AvailableExpressionTypeFilter(new String[]{
+				ExpressionConstants.CONSTANT_TYPE,
+				ExpressionConstants.VARIABLE_TYPE,
+				ExpressionConstants.SCRIPT_TYPE,
+				ExpressionConstants.PARAMETER_TYPE,
+				ExpressionConstants.FORM_FIELD_TYPE,
+				ExpressionConstants.DOCUMENT_TYPE
+		}) ;
+
+		AvailableExpressionTypeFilter storageExpressionFilter =  new AvailableExpressionTypeFilter(new String[]{
+				ExpressionConstants.VARIABLE_TYPE,
+				ExpressionConstants.DOCUMENT_REF_TYPE
+		}) ;
+
+		operationViewer = new OperationViewer(composite, widgetFactory,getEditingDomain(), expressionFilter, storageExpressionFilter) ;
+		operationViewer.setStorageExpressionNatureProvider(new DataExpressionNatureProviderForFormOutput());
+		operationViewer.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create()) ;
+
+		bindWidgets();
+
+	}
 
 
 
-    public void createControl(Composite composite, TabbedPropertySheetWidgetFactory widgetFactory, ExtensibleGridPropertySection extensibleGridPropertySection) {
-        composite.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create());
-        composite.setLayout(GridLayoutFactory.fillDefaults().numColumns(1).create());
+	protected TransactionalEditingDomain getEditingDomain() {
+		return editingDomain;
+	}
 
-    	AvailableExpressionTypeFilter expressionFilter =  new AvailableExpressionTypeFilter(new String[]{
-    			ExpressionConstants.CONSTANT_TYPE,
-    			ExpressionConstants.VARIABLE_TYPE,
-    			ExpressionConstants.SCRIPT_TYPE,
-    			ExpressionConstants.PARAMETER_TYPE,
-    			ExpressionConstants.FORM_FIELD_TYPE,
-    			ExpressionConstants.DOCUMENT_TYPE
-        }) ;
-    	
-     	AvailableExpressionTypeFilter storageExpressionFilter =  new AvailableExpressionTypeFilter(new String[]{
-    			ExpressionConstants.VARIABLE_TYPE,
-    			ExpressionConstants.DOCUMENT_REF_TYPE
-        }) ;
+	protected void bindWidgets() {
+		if(operationViewer != null && !operationViewer.isDisposed()){
+			if(dataBinding != null){
+				dataBinding.dispose();
+			}
+			dataBinding = new EMFDataBindingContext();
+			operationViewer.setContext(dataBinding) ;
+			Operation action = element.getAction() ;
+			if(action == null){
+				action = ExpressionFactory.eINSTANCE.createOperation() ;
+				Operator op = ExpressionFactory.eINSTANCE.createOperator() ;
+				op.setType(ExpressionConstants.ASSIGNMENT_OPERATOR) ;
+				op.setExpression("=") ;
+				action.setOperator(op) ;
 
-        operationViewer = new OperationViewer(composite, widgetFactory,getEditingDomain(), expressionFilter, storageExpressionFilter) ;
-        operationViewer.setStorageExpressionNatureProvider(new DataExpressionNatureProviderForFormOutput());
-        operationViewer.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create()) ;
-
-        bindWidgets();
-    }
-
-
-
-    protected TransactionalEditingDomain getEditingDomain() {
-        return editingDomain;
-    }
-
-    protected void bindWidgets() {
-        if(operationViewer != null && !operationViewer.isDisposed()){
-            if(dataBinding != null){
-                dataBinding.dispose();
-            }
-            dataBinding = new EMFDataBindingContext();
-            operationViewer.setContext(dataBinding) ;
-            Operation action = element.getAction() ;
-            if(action == null){
-                action = ExpressionFactory.eINSTANCE.createOperation() ;
-                Operator op = ExpressionFactory.eINSTANCE.createOperator() ;
-                op.setType(ExpressionConstants.ASSIGNMENT_OPERATOR) ;
-                op.setExpression("=") ;
-                action.setOperator(op) ;
-
-                Expression variableExp = ExpressionFactory.eINSTANCE.createExpression() ;
-                Expression actionExp = ExpressionFactory.eINSTANCE.createExpression() ;
-                action.setLeftOperand(variableExp) ;
-                action.setRightOperand(actionExp) ;
-                editingDomain.getCommandStack().execute(SetCommand.create(editingDomain, element, FormPackage.Literals.WIDGET__ACTION, action));
-            }
-            operationViewer.setEditingDomain(getEditingDomain()) ;
-            operationViewer.setEObject(element) ;
-        }
-    }
+				Expression variableExp = ExpressionFactory.eINSTANCE.createExpression() ;
+				Expression actionExp = ExpressionFactory.eINSTANCE.createExpression() ;
+				action.setLeftOperand(variableExp) ;
+				action.setRightOperand(actionExp) ;
+				editingDomain.getCommandStack().execute(SetCommand.create(editingDomain, element, FormPackage.Literals.WIDGET__ACTION, action));
+			}
+			operationViewer.setEditingDomain(getEditingDomain()) ;
+			operationViewer.setEObject(element) ;
+			if(element instanceof FileWidget){
+				UpdateValueStrategy strategy = new UpdateValueStrategy();
+				strategy.setConverter(new Converter(Boolean.class,Boolean.class){
+					public Object convert(Object fromObject) {
+						return !((Boolean)fromObject).booleanValue();
+					}
+				});
+				dataBinding.bindValue(SWTObservables.observeVisible(ExtensibleGridPropertySection.getLabelCompositeOf(operationViewer.getParent())), EMFEditObservables.observeValue(getEditingDomain(), element, FormPackage.Literals.FILE_WIDGET__DOWNLOAD_ONLY),strategy,strategy);
+				dataBinding.bindValue(SWTObservables.observeVisible(operationViewer.getParent()), EMFEditObservables.observeValue(getEditingDomain(), element, FormPackage.Literals.FILE_WIDGET__DOWNLOAD_ONLY),strategy,strategy);
+			}
+		}
+	}
 
 
-    public void dispose() {
-        if(dataBinding!= null){
-            dataBinding.dispose();
-        }
-        if(operationViewer != null){
-            operationViewer.dispose() ;
-        }
-    }
+	public void dispose() {
+		if(dataBinding!= null){
+			dataBinding.dispose();
+		}
+		if(operationViewer != null){
+			operationViewer.dispose() ;
+		}
+	}
 
-    public String getLabel() {
-        return Messages.outputOperation;
-    }
+	public String getLabel() {
+		return Messages.outputOperation;
+	}
 
-    public boolean isRelevantFor(EObject eObject) {
-        if(eObject instanceof FormField && ! ModelHelper.isInDuplicatedGrp(eObject)){
-            Form form = ModelHelper.getForm((Widget) eObject);
-            return !(form instanceof ViewForm);
-        }else{
-            return false;
-        }
+	public boolean isRelevantFor(EObject eObject) {
+		if(eObject instanceof FormField && ! ModelHelper.isInDuplicatedGrp(eObject)){
+			Form form = ModelHelper.getForm((Widget) eObject);
+			return !(form instanceof ViewForm);
+		}else{
+			return false;
+		}
 
-    }
+	}
 
 
-    public void setEObject(EObject object) {
-        element = (Widget) object;
-    }
+	public void setEObject(EObject object) {
+		element = (Widget) object;
+	}
 
-    public void setEditingDomain(TransactionalEditingDomain editingDomain) {
-        this.editingDomain = editingDomain;
-    }
+	public void setEditingDomain(TransactionalEditingDomain editingDomain) {
+		this.editingDomain = editingDomain;
+	}
 
-    public void setSelection(ISelection selection) {
+	public void setSelection(ISelection selection) {
 
-    }
+	}
 
 
 
 	public void refresh() {
-	
+
 	}
 
 }
