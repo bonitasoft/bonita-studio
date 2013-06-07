959ddb3c757227edbc8e9a75405226a4e5f39d87
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-06-07 06:51:35 +0000

    fix migration issues

--- plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/version/UpdateModelVersion.java
+++ plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/version/UpdateModelVersion.java
@@ -21,6 +21,7 @@ import org.bonitasoft.studio.common.ModelVersion;
 import org.bonitasoft.studio.common.ProductVersion;
 import org.bonitasoft.studio.model.process.MainProcess;
 import org.bonitasoft.studio.model.process.ProcessFactory;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edapt.migration.CustomMigration;
 import org.eclipse.emf.edapt.migration.Instance;
 import org.eclipse.emf.edapt.migration.Metamodel;
@@ -37,15 +38,38 @@ public class UpdateModelVersion extends CustomMigration {
 	public void migrateAfter(Model model, Metamodel metamodel)
 			throws MigrationException {
 		for(Instance mainProc : model.getAllInstances("process.MainProcess")){
+			final EStructuralFeature f = mainProc.getEClass().getEStructuralFeature("configId");
+			MainProcess currentDiagram = null;
+			if(f != null){
+				String oldName = mainProc.get("name");
+				String oldVersion = mainProc.get("version");
+				String oldBonitaModelVersion = mainProc.get("bonitaModelVersion");
+				String oldProductVersion = mainProc.get("bonitaVersion");
+				String oldConfigId = mainProc.get("configId");
+				if(!ProductVersion.CURRENT_VERSION.equals(oldProductVersion) || !ModelVersion.CURRENT_VERSION.equals(oldBonitaModelVersion)){
+					currentDiagram = ProcessFactory.eINSTANCE.createMainProcess();
+					currentDiagram.setName(oldName);
+					currentDiagram.setVersion(oldVersion);
+					currentDiagram.setBonitaModelVersion(oldBonitaModelVersion);
+					currentDiagram.setBonitaVersion(oldProductVersion);
+					currentDiagram.setConfigId(oldConfigId);
+				}
+			}
 			mainProc.set("bonitaVersion", ProductVersion.CURRENT_VERSION);
 			mainProc.set("bonitaModelVersion", ModelVersion.CURRENT_VERSION);
-			final MainProcess diagram = ProcessFactory.eINSTANCE.createMainProcess();
-			diagram.setName((String) mainProc.get("name"));
-			diagram.setVersion((String) mainProc.get("version"));
-			diagram.setBonitaModelVersion(ModelVersion.CURRENT_VERSION);
-			diagram.setBonitaVersion(ProductVersion.CURRENT_VERSION);
-			mainProc.set("configId",ConfigurationIdProvider.getConfigurationIdProvider().getConfigurationId(diagram));
+
+
+			if(currentDiagram != null){
+				if(ConfigurationIdProvider.getConfigurationIdProvider().isConfigurationIdValid(currentDiagram)){
+					final MainProcess diagram = ProcessFactory.eINSTANCE.createMainProcess();
+					diagram.setName((String) mainProc.get("name"));
+					diagram.setVersion((String) mainProc.get("version"));
+					diagram.setBonitaModelVersion(ModelVersion.CURRENT_VERSION);
+					diagram.setBonitaVersion(ProductVersion.CURRENT_VERSION);
+					mainProc.set("configId",ConfigurationIdProvider.getConfigurationIdProvider().getConfigurationId(diagram));
+				}
+			}
 		}
 	}
-	
+
 }
