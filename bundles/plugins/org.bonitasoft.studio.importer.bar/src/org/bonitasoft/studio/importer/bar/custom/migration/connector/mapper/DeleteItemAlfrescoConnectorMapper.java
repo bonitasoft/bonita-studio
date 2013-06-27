7271272431af947650067cd64dfe479ea763940f
Arelie Zara <aurelie.zara@bonitasoft.com>
2013-06-27 09:41:35 +0000

    STUDIO-2452 Migrate Alfresco connector configurations from 5.x to 6.x

---
+++ bundles/plugins/org.bonitasoft.studio.importer.bar/src/org/bonitasoft/studio/importer/bar/custom/migration/connector/mapper/DeleteItemAlfrescoConnectorMapper.java
@@ -0,0 +1,22 @@
+package org.bonitasoft.studio.importer.bar.custom.migration.connector.mapper;
+
+import org.bonitasoft.studio.connectors.extension.AbstractConnectorDefinitionMapper;
+import org.bonitasoft.studio.connectors.extension.IConnectorDefinitionMapper;
+
+public class DeleteItemAlfrescoConnectorMapper extends
+		AbstractConnectorDefinitionMapper implements IConnectorDefinitionMapper {
+
+	private static final String DELETE_ITEM_ALFRESCO_ID = "Alfresco34DeleteItemById";
+	private static final String LEGACY_DELETE_ITEM_ALFRESCO_ID="AlfrescoDeleteItemById";
+	
+	@Override
+	public String getDefinitionId() {
+		return DELETE_ITEM_ALFRESCO_ID;
+	}
+
+	@Override
+	public String getLegacyConnectorId() {
+		return LEGACY_DELETE_ITEM_ALFRESCO_ID;
+	}
+
+}
