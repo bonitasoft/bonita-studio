f95cb7123f892a11159020d7aa023f079e5512ef
Arelie Zara <aurelie.zara@bonitasoft.com>
2013-06-27 13:09:24 +0000

    STUDIO-695 It should be possible to change actor filter when one actor filter is already selected

--- bundles/plugins/org.bonitasoft.studio.actors/src/org/bonitasoft/studio/actors/ui/section/AbstractActorsPropertySection.java
+++ bundles/plugins/org.bonitasoft.studio.actors/src/org/bonitasoft/studio/actors/ui/section/AbstractActorsPropertySection.java
@@ -75,83 +75,83 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
  */
 public abstract class AbstractActorsPropertySection extends AbstractBonitaDescriptionSection implements ISelectionChangedListener{
 
-    protected ComboViewer actorComboViewer;
-    protected EMFDataBindingContext emfDatabindingContext;
-    protected Text filterText;
-    protected ToolItem removeConnectorButton;
-    protected Button updateConnectorButton;
-    private StyledFilterLabelProvider filterLabelProvider;
-    protected Button setButton;
+	protected ComboViewer actorComboViewer;
+	protected EMFDataBindingContext emfDatabindingContext;
+	protected Text filterText;
+	protected ToolItem removeConnectorButton;
+	protected Button updateConnectorButton;
+	private StyledFilterLabelProvider filterLabelProvider;
+	protected Button setButton;
 
-    @Override
-    public void createControls(Composite parent,TabbedPropertySheetPage aTabbedPropertySheetPage) {
-        super.createControls(parent, aTabbedPropertySheetPage) ;
-        TabbedPropertySheetWidgetFactory widgetFactory = aTabbedPropertySheetPage.getWidgetFactory() ;
-        Composite mainComposite = widgetFactory.createComposite(parent, SWT.NONE) ;
-        mainComposite.setLayout(GridLayoutFactory.fillDefaults().numColumns(2).margins(10, 10).extendedMargins(0, 25, 0, 25).spacing(10, 15).create()) ;
-        mainComposite.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
-        createRadioComposite(widgetFactory, mainComposite);
+	@Override
+	public void createControls(Composite parent,TabbedPropertySheetPage aTabbedPropertySheetPage) {
+		super.createControls(parent, aTabbedPropertySheetPage) ;
+		TabbedPropertySheetWidgetFactory widgetFactory = aTabbedPropertySheetPage.getWidgetFactory() ;
+		Composite mainComposite = widgetFactory.createComposite(parent, SWT.NONE) ;
+		mainComposite.setLayout(GridLayoutFactory.fillDefaults().numColumns(2).margins(10, 10).extendedMargins(0, 25, 0, 25).spacing(10, 15).create()) ;
+		mainComposite.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+		createRadioComposite(widgetFactory, mainComposite);
 
-        Label actorsLabel = widgetFactory.createLabel(mainComposite, Messages.selectActor) ;
-        actorsLabel.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).create()) ;
+		Label actorsLabel = widgetFactory.createLabel(mainComposite, Messages.selectActor) ;
+		actorsLabel.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).create()) ;
 
-        actorComboViewer = new ComboViewer(mainComposite, SWT.SINGLE | SWT.BORDER | SWT.READ_ONLY) ;
-        actorComboViewer.getCombo().setLayoutData(GridDataFactory.fillDefaults().create()) ;
-        actorComboViewer.setContentProvider(new ArrayContentProvider()) ;
-        actorComboViewer.setLabelProvider(new LabelProvider(){
-            @Override
-            public String getText(Object element) {
-                if(element instanceof Actor){
-                    String doc = ((Actor) element).getDocumentation() ;
-                    if(doc != null && !doc.isEmpty()){
-                        doc = " -- "+ doc ;
-                    }else{
-                        doc = "" ;
-                    }
-                    return ((Actor) element).getName() + doc ;
-                }
-                return super.getText(element);
-            }
-        });
-
-
-        filterLabelProvider = new StyledFilterLabelProvider() ;
-        createFiltersViewer(mainComposite) ;
-
-        updateDatabinding() ;
-    }
+		actorComboViewer = new ComboViewer(mainComposite, SWT.SINGLE | SWT.BORDER | SWT.READ_ONLY) ;
+		actorComboViewer.getCombo().setLayoutData(GridDataFactory.fillDefaults().create()) ;
+		actorComboViewer.setContentProvider(new ArrayContentProvider()) ;
+		actorComboViewer.setLabelProvider(new LabelProvider(){
+			@Override
+			public String getText(Object element) {
+				if(element instanceof Actor){
+					String doc = ((Actor) element).getDocumentation() ;
+					if(doc != null && !doc.isEmpty()){
+						doc = " -- "+ doc ;
+					}else{
+						doc = "" ;
+					}
+					return ((Actor) element).getName() + doc ;
+				}
+				return super.getText(element);
+			}
+		});
 
 
+		filterLabelProvider = new StyledFilterLabelProvider() ;
+		createFiltersViewer(mainComposite) ;
 
-    protected void createFiltersViewer(Composite parent) {
-
-        final Label actorFilters = getWidgetFactory().createLabel(parent, Messages.actorFilter, SWT.NONE) ;
-        actorFilters.setLayoutData(GridDataFactory.fillDefaults().align(SWT.END, SWT.CENTER).create()) ;
-
-        final Composite viewerComposite = getWidgetFactory().createPlainComposite(parent, SWT.NONE);
-        viewerComposite.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create()) ;
-        viewerComposite.setLayout(GridLayoutFactory.fillDefaults().numColumns(4).margins(0, 0).create()) ;
-
-
-        setButton = createSetButton(viewerComposite);
-
-        filterText = getWidgetFactory().createText(viewerComposite,"", SWT.BORDER | SWT.SINGLE | SWT.NO_FOCUS | SWT.READ_ONLY);
-        filterText.setLayoutData(GridDataFactory.swtDefaults().align(SWT.FILL,SWT.CENTER).grab(true,false).create());
-
-        updateConnectorButton = createUpdateButton(viewerComposite);
-        removeConnectorButton = createRemoveButton(viewerComposite);
-    }
+		updateDatabinding() ;
+	}
 
 
 
-    protected EStructuralFeature getFilterFeature() {
-        return ProcessPackage.Literals.ASSIGNABLE__FILTERS ;
-    }
+	protected void createFiltersViewer(Composite parent) {
+
+		final Label actorFilters = getWidgetFactory().createLabel(parent, Messages.actorFilter, SWT.NONE) ;
+		actorFilters.setLayoutData(GridDataFactory.fillDefaults().align(SWT.END, SWT.CENTER).create()) ;
+
+		final Composite viewerComposite = getWidgetFactory().createPlainComposite(parent, SWT.NONE);
+		viewerComposite.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create()) ;
+		viewerComposite.setLayout(GridLayoutFactory.fillDefaults().numColumns(4).margins(0, 0).create()) ;
+
+
+		setButton = createSetButton(viewerComposite);
+
+		filterText = getWidgetFactory().createText(viewerComposite,"", SWT.BORDER | SWT.SINGLE | SWT.NO_FOCUS | SWT.READ_ONLY);
+		filterText.setLayoutData(GridDataFactory.swtDefaults().align(SWT.FILL,SWT.CENTER).grab(true,false).create());
+
+		updateConnectorButton = createUpdateButton(viewerComposite);
+		removeConnectorButton = createRemoveButton(viewerComposite);
+	}
+
+
+
+	protected EStructuralFeature getFilterFeature() {
+		return ProcessPackage.Literals.ASSIGNABLE__FILTERS ;
+	}
 
 
 	protected ToolItem createRemoveButton(Composite buttonsComposite) {
 		ToolBar toolBar = new ToolBar(buttonsComposite, SWT.FLAT);
-//		getWidgetFactory().adapt(toolBar);
+		//		getWidgetFactory().adapt(toolBar);
 		ToolItem toolItem = new ToolItem(toolBar, SWT.FLAT);
 		toolItem.setImage(Pics.getImage(PicsConstants.clear));
 		toolItem.setToolTipText(Messages.remove);
@@ -182,147 +182,152 @@ public abstract class AbstractActorsPropertySection extends AbstractBonitaDescri
 	}
 
 
-    protected Button createUpdateButton(Composite buttonsComposite) {
-        Button updateButton = getWidgetFactory().createButton(buttonsComposite, Messages.edit, SWT.FLAT);
-        updateButton.setLayoutData(GridDataFactory.fillDefaults().hint(85,SWT.DEFAULT).create()) ;
-        updateButton.addListener(SWT.Selection, new Listener() {
+	protected Button createUpdateButton(Composite buttonsComposite) {
+		Button updateButton = getWidgetFactory().createButton(buttonsComposite, Messages.edit, SWT.FLAT);
+		updateButton.setLayoutData(GridDataFactory.fillDefaults().hint(85,SWT.DEFAULT).create()) ;
+		updateButton.addListener(SWT.Selection, new Listener() {
 
-            @Override
-            public void handleEvent(Event event) {
-                Assignable assignable = (Assignable) getEObject() ;
-                ActorFilter filter = assignable.getFilters().get(0) ;
-                ActorFilterDefRepositoryStore defStore = (ActorFilterDefRepositoryStore) RepositoryManager.getInstance().getRepositoryStore(ActorFilterDefRepositoryStore.class) ;
-                ConnectorDefinition def = defStore.getDefinition(filter.getDefinitionId(),filter.getDefinitionVersion()) ;
-                if(def != null){
-                    WizardDialog wizardDialog = new ActorFilterDefinitionWizardDialog(Display.getCurrent().getActiveShell(), new FilterWizard(filter,getFilterFeature(),getFilterFeatureToCheckUniqueID()));
-                    if(wizardDialog.open() == Dialog.OK){
-                        Assignable newAssignable = (Assignable) getEObject() ;
-                        ActorFilter newfilter = newAssignable.getFilters().get(0) ;
-                        filterText.setText(filterLabelProvider.getText(newfilter) ) ;
-                        updateButtons() ;
-                    }
-                }
-            }
+			@Override
+			public void handleEvent(Event event) {
+				Assignable assignable = (Assignable) getEObject() ;
+				ActorFilter filter = assignable.getFilters().get(0) ;
+				ActorFilterDefRepositoryStore defStore = (ActorFilterDefRepositoryStore) RepositoryManager.getInstance().getRepositoryStore(ActorFilterDefRepositoryStore.class) ;
+				ConnectorDefinition def = defStore.getDefinition(filter.getDefinitionId(),filter.getDefinitionVersion()) ;
+				if(def != null){
+					WizardDialog wizardDialog = new ActorFilterDefinitionWizardDialog(Display.getCurrent().getActiveShell(), new FilterWizard(filter,getFilterFeature(),getFilterFeatureToCheckUniqueID()));
+					if(wizardDialog.open() == Dialog.OK){
+						Assignable newAssignable = (Assignable) getEObject() ;
+						ActorFilter newfilter = newAssignable.getFilters().get(0) ;
+						filterText.setText(filterLabelProvider.getText(newfilter) ) ;
+						updateButtons() ;
+					}
+				}
+			}
 
-        });
-        return updateButton;
-    }
+		});
+		return updateButton;
+	}
 
-    protected Set<EStructuralFeature> getFilterFeatureToCheckUniqueID() {
-        Set<EStructuralFeature> res = new HashSet<EStructuralFeature>();
-        res.add(getFilterFeature());
-        return res;
-    }
-
-
-
-    protected Button createSetButton(Composite buttonsComposite) {
-        final Button setButton = getWidgetFactory().createButton(buttonsComposite, Messages.set, SWT.FLAT);
-        setButton.setLayoutData(GridDataFactory.fillDefaults().hint(85, SWT.DEFAULT).create()) ;
-        setButton.addSelectionListener(new SelectionListener() {
-
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                WizardDialog wizardDialog = new ActorFilterDefinitionWizardDialog(Display.getCurrent().getActiveShell(), new FilterWizard(getEObject(), getFilterFeature(), getFilterFeatureToCheckUniqueID()));
-                if(wizardDialog.open() == Dialog.OK){
-                    Assignable assignable = (Assignable) getEObject() ;
-                    ActorFilter filter = assignable.getFilters().get(0) ;
-                    filterText.setText(filterLabelProvider.getText(filter)) ;
-                    updateButtons() ;
-                }
-            }
-
-            @Override
-            public void widgetDefaultSelected(SelectionEvent e) {
-
-            }
-        });
-        return setButton ;
-    }
+	protected Set<EStructuralFeature> getFilterFeatureToCheckUniqueID() {
+		Set<EStructuralFeature> res = new HashSet<EStructuralFeature>();
+		res.add(getFilterFeature());
+		return res;
+	}
 
 
 
-    protected abstract void createRadioComposite(TabbedPropertySheetWidgetFactory widgetFactory,Composite mainComposite) ;
+	protected Button createSetButton(Composite buttonsComposite) {
+		final Button setButton = getWidgetFactory().createButton(buttonsComposite, Messages.set, SWT.FLAT);
+		setButton.setLayoutData(GridDataFactory.fillDefaults().hint(85, SWT.DEFAULT).create()) ;
+		setButton.addSelectionListener(new SelectionListener() {
 
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				WizardDialog wizardDialog = new ActorFilterDefinitionWizardDialog(Display.getCurrent().getActiveShell(), new FilterWizard(getEObject(), getFilterFeature(), getFilterFeatureToCheckUniqueID()));
+				if(wizardDialog.open() == Dialog.OK){
+					Assignable assignable = (Assignable) getEObject() ;
+					if (assignable.getFilters().size()>1){
+						getEditingDomain().getCommandStack().execute(RemoveCommand.create(getEditingDomain(),assignable,assignable.getFilters(),assignable.getFilters().get(0)));
+					}
+					if (!assignable.getFilters().isEmpty()){
+						ActorFilter filter = assignable.getFilters().get(0) ;
+						filterText.setText(filterLabelProvider.getText(filter)) ;
+					}
+					updateButtons() ;
+				}
+			}
 
-    @Override
-    public void refresh() {
-        super.refresh();
-        if(filterText != null && !filterText.isDisposed()){
-            Assignable assignable = (Assignable) getEObject() ;
-            if(assignable != null && !assignable.getFilters().isEmpty()){
-                ActorFilter filter = assignable.getFilters().get(0) ;
-                filterText.setText(filterLabelProvider.getText(filter) ) ;
-            }else{
-                filterText.setText("") ;
-            }
-            updateButtons();
-        }
-        updateDatabinding() ;
-    }
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
 
-
-    protected void updateDatabinding() {
-        Assignable assignable = (Assignable) getEObject() ;
-        if(assignable != null){
-            if(emfDatabindingContext != null){
-                emfDatabindingContext.dispose() ;
-            }
-            emfDatabindingContext = new EMFDataBindingContext() ;
-            AbstractProcess process = ModelHelper.getParentProcess(assignable) ;
-            emfDatabindingContext.bindValue(ViewersObservables.observeInput(actorComboViewer), EMFObservables.observeValue(process, ProcessPackage.Literals.ABSTRACT_PROCESS__ACTORS)) ;
-            emfDatabindingContext.bindValue(ViewersObservables.observeSingleSelection(actorComboViewer), EMFEditObservables.observeValue(getEditingDomain(),assignable, ProcessPackage.Literals.ASSIGNABLE__ACTOR)) ;
-        }
-    }
+			}
+		});
+		return setButton ;
+	}
 
 
 
-    @Override
-    public void selectionChanged(SelectionChangedEvent event) {
-        updateButtons() ;
-    }
+	protected abstract void createRadioComposite(TabbedPropertySheetWidgetFactory widgetFactory,Composite mainComposite) ;
+
+
+	@Override
+	public void refresh() {
+		super.refresh();
+		if(filterText != null && !filterText.isDisposed()){
+			Assignable assignable = (Assignable) getEObject() ;
+			if(assignable != null && !assignable.getFilters().isEmpty()){
+				ActorFilter filter = assignable.getFilters().get(0) ;
+				filterText.setText(filterLabelProvider.getText(filter) ) ;
+			}else{
+				filterText.setText("") ;
+			}
+			updateButtons();
+		}
+		updateDatabinding() ;
+	}
+
+
+	protected void updateDatabinding() {
+		Assignable assignable = (Assignable) getEObject() ;
+		if(assignable != null){
+			if(emfDatabindingContext != null){
+				emfDatabindingContext.dispose() ;
+			}
+			emfDatabindingContext = new EMFDataBindingContext() ;
+			AbstractProcess process = ModelHelper.getParentProcess(assignable) ;
+			emfDatabindingContext.bindValue(ViewersObservables.observeInput(actorComboViewer), EMFObservables.observeValue(process, ProcessPackage.Literals.ABSTRACT_PROCESS__ACTORS)) ;
+			emfDatabindingContext.bindValue(ViewersObservables.observeSingleSelection(actorComboViewer), EMFEditObservables.observeValue(getEditingDomain(),assignable, ProcessPackage.Literals.ASSIGNABLE__ACTOR)) ;
+		}
+	}
 
 
 
-    private void updateButtons() {
-        if (filterText != null) {
-            Assignable assignable = (Assignable) getEObject() ;
-            ActorFilter filter = null ;
-            if(!assignable.getFilters().isEmpty()){
-                filter = assignable.getFilters().get(0) ;
-            }
+	@Override
+	public void selectionChanged(SelectionChangedEvent event) {
+		updateButtons() ;
+	}
 
-            if(!setButton.isDisposed()){
-                setButton.setEnabled(filter == null) ;
-            }
 
-            if (!removeConnectorButton.isDisposed()) {
-                removeConnectorButton.setEnabled(filter != null);
-            }
 
-            if(!updateConnectorButton.isDisposed()){
-                if(filter != null){
-                    ActorFilterDefRepositoryStore connectorDefStore = (ActorFilterDefRepositoryStore) RepositoryManager.getInstance().getRepositoryStore(ActorFilterDefRepositoryStore.class) ;
-                    ConnectorDefinition def = connectorDefStore.getDefinition(filter.getDefinitionId(),filter.getDefinitionVersion()) ;
-                    updateConnectorButton.setEnabled(def!= null) ;
-                }else{
-                    updateConnectorButton.setEnabled(false) ;
-                }
+	private void updateButtons() {
+		if (filterText != null) {
+			Assignable assignable = (Assignable) getEObject() ;
+			ActorFilter filter = null ;
+			if(!assignable.getFilters().isEmpty()){
+				filter = assignable.getFilters().get(0) ;
+			}
 
-            }
-        }
-    }
+			//       if(!setButton.isDisposed()){
+				//           setButton.setEnabled(filter == null) ;
+			//       }
 
-    
-    @Override
+			if (!removeConnectorButton.isDisposed()) {
+				removeConnectorButton.setEnabled(filter != null);
+			}
+
+			if(!updateConnectorButton.isDisposed()){
+				if(filter != null){
+					ActorFilterDefRepositoryStore connectorDefStore = (ActorFilterDefRepositoryStore) RepositoryManager.getInstance().getRepositoryStore(ActorFilterDefRepositoryStore.class) ;
+					ConnectorDefinition def = connectorDefStore.getDefinition(filter.getDefinitionId(),filter.getDefinitionVersion()) ;
+					updateConnectorButton.setEnabled(def!= null) ;
+				}else{
+					updateConnectorButton.setEnabled(false) ;
+				}
+
+			}
+		}
+	}
+
+
+	@Override
 	public String getSectionDescription() {
-    	final EObject selectedEobject = getEObject();
+		final EObject selectedEobject = getEObject();
 		if(selectedEobject instanceof AbstractProcess){
-    		return Messages.addRemoveActors;
-    	} else if(selectedEobject instanceof Lane){
-    		return Messages.actorDescriptionLane;
-    	} else {
-    		return Messages.actorDescriptionTask;
-    	}
+			return Messages.addRemoveActors;
+		} else if(selectedEobject instanceof Lane){
+			return Messages.actorDescriptionLane;
+		} else {
+			return Messages.actorDescriptionTask;
+		}
 	}
 }
