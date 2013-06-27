4dd86560787d018b253fbbe101cbd65b08a2a38d
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-06-27 15:38:24 +0000

    STUDIO-2475

--- plugins/org.bonitasoft.studio.properties/src/org/bonitasoft/studio/properties/sections/general/NameGridPropertySectionContribution.java
+++ plugins/org.bonitasoft.studio.properties/src/org/bonitasoft/studio/properties/sections/general/NameGridPropertySectionContribution.java
@@ -33,6 +33,7 @@ import org.bonitasoft.studio.common.OpenNameAndVersionForDiagramDialog.Processes
 import org.bonitasoft.studio.common.emf.tools.ExpressionHelper;
 import org.bonitasoft.studio.common.emf.tools.ModelHelper;
 import org.bonitasoft.studio.common.jface.databinding.validator.InputLengthValidator;
+import org.bonitasoft.studio.common.jface.databinding.validator.SpecialCharactersValidator;
 import org.bonitasoft.studio.common.jface.databinding.validator.URLEncodableInputValidator;
 import org.bonitasoft.studio.common.jface.databinding.validator.UTF8InputValidator;
 import org.bonitasoft.studio.common.log.BonitaStudioLog;
@@ -243,7 +244,8 @@ public class NameGridPropertySectionContribution extends AbstractNamePropertySec
 		labelTargetToModelUpdate = new UpdateValueStrategy();
 		labelTargetToModelUpdate.setAfterGetValidator(new UTF8InputValidator(Messages.name)) ;
 		labelTargetToModelUpdate.setBeforeSetValidator(new InputLengthValidator(Messages.name, 50)) ;
-
+		labelTargetToModelUpdate.setAfterConvertValidator(new SpecialCharactersValidator());
+		
 		observable = SWTObservables.observeDelayedValue(400, SWTObservables.observeText(text, SWT.Modify));
 		observable.addValueChangeListener(new IValueChangeListener() {
 
