50290e0e01226816817b3e3733ce59a5adaf44f2
Maxence Raoux <maxence.raoux@bonitasoft.com>
2013-07-11 12:39:15 +0000

    STUDIO-2556

--- tests/org.bonitasoft.studio.importer.bar.tests/src/org/bonitasoft/studio/importer/bar/tests/connector/TestConnectorMigrationUseCase.java
+++ tests/org.bonitasoft.studio.importer.bar.tests/src/org/bonitasoft/studio/importer/bar/tests/connector/TestConnectorMigrationUseCase.java
@@ -417,4 +417,22 @@ public class TestConnectorMigrationUseCase {
 				mainProc, ProcessPackage.Literals.CONNECTOR);
 		assertEquals("Invalid number of connector", 1, connectors.size());
 	}
+	
+	@Test
+	public void testCMISDeleteObjectConnectorMigration() throws Exception {
+		final URL url = TestConnectorMigrationUseCase.class
+				.getResource("CMISDeleteObjectMigrationUseCase--1.0.bar");
+		final File migratedProc = BarImporterTestUtil.migrateBar(url);
+		assertNotNull("Fail to migrate bar file", migratedProc);
+		assertNotNull("Fail to migrate bar file", migratedProc.exists());
+		final Resource resource = BarImporterTestUtil
+				.assertIsLoadable(migratedProc);
+		final MainProcess mainProc = BarImporterTestUtil
+				.getMainProcess(resource);
+		final List<Connector> connectors = ModelHelper.getAllItemsOfType(
+				mainProc, ProcessPackage.Literals.CONNECTOR);
+		assertEquals("Invalid number of connector", 1, connectors.size());
+	}
 }
+
+
