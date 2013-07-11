89d6a81771a5284f25c07df050711e9a9ba6449e
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-07-11 15:35:52 +0000

    import custom connectors

---
+++ tests/org.bonitasoft.studio.importer.bar.tests/src/org/bonitasoft/studio/importer/bar/tests/connector/TestCustomConnectorMigrationUseCase.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (C) 2012 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.importer.bar.tests.connector;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+import org.bonitasoft.engine.bpm.connector.ConnectorEvent;
+import org.bonitasoft.studio.common.ExpressionConstants;
+import org.bonitasoft.studio.common.emf.tools.ModelHelper;
+import org.bonitasoft.studio.common.jface.FileActionDialog;
+import org.bonitasoft.studio.common.repository.RepositoryManager;
+import org.bonitasoft.studio.connectors.repository.ConnectorDefRepositoryStore;
+import org.bonitasoft.studio.importer.bar.tests.BarImporterTestUtil;
+import org.bonitasoft.studio.model.expression.AbstractExpression;
+import org.bonitasoft.studio.model.expression.Expression;
+import org.bonitasoft.studio.model.expression.Operator;
+import org.bonitasoft.studio.model.process.Connector;
+import org.bonitasoft.studio.model.process.MainProcess;
+import org.bonitasoft.studio.model.process.ProcessPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Romain Bioteau
+ * 
+ */
+public class TestCustomConnectorMigrationUseCase {
+
+	private static boolean disablepopup;
+
+	@BeforeClass
+	public static void disablePopup() {
+		disablepopup = FileActionDialog.getDisablePopup();
+		FileActionDialog.setDisablePopup(true);
+	}
+
+	@AfterClass
+	public static void resetdisablePopup() {
+		FileActionDialog.setDisablePopup(disablepopup);
+	}
+
+	@Test
+	public void testCustomonnectorMigration() throws Exception {
+		final URL url = TestCustomConnectorMigrationUseCase.class
+				.getResource("CustomConnectorMigrationUseCase--1.0.bar");
+		final File migratedProc = BarImporterTestUtil.migrateBar(url);
+		assertNotNull("Fail to migrate bar file", migratedProc);
+		assertNotNull("Fail to migrate bar file", migratedProc.exists());
+		final Resource resource = BarImporterTestUtil
+				.assertIsLoadable(migratedProc);
+		final MainProcess mainProc = BarImporterTestUtil
+				.getMainProcess(resource);
+
+		final ConnectorDefRepositoryStore store = (ConnectorDefRepositoryStore) RepositoryManager.getInstance().getRepositoryStore(ConnectorDefRepositoryStore.class);
+		assertNotNull(store.getDefinition("MyConnectorId","1.0"));
+		
+		
+		final List<Connector> connectors = ModelHelper.getAllItemsOfType(
+				mainProc, ProcessPackage.Literals.CONNECTOR);
+		assertEquals("Invalid number of connector", 1, connectors.size());
+//		Connector c = connectors.get(0);
+//		assertEquals("grrovyConnector", c.getName());
+//		assertEquals(ConnectorEvent.ON_ENTER.name(), c.getEvent());
+//		assertEquals(1, c.getConfiguration().getParameters().size());
+//		final AbstractExpression expression = c.getConfiguration()
+//				.getParameters().get(0).getExpression();
+//		assertNotNull(expression);
+//		assertTrue(expression instanceof Expression);
+//		assertEquals(ExpressionConstants.SCRIPT_TYPE,
+//				((Expression) expression).getType());
+//		assertEquals(2, ((Expression) expression).getReferencedElements()
+//				.size());
+//
+//		assertEquals(1, c.getOutputs().size());
+//		Expression leftOp = c.getOutputs().get(0).getLeftOperand();
+//		Expression rightOp = c.getOutputs().get(0).getLeftOperand();
+//		Operator op = c.getOutputs().get(0).getOperator();
+//		assertNotNull(rightOp);
+//		assertNotNull(leftOp);
+//		assertNotNull(op);
+//
+//		assertFalse(leftOp.getContent().isEmpty());
+//		assertFalse(rightOp.getContent().isEmpty());
+//
+//		assertEquals(ExpressionConstants.ASSIGNMENT_OPERATOR, op.getType());
+//		assertEquals(ExpressionConstants.VARIABLE_TYPE, leftOp.getType());
+	}
+
+	
+
+}
