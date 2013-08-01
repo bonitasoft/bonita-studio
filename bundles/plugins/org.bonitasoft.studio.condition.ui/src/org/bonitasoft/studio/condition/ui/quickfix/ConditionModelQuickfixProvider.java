54eda5647a8f9de50fe7f962a90e7a77d3f116a5
mraoux <maxence.raoux@bonitasoft.com>
2013-08-01 17:34:51 +0200

    STUDIO-2419 Only warning message, no quickfix

--- bundles/plugins/org.bonitasoft.studio.condition.ui/src/org/bonitasoft/studio/condition/ui/quickfix/ConditionModelQuickfixProvider.java
+++ bundles/plugins/org.bonitasoft.studio.condition.ui/src/org/bonitasoft/studio/condition/ui/quickfix/ConditionModelQuickfixProvider.java
@@ -1,14 +1,6 @@
 package org.bonitasoft.studio.condition.ui.quickfix;
 
-import org.bonitasoft.studio.condition.validation.ConditionModelJavaValidator;
-import org.bonitasoft.studio.pics.PicsConstants;
-import org.eclipse.xtext.ui.editor.model.IXtextDocument;
-import org.eclipse.xtext.ui.editor.model.edit.IModification;
-import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
 import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
-import org.eclipse.xtext.ui.editor.quickfix.Fix;
-import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
-import org.eclipse.xtext.validation.Issue;
 
 /**
  * @author Maxence Raoux
@@ -16,16 +8,16 @@ import org.eclipse.xtext.validation.Issue;
  */
 public class ConditionModelQuickfixProvider extends DefaultQuickfixProvider {
 
-	@Fix(ConditionModelJavaValidator.INVALID_EQUALITY_SIGN)
-	public void fixInvalidEqualitySign(final Issue issue, IssueResolutionAcceptor acceptor) {
-		
-		acceptor.accept(issue, "Replace \"=\" by \"==\"", "", PicsConstants.edit, new IModification() {
-			@Override
-			public void apply(IModificationContext context) throws Exception {
-				IXtextDocument d = context.getXtextDocument();
-				//Modification go here
-			}
-		});
-	}
+//	@Fix(ConditionModelJavaValidator.INVALID_EQUALITY_SIGN)
+//	public void fixInvalidEqualitySign(final Issue issue, IssueResolutionAcceptor acceptor) {
+//		
+//		acceptor.accept(issue, "Replace \"=\" by \"==\"", "", PicsConstants.edit, new IModification() {
+//			@Override
+//			public void apply(IModificationContext context) throws Exception {
+//				IXtextDocument d = context.getXtextDocument();
+//				//Modification go here
+//			}
+//		});
+//	}
 
 }
