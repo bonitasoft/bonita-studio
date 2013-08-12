543e8f9fb5dfa928af8ddbc70895c3daafc7e2e6
azara <aurelie.zara@bonitasoft.com>
2013-08-12 18:16:57 +0200

    STUDIO-2071 Diagram validation should fail when a connector definition is not found (eg. on connector version update)

--- bundles/plugins/org.bonitasoft.studio.validation/src/org/bonitasoft/studio/validation/constraints/process/ConnectorExistenceConstraint.java
+++ bundles/plugins/org.bonitasoft.studio.validation/src/org/bonitasoft/studio/validation/constraints/process/ConnectorExistenceConstraint.java
@@ -1,29 +1,25 @@
 package org.bonitasoft.studio.validation.constraints.process;
 
-import java.util.List;
-
-import javax.net.ssl.SSLEngineResult.Status;
-
-import org.bonitasoft.studio.common.emf.tools.ModelHelper;
 import org.bonitasoft.studio.common.repository.RepositoryManager;
 import org.bonitasoft.studio.connector.model.definition.AbstractDefinitionRepositoryStore;
 import org.bonitasoft.studio.connector.model.definition.ConnectorDefinition;
 import org.bonitasoft.studio.connectors.repository.ConnectorDefRepositoryStore;
 import org.bonitasoft.studio.model.process.Connector;
-import org.bonitasoft.studio.model.process.PageFlow;
-import org.bonitasoft.studio.model.process.Pool;
 import org.bonitasoft.studio.validation.constraints.AbstractLiveValidationMarkerConstraint;
 import org.bonitasoft.studio.validation.i18n.Messages;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.IValidationContext;
 
 public class ConnectorExistenceConstraint extends
 		AbstractLiveValidationMarkerConstraint {
 
+
+
+	public static final String ID = "org.bonitasoft.studio.validation.constraints.connectorexistence";
+	
+	
 	@Override
 	protected IStatus performLiveValidation(IValidationContext context) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -42,8 +38,7 @@ public class ConnectorExistenceConstraint extends
 
 	@Override
 	protected String getConstraintId() {
-		
-		return "org.bonitasoft.studio.validation.constraints.connectorexistence";
+		return ID;
 	}
 
 }
