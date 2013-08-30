85b0de9c797d3e0867cd2a333b74528fa321d131
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-08-30 12:03:41 +0200

    STUDIO-2515

--- bundles/plugins/org.bonitasoft.studio.validators/src/org/bonitasoft/studio/validators/ui/handler/EditValidatorHandler.java
+++ bundles/plugins/org.bonitasoft.studio.validators/src/org/bonitasoft/studio/validators/ui/handler/EditValidatorHandler.java
@@ -24,6 +24,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 
 
 /**
@@ -32,19 +33,21 @@ import org.eclipse.swt.widgets.Display;
  */
 public class EditValidatorHandler extends AbstractHandler {
 
-    /* (non-Javadoc)
-     * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
-     */
-    @Override
-    public Object execute(ExecutionEvent event) throws ExecutionException {
-        SelectUserValidatorWizard selectWizard = new SelectUserValidatorWizard() ;
-        WizardDialog selectDialog = new WizardDialog(Display.getCurrent().getActiveShell(),selectWizard) ;
-        if(selectDialog.open() == Dialog.OK){
-            ValidatorWizard wizard = new ValidatorWizard(selectWizard.getValidatorDescriptor()) ;
-            WizardDialog wd = new WizardDialog(Display.getCurrent().getActiveShell(),wizard) ;
-            wd.open();
-        }
-        return null;
-    }
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		SelectUserValidatorWizard selectWizard = new SelectUserValidatorWizard() ;
+		WizardDialog selectDialog = new WizardDialog(Display.getCurrent().getActiveShell(),selectWizard) ;
+		if(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().closeAllEditors(true)){
+			if(selectDialog.open() == Dialog.OK){
+				ValidatorWizard wizard = new ValidatorWizard(selectWizard.getValidatorDescriptor()) ;
+				WizardDialog wd = new WizardDialog(Display.getCurrent().getActiveShell(),wizard) ;
+				wd.open();
+			}
+		}
+		return null;
+	}
 
 }
