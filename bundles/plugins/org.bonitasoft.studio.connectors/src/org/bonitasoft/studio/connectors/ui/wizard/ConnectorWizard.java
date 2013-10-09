779cd4e95297ccb3be019d88eae814f6dae82cb9
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-10-09 18:10:18 +0200

    STUDIO-3004 Set the former default value for ignoreErrors in model to be compliant at migration time.
    Set new default value programmaticaly

--- bundles/plugins/org.bonitasoft.studio.connectors/src/org/bonitasoft/studio/connectors/ui/wizard/ConnectorWizard.java
+++ bundles/plugins/org.bonitasoft.studio.connectors/src/org/bonitasoft/studio/connectors/ui/wizard/ConnectorWizard.java
@@ -136,6 +136,7 @@ public class ConnectorWizard extends ExtensibleWizard implements IConnectorDefin
 	public ConnectorWizard(EObject container,EStructuralFeature connectorContainmentFeature ,Set<EStructuralFeature> featureToCheckForUniqueID){
 		this.container = container ;
 		connectorWorkingCopy = ProcessFactory.eINSTANCE.createConnector() ;
+		connectorWorkingCopy.setIgnoreErrors(false);
 		connectorWorkingCopy.setConfiguration(ConnectorConfigurationFactory.eINSTANCE.createConnectorConfiguration()) ;
 		editMode = false ;
 		this.connectorContainmentFeature = connectorContainmentFeature ;
