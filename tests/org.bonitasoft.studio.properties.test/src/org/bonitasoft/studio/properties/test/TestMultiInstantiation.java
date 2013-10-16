9060c63cfa6f0dedda1cf3554d6992439fee9d80
azara <aurelie.zara@bonitasoft.com>
2013-10-16 10:49:57 +0200

    correction test testMulttiInstanceCardinality (widget names)

--- tests/org.bonitasoft.studio.properties.test/src/org/bonitasoft/studio/properties/test/TestMultiInstantiation.java
+++ tests/org.bonitasoft.studio.properties.test/src/org/bonitasoft/studio/properties/test/TestMultiInstantiation.java
@@ -155,15 +155,15 @@ public class TestMultiInstantiation extends SWTBotGefTestCase implements SWTBotC
         gmfEditor = bot.gefEditor("Step1");
 
         // configure the nbTicketsAvailable widget to a Message field
-        setWidgetProperties(gmfEditor,  "nbTicketsAvailable",  null, "Message", null,0,  null,null);
+        setWidgetProperties(gmfEditor,  "nbTicketsAvailable1",  null, "Message", null,0,  null,null);
         bot.toolbarButtonWithId(ExpressionViewer.SWTBOT_ID_EDITBUTTON, 0).click();
         SWTBotTestUtil.setScriptExpression( bot, "nbTicketsAvailable",  "\"Only \"+nbTicketsAvailable+\" tickets available.\"",  "java.lang.String" );
 
 
         // configure the nbTickets widget to a text field
-        setWidgetProperties(gmfEditor,  "nbTickets",  "Nbr de Tickets à reserver", "Text field", "0",  1, "nbTickets","java.lang.Integer");
+        setWidgetProperties(gmfEditor,  "nbTickets1",  "Nbr de Tickets à reserver", "Text field", "0",  1, "nbTickets","java.lang.Integer");
         bot.toolbarButtonWithId(ExpressionViewer.SWTBOT_ID_EDITBUTTON, 1).click();
-        SWTBotTestUtil.setScriptExpression( bot, "nbTickets",  "Integer.valueOf(field_nbTickets)",  "java.lang.Integer" );
+        SWTBotTestUtil.setScriptExpression( bot, "nbTickets",  "Integer.valueOf(field_nbTickets1)",  "java.lang.Integer" );
 
         // Save the form
         bot.saveAllEditors();
