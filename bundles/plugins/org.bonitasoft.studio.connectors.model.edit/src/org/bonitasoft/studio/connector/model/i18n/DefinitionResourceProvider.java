b924c9a56060e60419709eb08129b5005e1e157f
azara <aurelie.zara@bonitasoft.com>
2013-11-05 14:25:01 +0100

    BS-1905 Add a connector generates a NPE

--- bundles/plugins/org.bonitasoft.studio.connectors.model.edit/src/org/bonitasoft/studio/connector/model/i18n/DefinitionResourceProvider.java
+++ bundles/plugins/org.bonitasoft.studio.connectors.model.edit/src/org/bonitasoft/studio/connector/model/i18n/DefinitionResourceProvider.java
@@ -560,7 +560,11 @@ public class DefinitionResourceProvider {
 				URL iconURL = null;
 				if(!iconFile.exists()){
 					try {
-						iconURL = FileLocator.toFileURL(bundle.getResource(store.getResource().getName()+"/"+category.getIcon()));
+						String name = store.getResource().getName()+"/"+category.getIcon();
+						URL resourceURL = bundle.getResource(name);
+						if(resourceURL != null){
+							iconURL = FileLocator.toFileURL(resourceURL);
+						}
 					} catch (IOException e) {
 						BonitaStudioLog.error(e);
 					}
