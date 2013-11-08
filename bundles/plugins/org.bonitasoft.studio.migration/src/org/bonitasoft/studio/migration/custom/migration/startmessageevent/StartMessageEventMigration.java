b4cfbd4e1aee587cb45c12bd20d4591171160a2e
azara <aurelie.zara@bonitasoft.com>
2013-11-08 12:13:29 +0100

    fix bug on startMessageEventMigration

--- bundles/plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/custom/migration/startmessageevent/StartMessageEventMigration.java
+++ bundles/plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/custom/migration/startmessageevent/StartMessageEventMigration.java
@@ -20,16 +20,18 @@ public class StartMessageEventMigration extends CustomMigration {
 			throws MigrationException {
 		for (Instance ste : model.getAllInstances("process.StartMessageEvent")){
 			Instance tableCorrelation = ste.get("correlation");
-			model.delete(tableCorrelation);
-			Instance messageFlow = ste.get("incomingMessag");
-			Instance messageSource = messageFlow.get("source");
-			List<Instance> events = new ArrayList<Instance>();
-			events = messageSource.get("events");
-			for (Instance event:events){
-				Instance catchMessageEventName = event.get("targetElementExpression");
-				if (catchMessageEventName.get("name").equals(ste.get("name"))){
-					Instance correlation = event.get("correlation");
-					correlation.set("correlationType",metamodel.getEEnumLiteral("process.CorrelationTypeActive.INACTIVE"));
+			if (tableCorrelation!=null){
+				model.delete(tableCorrelation);
+				Instance messageFlow = ste.get("incomingMessag");
+				Instance messageSource = messageFlow.get("source");
+				List<Instance> events = new ArrayList<Instance>();
+				events = messageSource.get("events");
+				for (Instance event:events){
+					Instance catchMessageEventName = event.get("targetElementExpression");
+					if (catchMessageEventName.get("name").equals(ste.get("name"))){
+						Instance correlation = event.get("correlation");
+						correlation.set("correlationType",metamodel.getEEnumLiteral("process.CorrelationTypeActive.INACTIVE"));
+					}
 				}
 			}
 		}
