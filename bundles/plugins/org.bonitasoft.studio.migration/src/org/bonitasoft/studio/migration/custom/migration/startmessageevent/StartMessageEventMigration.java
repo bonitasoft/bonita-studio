32c0d4b9fc8ed786d516a976f18442a1470c3c6e
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-11-08 12:26:02 +0100

    fix npe in migration rule
    Conflicts:
    	bundles/plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/custom/migration/startmessageevent/StartMessageEventMigration.java

--- bundles/plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/custom/migration/startmessageevent/StartMessageEventMigration.java
+++ bundles/plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/custom/migration/startmessageevent/StartMessageEventMigration.java
@@ -3,10 +3,6 @@ package org.bonitasoft.studio.migration.custom.migration.startmessageevent;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bonitasoft.studio.model.expression.ExpressionFactory;
-import org.bonitasoft.studio.model.expression.ExpressionPackage;
-import org.bonitasoft.studio.model.expression.TableExpression;
-import org.bonitasoft.studio.model.process.CorrelationTypeActive;
 import org.eclipse.emf.edapt.migration.CustomMigration;
 import org.eclipse.emf.edapt.migration.Instance;
 import org.eclipse.emf.edapt.migration.Metamodel;
@@ -20,17 +16,21 @@ public class StartMessageEventMigration extends CustomMigration {
 			throws MigrationException {
 		for (Instance ste : model.getAllInstances("process.StartMessageEvent")){
 			Instance tableCorrelation = ste.get("correlation");
-			if (tableCorrelation!=null){
+			if(tableCorrelation != null){
 				model.delete(tableCorrelation);
-				Instance messageFlow = ste.get("incomingMessag");
+			}
+			Instance messageFlow = ste.get("incomingMessag");
+			if(messageFlow != null){
 				Instance messageSource = messageFlow.get("source");
-				List<Instance> events = new ArrayList<Instance>();
-				events = messageSource.get("events");
-				for (Instance event:events){
-					Instance catchMessageEventName = event.get("targetElementExpression");
-					if (catchMessageEventName.get("name").equals(ste.get("name"))){
-						Instance correlation = event.get("correlation");
-						correlation.set("correlationType",metamodel.getEEnumLiteral("process.CorrelationTypeActive.INACTIVE"));
+				if(messageSource != null){
+					List<Instance> events = new ArrayList<Instance>();
+					events = messageSource.get("events");
+					for (Instance event:events){
+						Instance catchMessageEventName = event.get("targetElementExpression");
+						if (catchMessageEventName.get("name").equals(ste.get("name"))){
+							Instance correlation = event.get("correlation");
+							correlation.set("correlationType",metamodel.getEEnumLiteral("process.CorrelationTypeActive.INACTIVE"));
+						}
 					}
 				}
 			}
