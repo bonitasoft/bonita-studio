387bacf4fa721f9606e3b72abe564f6a3b822743
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-12-04 17:34:02 +0100

    fix connector existence constraint

--- bundles/plugins/org.bonitasoft.studio.validation/src/org/bonitasoft/studio/validation/constraints/process/ConnectorExistenceConstraint.java
+++ bundles/plugins/org.bonitasoft.studio.validation/src/org/bonitasoft/studio/validation/constraints/process/ConnectorExistenceConstraint.java
@@ -1,23 +1,24 @@
 package org.bonitasoft.studio.validation.constraints.process;
 
+import org.bonitasoft.studio.actors.repository.ActorFilterDefRepositoryStore;
 import org.bonitasoft.studio.common.repository.RepositoryManager;
 import org.bonitasoft.studio.connector.model.definition.AbstractDefinitionRepositoryStore;
 import org.bonitasoft.studio.connector.model.definition.ConnectorDefinition;
 import org.bonitasoft.studio.connectors.repository.ConnectorDefRepositoryStore;
+import org.bonitasoft.studio.model.process.ActorFilter;
 import org.bonitasoft.studio.model.process.Connector;
 import org.bonitasoft.studio.validation.constraints.AbstractLiveValidationMarkerConstraint;
 import org.bonitasoft.studio.validation.i18n.Messages;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.validation.IValidationContext;
 
-public class ConnectorExistenceConstraint extends
-		AbstractLiveValidationMarkerConstraint {
+public class ConnectorExistenceConstraint extends AbstractLiveValidationMarkerConstraint {
 
 
 
 	public static final String ID = "org.bonitasoft.studio.validation.constraints.connectorexistence";
-	
-	
+
+
 	@Override
 	protected IStatus performLiveValidation(IValidationContext context) {
 		return null;
@@ -25,14 +26,27 @@ public class ConnectorExistenceConstraint extends
 
 	@Override
 	protected IStatus performBatchValidation(IValidationContext context) {
-		final AbstractDefinitionRepositoryStore<?> connectorDefStore = (AbstractDefinitionRepositoryStore<?>) RepositoryManager
-				.getInstance().getRepositoryStore(ConnectorDefRepositoryStore.class);
 		Connector connector = (Connector)context.getTarget();
+		AbstractDefinitionRepositoryStore<?> connectorDefStore = null;
+		boolean isConnector = true;
+		if(!(connector instanceof ActorFilter)){
+			 connectorDefStore = (AbstractDefinitionRepositoryStore<?>) RepositoryManager
+						.getInstance().getRepositoryStore(ConnectorDefRepositoryStore.class);
+		}else{
+			isConnector = false;
+			connectorDefStore = (AbstractDefinitionRepositoryStore<?>) RepositoryManager
+					.getInstance().getRepositoryStore(ActorFilterDefRepositoryStore.class);
+		}
 		ConnectorDefinition def = connectorDefStore.getDefinition(connector.getDefinitionId(),connector.getDefinitionVersion());
 		if (def!=null){
 			return context.createSuccessStatus();
 		}  else {
-		return context.createFailureStatus( Messages.bind(Messages.Validation_noConnectorDefFound,connector.getName()));
+			if(isConnector){
+				return context.createFailureStatus( Messages.bind(Messages.Validation_noConnectorDefFound,connector.getName(),connector.getDefinitionId()+"--"+connector.getDefinitionVersion()));
+			}else{
+				return context.createFailureStatus( Messages.bind(Messages.Validation_noActorFilterDefFound,connector.getName(),connector.getDefinitionId()+"--"+connector.getDefinitionVersion()));
+			}
+			
 		}
 	}
 
