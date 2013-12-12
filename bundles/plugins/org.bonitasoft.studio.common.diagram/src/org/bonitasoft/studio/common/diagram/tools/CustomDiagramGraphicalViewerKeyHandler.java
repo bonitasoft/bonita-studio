29ac9bf8f2d1b4a73d5287522b40d12bf4b904c0
azara <aurelie.zara@bonitasoft.com>
2013-12-12 14:10:18 +0100

    improve code

--- bundles/plugins/org.bonitasoft.studio.common.diagram/src/org/bonitasoft/studio/common/diagram/tools/CustomDiagramGraphicalViewerKeyHandler.java
+++ bundles/plugins/org.bonitasoft.studio.common.diagram/src/org/bonitasoft/studio/common/diagram/tools/CustomDiagramGraphicalViewerKeyHandler.java
@@ -1,5 +1,19 @@
 /**
- * 
+ * Copyright (C) 2013 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.common.diagram.tools;
 
@@ -26,7 +40,7 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.progress.IProgressService;
 
 /**
- * @author aurelie
+ * @author aurelie Zara
  *
  */
 @SuppressWarnings("restriction")
@@ -35,7 +49,6 @@ public class CustomDiagramGraphicalViewerKeyHandler extends
 
 	public CustomDiagramGraphicalViewerKeyHandler(GraphicalViewer viewer) {
 		super(viewer);
-		// TODO Auto-generated constructor stub
 	}
 	
 @Override
@@ -75,11 +88,7 @@ public boolean keyPressed(KeyEvent event) {
 	return super.keyPressed(event);
 }
 
-@Override
-public boolean keyReleased(KeyEvent event) {
-	
-	return super.keyReleased(event);
-}
+
 
 
 }
