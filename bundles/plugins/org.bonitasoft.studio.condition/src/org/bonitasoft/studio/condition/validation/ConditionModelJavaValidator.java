a51b300c81ec032e507ab8771184dfcaca597b6e
Romain Bioteau <romain.bioteau@bonitasoft.com>
2014-01-03 16:05:11 +0100

    BS-2674 Fix Xtext process reference resolution

--- bundles/plugins/org.bonitasoft.studio.condition/src/org/bonitasoft/studio/condition/validation/ConditionModelJavaValidator.java
+++ bundles/plugins/org.bonitasoft.studio.condition/src/org/bonitasoft/studio/condition/validation/ConditionModelJavaValidator.java
@@ -1,17 +1,13 @@
 package org.bonitasoft.studio.condition.validation;
 
-import org.bonitasoft.studio.condition.conditionModel.ConditionModelFactory;
 import org.bonitasoft.studio.condition.conditionModel.ConditionModelPackage;
 import org.bonitasoft.studio.condition.conditionModel.Expression;
 import org.bonitasoft.studio.condition.conditionModel.Expression_Boolean;
 import org.bonitasoft.studio.condition.conditionModel.Expression_ProcessRef;
 import org.bonitasoft.studio.condition.conditionModel.Operation;
 import org.bonitasoft.studio.condition.conditionModel.Operation_Compare;
-import org.bonitasoft.studio.condition.conditionModel.Unary_Operation;
-import org.bonitasoft.studio.condition.conditionModel.impl.ConditionModelPackageImpl;
 import org.bonitasoft.studio.condition.conditionModel.util.ConditionModelSwitch;
 import org.bonitasoft.studio.condition.i18n.Messages;
-import org.bonitasoft.studio.condition.services.ConditionModelGrammarAccess.Operation_CompareElements;
 import org.bonitasoft.studio.model.parameter.Parameter;
 import org.bonitasoft.studio.model.process.BooleanType;
 import org.bonitasoft.studio.model.process.Data;
@@ -24,9 +20,7 @@ import org.bonitasoft.studio.model.process.JavaObjectData;
 import org.bonitasoft.studio.model.process.LongType;
 import org.bonitasoft.studio.model.process.StringType;
 import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
