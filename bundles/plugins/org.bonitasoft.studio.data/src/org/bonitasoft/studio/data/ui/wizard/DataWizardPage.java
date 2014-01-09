b434c263f3c4b0f51337ce3231c49f11692ba123
Romain Bioteau <romain.bioteau@bonitasoft.com>
2014-01-09 14:22:57 +0100

    BS-1933 Reset name even if invalid

--- bundles/plugins/org.bonitasoft.studio.data/src/org/bonitasoft/studio/data/ui/wizard/DataWizardPage.java
+++ bundles/plugins/org.bonitasoft.studio.data/src/org/bonitasoft/studio/data/ui/wizard/DataWizardPage.java
@@ -19,7 +19,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
@@ -74,13 +73,12 @@ import org.bonitasoft.studio.model.process.StringType;
 import org.bonitasoft.studio.model.process.Task;
 import org.bonitasoft.studio.model.process.XMLData;
 import org.bonitasoft.studio.model.process.XMLType;
-import org.bonitasoft.studio.model.process.impl.DataTypeImpl;
-import org.bonitasoft.studio.model.process.impl.StringTypeImpl;
 import org.bonitasoft.studio.model.process.util.ProcessSwitch;
 import org.bonitasoft.studio.pics.Pics;
 import org.bonitasoft.studio.pics.PicsConstants;
 import org.bonitasoft.studio.xml.repository.XSDFileStore;
 import org.bonitasoft.studio.xml.repository.XSDRepositoryStore;
+import org.eclipse.core.databinding.Binding;
 import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.conversion.Converter;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
@@ -260,7 +258,7 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 		this.showAutoGenerateform = showAutoGenerateform;
 		this.fixedReturnType = fixedReturnType;
 	}
-	
+
 	private String getCurrentDataAwareContextName(){
 		String name = "---";
 		EObject context = container;
@@ -353,7 +351,7 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 		createDataOptions(mainComposite);
 
 		updateDatabinding();
-		
+
 		if(fixedReturnType!=null){
 			for (Object object : (EObjectContainmentEList)typeCombo.getInput()) {
 				final DataType type = (DataType) object;
@@ -366,7 +364,7 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 				}
 			}			
 		} else {
-			
+
 		}
 		setControl(mainComposite);
 	}
@@ -392,7 +390,7 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 				emfDatabindingContext.dispose();
 			}
 			emfDatabindingContext = new EMFDataBindingContext();
-
+			pageSupport = WizardPageSupport.create(this, emfDatabindingContext);
 			bindNameAndDescription();
 			bindGenerateDataCheckbox();
 			bindDataTypeCombo();
@@ -401,7 +399,7 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 			bindTransientButton();
 			bindDefaultValueViewer();
 			bindIsMultipleData();
-			
+
 			typeDescriptionDecorator.setDescriptionText(getHintFor(data.getDataType()));
 
 			MultiValidator returnTypeValidator = new MultiValidator() {
@@ -491,7 +489,7 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 				}
 			};
 			emfDatabindingContext.addValidationStatusProvider(returnTypeValidator);
-			pageSupport = WizardPageSupport.create(this, emfDatabindingContext);
+			
 		}
 	}
 
@@ -562,9 +560,9 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 				/* Search above level */
 				List<Data> allData = null;
 				if( container instanceof AbstractProcess){
-					 allData = ModelHelper.getAllItemsOfType(ModelHelper.getParentProcess(container), ProcessPackage.Literals.DATA);
+					allData = ModelHelper.getAllItemsOfType(ModelHelper.getParentProcess(container), ProcessPackage.Literals.DATA);
 				}else{
-				//	 allData = ModelHelper.getAccessibleData(container, true);
+					//	 allData = ModelHelper.getAccessibleData(container, true);
 					allData = getAllAccessibleDatas(container);
 				}
 				for (final Data object : allData) {
@@ -579,7 +577,7 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 
 				return new GroovyReferenceValidator(Messages.name).validate(value);
 			}
-			
+
 			private List<Data> getAllAccessibleDatas(EObject container){
 				List<Data> allDatas = ModelHelper.getAccessibleData(container, true);
 				for (Object o:ModelHelper.getAllItemsOfType(container, ProcessPackage.Literals.DATA)){
@@ -589,21 +587,29 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 						}
 					}
 				}
-				
+
 				return allDatas;
 			}
 		});
 
-	
+
 
 		UpdateValueStrategy descTargetToModel = new UpdateValueStrategy();
 		descTargetToModel.setAfterGetValidator(new InputLengthValidator(Messages.dataDescriptionLabel, 255));
-		emfDatabindingContext.bindValue(SWTObservables.observeText(nameText, SWT.Modify),
+		String previousName = null;
+		if(nameText != null && !nameText.isDisposed() && nameText.getText() != null){
+			previousName = nameText.getText();
+		}
+		ISWTObservableValue observeText = SWTObservables.observeText(nameText, SWT.Modify);
+		Binding bindValue = emfDatabindingContext.bindValue(observeText,
 				EMFObservables.observeValue(data, ProcessPackage.Literals.ELEMENT__NAME), nameStrategy, null);
 		emfDatabindingContext.bindValue(SWTObservables.observeText(descriptionText, SWT.Modify),
 				EMFObservables.observeValue(data, ProcessPackage.Literals.ELEMENT__DOCUMENTATION),
 				descTargetToModel,
 				null);
+		if(previousName!= null){
+			observeText.setValue(previousName);
+		}
 	}
 
 	protected void bindDataTypeCombo() {
@@ -834,9 +840,9 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 			}
 		});
 
-		
+
 		refreshDataNames();
-		
+
 		defaultValueViewer.addFilter(new AvailableExpressionTypeFilter(new String[] {ExpressionConstants.VARIABLE_TYPE,ExpressionConstants.CONSTANT_TYPE, ExpressionConstants.SCRIPT_TYPE,
 				ExpressionConstants.PARAMETER_TYPE }){
 			@Override
@@ -1279,25 +1285,25 @@ public class DataWizardPage extends WizardPage implements IBonitaVariableContext
 
 	@Override
 	public boolean isPageFlowContext() {
-		
+
 		return isPageFlowContext;
 	}
 
 	@Override
 	public void setIsPageFlowContext(boolean isPageFlowContext) {
 		this.isPageFlowContext=isPageFlowContext;
-		
+
 	}
-	
+
 	public void refreshDataNames(){
 		if(!(container instanceof AbstractProcess)){
 			List<Data> availableData = ModelHelper.getAccessibleData(ModelHelper.getParentProcess(container));
 			if(isPageFlowContext && container instanceof Task){
 				availableData.addAll(((Task)container).getData());
 			}
-				for(Data d : availableData){
-					availableDataNames.add(d.getName());
-				}
+			for(Data d : availableData){
+				availableDataNames.add(d.getName());
+			}
 		}
 	}
 }
