47c130d2ed70cac4d6953932c9eb03c569d30b8d
Romain Bioteau <romain.bioteau@bonitasoft.com>
2014-01-15 15:16:49 +0100

    fix tests

--- tests/org.bonitasoft.studio.exporter.tests/src/org/bonitasoft/studio/exporter/tests/bpmn/BPMNTestUtil.java
+++ tests/org.bonitasoft.studio.exporter.tests/src/org/bonitasoft/studio/exporter/tests/bpmn/BPMNTestUtil.java
@@ -31,7 +31,6 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.gmf.runtime.emf.core.GMFEditingDomainFactory;
 import org.eclipse.gmf.runtime.notation.Diagram;
-import org.eclipse.swt.widgets.Display;
 import org.omg.spec.bpmn.di.util.DiResourceFactoryImpl;
 import org.omg.spec.bpmn.model.DocumentRoot;
 
@@ -86,9 +85,9 @@ public class BPMNTestUtil {
         GMFEditingDomainFactory.getInstance().createEditingDomain(rSet) ;
         DiagramEditPart dep;
         try{
-            dep = OffscreenEditPartFactory.getInstance().createDiagramEditPart(diagramFor, Display.getDefault().getActiveShell());
+            dep = OffscreenEditPartFactory.getInstance().createDiagramEditPart(diagramFor,  newDiagramFileStore.getOpenedEditor().getSite().getShell());
         } catch(Exception ex){
-            dep = OffscreenEditPartFactory.getInstance().createDiagramEditPart(diagramFor, Display.getDefault().getActiveShell());
+            dep = OffscreenEditPartFactory.getInstance().createDiagramEditPart(diagramFor,  newDiagramFileStore.getOpenedEditor().getSite().getShell());
         }
         MainProcessEditPart mped = (MainProcessEditPart) dep;
         IBonitaModelExporter exporter = new BonitaModelExporterImpl(mped);
