5dbbd5ecef52bd9a8fbfdcdb398940d0f326427b
Romain Bioteau <romain.bioteau@bonitasoft.com>
2014-04-14 11:31:39 +0200

    BS-8259 Fix welcome page title

--- bundles/plugins/org.bonitasoft.studio.application/src/org/bonitasoft/studio/application/advisor/RecoverWorkspaceAdvisor.java
+++ bundles/plugins/org.bonitasoft.studio.application/src/org/bonitasoft/studio/application/advisor/RecoverWorkspaceAdvisor.java
@@ -20,64 +20,65 @@ import org.eclipse.ui.PlatformUI;
 
 /**
  * @author Romain
- * 
+ *
  */
 public class RecoverWorkspaceAdvisor extends InstallerApplicationWorkbenchAdvisor {
 
-    private String targetPath;
+	private String targetPath;
 
-    public RecoverWorkspaceAdvisor(String targetPath) {
-        super();
-        this.targetPath = targetPath;
-    }
 
-    /*
-     * (non-Javadoc)
-     * @see org.bonitasoft.studio.application.advisor.InstallerApplicationWorkbenchAdvisor#preStartup()
-     */
-    @Override
-    public void preStartup() {
-        try {
-            ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-        } catch (CoreException e) {
-            BonitaStudioLog.error(e);
-        }
-        IProject defaultDir = ResourcesPlugin.getWorkspace().getRoot().getProject("default");
-        if (defaultDir != null && defaultDir.exists()) {
-            super.preStartup();
-        } else {
-            MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.invalidWorkspaceTitle, Messages.invalidWorkspace);
-            PlatformUI.getWorkbench().close();
-        }
+	public RecoverWorkspaceAdvisor(String targetPath){
+		super() ;
+		this.targetPath = targetPath ;
+	}
 
-    }
+	/* (non-Javadoc)
+	 * @see org.bonitasoft.studio.application.advisor.InstallerApplicationWorkbenchAdvisor#preStartup()
+	 */
+	@Override
+	public void preStartup() {
+		try {
+			ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE,new NullProgressMonitor());
+		} catch (CoreException e) {
+			BonitaStudioLog.error(e);
+		}
+		IProject defaultDir = ResourcesPlugin.getWorkspace().getRoot().getProject("default");
+		if (defaultDir != null && defaultDir.exists()){
+			super.preStartup();
+		} else {
+			MessageDialog.openError(Display.getCurrent().getActiveShell(),Messages.invalidWorkspaceTitle, Messages.invalidWorkspace);
+			PlatformUI.getWorkbench().close() ; 
+		}
 
-    /*
-     * (non-Javadoc)
-     * @see org.bonitasoft.studio.application.advisor.InstallerApplicationWorkbenchAdvisor#executePostStartupHandler()
-     */
-    @Override
-    protected void executePostStartupHandler() {
-        for (IRepository repo : RepositoryManager.getInstance().getAllRepositories()) {
-            if (!repo.isShared()) {
-                RepositoryManager.getInstance().setRepository(repo.getName());
-                repo.exportToArchive(targetPath + File.separatorChar + repo.getName() + ".bos");
-                RepositoryManager.getInstance().getCurrentRepository().close();
-            }
-        }
-        RepositoryManager.getInstance().setRepository("default");
-    }
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see org.bonitasoft.studio.application.advisor.InstallerApplicationWorkbenchAdvisor#postStartup()
-     */
-    @Override
-    public void postStartup() {
-        IProject defaultDir = ResourcesPlugin.getWorkspace().getRoot().getProject("default");
-        if (defaultDir != null && defaultDir.exists()) {
-            super.postStartup();
-        }
-    }
+
+	/* (non-Javadoc)
+	 * @see org.bonitasoft.studio.application.advisor.InstallerApplicationWorkbenchAdvisor#executePostStartupHandler()
+	 */
+	@Override
+	protected void executePostStartupHandler() {
+		for(IRepository repo : RepositoryManager.getInstance().getAllRepositories()){
+			if(!repo.isShared()){
+				RepositoryManager.getInstance().setRepository(repo.getName());
+				repo.exportToArchive(targetPath+File.separatorChar+repo.getName()+".bos") ;
+				RepositoryManager.getInstance().getCurrentRepository().close();
+			}
+		}
+		RepositoryManager.getInstance().setRepository("default");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.bonitasoft.studio.application.advisor.InstallerApplicationWorkbenchAdvisor#postStartup()
+	 */
+	@Override
+	public void postStartup() {
+		IProject defaultDir = ResourcesPlugin.getWorkspace().getRoot().getProject("default");
+		if (defaultDir != null && defaultDir.exists()){
+			super.postStartup();
+		}
+	}
 
 }
+
+
