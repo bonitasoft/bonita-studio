1c41944a60eda6646f2355f6bbeb40ffd83da9fb
Florine Boudin <florine.boudin@bonitasoft.com>
2014-10-01 18:49:24 +0200

    BS-9613 Change operation set by default on File widget + Complete tests

--- bundles/plugins/org.bonitasoft.studio.diagram.form.custom/src/org/bonitasoft/studio/diagram/form/custom/model/WidgetMapping.java
+++ bundles/plugins/org.bonitasoft.studio.diagram.form.custom/src/org/bonitasoft/studio/diagram/form/custom/model/WidgetMapping.java
@@ -27,6 +27,7 @@ import org.bonitasoft.studio.model.process.BooleanType;
 import org.bonitasoft.studio.model.process.Data;
 import org.bonitasoft.studio.model.process.DataType;
 import org.bonitasoft.studio.model.process.Document;
+import org.bonitasoft.studio.model.process.DocumentType;
 import org.bonitasoft.studio.model.process.util.ProcessSwitch;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.ecore.EClass;
@@ -39,170 +40,188 @@ import org.eclipse.emf.ecore.EObject;
 public class WidgetMapping {
 
 
-	protected static List<EClass> singleValuatedTextWidgetType ;
-	static{
-		singleValuatedTextWidgetType = new ArrayList<EClass>();
-		singleValuatedTextWidgetType.add(FormPackage.Literals.TEXT_FORM_FIELD);
-		singleValuatedTextWidgetType.add(FormPackage.Literals.PASSWORD_FORM_FIELD);
-		singleValuatedTextWidgetType.add(FormPackage.Literals.TEXT_AREA_FORM_FIELD);
-		singleValuatedTextWidgetType.add(FormPackage.Literals.RICH_TEXT_AREA_FORM_FIELD);
-		singleValuatedTextWidgetType.add(FormPackage.Literals.TEXT_INFO);
-		singleValuatedTextWidgetType.add(FormPackage.Literals.MESSAGE_INFO);
-	}
-	
-	private boolean isGenerated;
+    protected static List<EClass> singleValuatedTextWidgetType ;
+    static{
+        singleValuatedTextWidgetType = new ArrayList<EClass>();
+        singleValuatedTextWidgetType.add(FormPackage.Literals.TEXT_FORM_FIELD);
+        singleValuatedTextWidgetType.add(FormPackage.Literals.PASSWORD_FORM_FIELD);
+        singleValuatedTextWidgetType.add(FormPackage.Literals.TEXT_AREA_FORM_FIELD);
+        singleValuatedTextWidgetType.add(FormPackage.Literals.RICH_TEXT_AREA_FORM_FIELD);
+        singleValuatedTextWidgetType.add(FormPackage.Literals.TEXT_INFO);
+        singleValuatedTextWidgetType.add(FormPackage.Literals.MESSAGE_INFO);
+    }
 
-	private boolean isMandatory;
-	
-	private boolean isReadOnly;
-	
-	private Object modelElement;
+    private boolean isGenerated;
 
-	private Widget widgetType;
-	
-	private List<WidgetMapping> children;
-	
-	private WidgetMapping parent;
+    private boolean isMandatory;
 
-	public WidgetMapping(Object modelElement){
-		Assert.isNotNull(modelElement);
-		this.modelElement = modelElement;
-		this.children = new ArrayList<WidgetMapping>();
-		setWidgetType(initializeWidgetType());
-	}
+    private boolean isReadOnly;
 
-	/**
-	 * @return
-	 */
-	private Widget initializeWidgetType() {
-		if(modelElement instanceof Document){
-			return FormFactory.eINSTANCE.createFileWidget();
-		}
-		return initializeWidgetTypeForData();
-	}
+    private Object modelElement;
 
-	protected Widget initializeWidgetTypeForData() {
-		List<EClass> widgetType = getCompatibleWidgetTypes();
-		if(!widgetType.isEmpty()){
-			EClass eClass = widgetType.get(0);
-			return (Widget) FormFactory.eINSTANCE.create(eClass);
-		}
-		return null;
-	}
+    private Widget widgetType;
 
-	public boolean isGenerated() {
-		return isGenerated;
-	}
+    private final List<WidgetMapping> children;
 
-	public void setGenerated(boolean isGenerated) {
-		this.isGenerated = isGenerated;
-	}
+    private WidgetMapping parent;
 
-	public Object getModelElement() {
-		return modelElement;
-	}
+    public WidgetMapping(final Object modelElement){
+        Assert.isNotNull(modelElement);
+        this.modelElement = modelElement;
+        children = new ArrayList<WidgetMapping>();
+        setWidgetType(initializeWidgetType());
+    }
 
-	public void setModelElement(EObject modelElement) {
-		this.modelElement = modelElement;
-	}
+    /**
+     * @return
+     */
+    private Widget initializeWidgetType() {
+        if(modelElement instanceof Document){
+            return FormFactory.eINSTANCE.createFileWidget();
+        }
+        return initializeWidgetTypeForData();
+    }
 
-	public Widget getWidgetType() {
-		return widgetType;
-	}
+    protected Widget initializeWidgetTypeForData() {
+        final List<EClass> widgetType = getCompatibleWidgetTypes();
+        if(!widgetType.isEmpty()){
+            final EClass eClass = widgetType.get(0);
+            return (Widget) FormFactory.eINSTANCE.create(eClass);
+        }
+        return null;
+    }
 
-	public void setWidgetType(Widget widgetType) {
-		Assert.isNotNull(widgetType);
-		Assert.isLegal(getCompatibleWidgetTypes().contains(widgetType.eClass()),widgetType.eClass().getName() + " is not compatible with "+ modelElement);
-		this.widgetType = widgetType;
-	}
+    public boolean isGenerated() {
+        return isGenerated;
+    }
 
-	public boolean isMandatory() {
-		return isMandatory;
-	}
+    public void setGenerated(final boolean isGenerated) {
+        this.isGenerated = isGenerated;
+    }
 
-	public void setMandatory(boolean isMandatory) {
-		this.isMandatory = isMandatory;
-	}
+    public Object getModelElement() {
+        return modelElement;
+    }
 
-	public boolean isReadOnly() {
-		return isReadOnly;
-	}
+    public void setModelElement(final EObject modelElement) {
+        this.modelElement = modelElement;
+    }
 
-	public void setReadOnly(boolean isReadOnly) {
-		this.isReadOnly = isReadOnly;
-	}
+    public Widget getWidgetType() {
+        return widgetType;
+    }
+
+    public void setWidgetType(final Widget widgetType) {
+        Assert.isNotNull(widgetType);
+        Assert.isLegal(getCompatibleWidgetTypes().contains(widgetType.eClass()),widgetType.eClass().getName() + " is not compatible with "+ modelElement);
+        this.widgetType = widgetType;
+    }
+
+    public boolean isMandatory() {
+        return isMandatory;
+    }
+
+    public void setMandatory(final boolean isMandatory) {
+        this.isMandatory = isMandatory;
+    }
+
+    public boolean isReadOnly() {
+        return isReadOnly;
+    }
+
+    public void setReadOnly(final boolean isReadOnly) {
+        this.isReadOnly = isReadOnly;
+    }
 
 
-	public List<EClass> getCompatibleWidgetTypes() {
-		if(modelElement instanceof Data){
-			return getCompatibleWidgetTypesForData((Data) modelElement);	
-		}else if(modelElement instanceof Document){
-			return Collections.<EClass>singletonList(FormPackage.Literals.FILE_WIDGET);
-		}
-		return Collections.<EClass>emptyList();
-	}
+    public List<EClass> getCompatibleWidgetTypes() {
+        if (modelElement instanceof Data) {
+            return getCompatibleWidgetTypesForData((Data) modelElement);
+        } else if (modelElement instanceof Document) {
+            return getCompatibleWidgetTypesForDocument((Document) modelElement);
+        }
+        return Collections.<EClass> emptyList();
+    }
 
-	protected List<EClass> getCompatibleWidgetTypesForData(Data currentData) {
-		DataType dataType = currentData.getDataType();
-		if(dataType == null){
-			throw new IllegalStateException(currentData+" should have a DataType");
-		}
-		if(currentData.isMultiple()){
-			if(dataType instanceof BooleanType){
-				return Collections.<EClass>singletonList(FormPackage.Literals.CHECK_BOX_MULTIPLE_FORM_FIELD);
-			}
-			return Collections.<EClass>singletonList(FormPackage.Literals.LIST_FORM_FIELD);
-		}else{
-			return getWidgetTypeForSingleData(dataType);
-		}
-	}
+    protected List<EClass> getCompatibleWidgetTypesForDocument(final Document currentDocument) {
+        final DocumentType docType = currentDocument.getDocumentType();
+        if (docType == null) {
+            throw new IllegalStateException(currentDocument + " should have a DocumentType");
+        }
+        // Multiple widget with documents ?
+        //        if (currentDocument.isMultiple()) {
+        //            return null;
+        //        } else {
+        return Collections.<EClass> singletonList(FormPackage.Literals.FILE_WIDGET);
+        //        }
+    }
 
-	protected List<EClass> getWidgetTypeForSingleData(DataType dataType) {
-		return new ProcessSwitch<List<EClass>>(){
+    protected List<EClass> getCompatibleWidgetTypesForData(final Data currentData) {
+        final DataType dataType = currentData.getDataType();
+        if (dataType == null) {
+            throw new IllegalStateException(currentData + " should have a DataType");
+        }
+        if (currentData.isMultiple()) {
+            if (dataType instanceof BooleanType) {
+                return Collections.<EClass> singletonList(FormPackage.Literals.CHECK_BOX_MULTIPLE_FORM_FIELD);
+            }
+            return Collections.<EClass> singletonList(FormPackage.Literals.LIST_FORM_FIELD);
+        } else {
+            return getWidgetTypeForSingleData(dataType);
+        }
+    }
 
-			public List<EClass> caseDateType(org.bonitasoft.studio.model.process.DateType object) {
-				return Collections.<EClass>singletonList(FormPackage.Literals.DATE_FORM_FIELD);
-			}
+    protected List<EClass> getWidgetTypeForSingleData(final DataType dataType) {
+        return new ProcessSwitch<List<EClass>>(){
 
-			public List<EClass> caseBooleanType(org.bonitasoft.studio.model.process.BooleanType object) {
-				return Collections.<EClass>singletonList(FormPackage.Literals.CHECK_BOX_SINGLE_FORM_FIELD);
-			}
+            @Override
+            public List<EClass> caseDateType(final org.bonitasoft.studio.model.process.DateType object) {
+                return Collections.<EClass>singletonList(FormPackage.Literals.DATE_FORM_FIELD);
+            }
 
-			public List<EClass> caseEnumType(org.bonitasoft.studio.model.process.EnumType object) {
-				List<EClass> result = new ArrayList<EClass>();
-				result.add(FormPackage.Literals.RADIO_FORM_FIELD);
-				result.add(FormPackage.Literals.SELECT_FORM_FIELD);
-				return result;
-			}
+            @Override
+            public List<EClass> caseBooleanType(final org.bonitasoft.studio.model.process.BooleanType object) {
+                return Collections.<EClass>singletonList(FormPackage.Literals.CHECK_BOX_SINGLE_FORM_FIELD);
+            }
 
-			public java.util.List<EClass> caseStringType(org.bonitasoft.studio.model.process.StringType object) {
-				return singleValuatedTextWidgetType;
-			}
-			
-			public List<EClass> defaultCase(org.eclipse.emf.ecore.EObject object) {
-				return Collections.<EClass>singletonList(FormPackage.Literals.TEXT_FORM_FIELD);
-			}
+            @Override
+            public List<EClass> caseEnumType(final org.bonitasoft.studio.model.process.EnumType object) {
+                final List<EClass> result = new ArrayList<EClass>();
+                result.add(FormPackage.Literals.RADIO_FORM_FIELD);
+                result.add(FormPackage.Literals.SELECT_FORM_FIELD);
+                return result;
+            }
 
-		}.doSwitch(dataType);
-	}
+            @Override
+            public java.util.List<EClass> caseStringType(final org.bonitasoft.studio.model.process.StringType object) {
+                return singleValuatedTextWidgetType;
+            }
 
-	public List<? extends WidgetMapping> getChildren() {
-		return children;
-	}
+            @Override
+            public List<EClass> defaultCase(final org.eclipse.emf.ecore.EObject object) {
+                return Collections.<EClass>singletonList(FormPackage.Literals.TEXT_FORM_FIELD);
+            }
 
-	public void addChild(WidgetMapping child) {
-		if(!children.contains(child)){
-			child.setParent(this);
-			children.add(child);
-		}
-	}
+        }.doSwitch(dataType);
+    }
 
-	public WidgetMapping getParent() {
-		return parent;
-	}
+    public List<? extends WidgetMapping> getChildren() {
+        return children;
+    }
 
-	public void setParent(WidgetMapping parent) {
-		this.parent = parent;
-	}
+    public void addChild(final WidgetMapping child) {
+        if(!children.contains(child)){
+            child.setParent(this);
+            children.add(child);
+        }
+    }
+
+    public WidgetMapping getParent() {
+        return parent;
+    }
+
+    public void setParent(final WidgetMapping parent) {
+        this.parent = parent;
+    }
 
 }
