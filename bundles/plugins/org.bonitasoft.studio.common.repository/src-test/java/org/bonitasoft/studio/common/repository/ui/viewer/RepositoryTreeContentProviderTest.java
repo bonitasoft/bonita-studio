105ba5c44d0ce74d9ec38462801762e7fd9c2476
apupier <aurelien.pupier@bonitasoft.com>
2014-11-05 16:01:10 +0100

    BS-11264: Test Repository Tree Content provider

---
+++ bundles/plugins/org.bonitasoft.studio.common.repository/src-test/java/org/bonitasoft/studio/common/repository/ui/viewer/RepositoryTreeContentProviderTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2014 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.common.repository.ui.viewer;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.assertj.core.api.Assertions;
+import org.bonitasoft.studio.common.repository.model.IRepositoryFileStore;
+import org.bonitasoft.studio.common.repository.model.IRepositoryStore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RepositoryTreeContentProviderTest {
+
+    @Mock
+    IRepositoryStore<IRepositoryFileStore> repoStore;
+
+    @Mock
+    IRepositoryFileStore repoFileStore;
+
+    @Test
+    public void testGetChildren() {
+        Mockito.doReturn(Arrays.asList(new IRepositoryFileStore[] { repoFileStore })).when(repoStore).getChildren();
+        final Object[] children = new RepositoryTreeContentProvider().getChildren(repoStore);
+        Assertions.assertThat(children).contains(repoFileStore);
+    }
+
+    @Test
+    public void testGetParent() {
+        Mockito.doReturn(repoStore).when(repoFileStore).getParentStore();
+        Assertions.assertThat(new RepositoryTreeContentProvider().getParent(repoFileStore)).isEqualTo(repoStore);
+    }
+
+    @Test
+    public void testHasChildren() {
+        Mockito.doReturn(Collections.emptyList()).when(repoStore).getChildren();
+        Assertions.assertThat(new RepositoryTreeContentProvider().hasChildren(repoStore)).isFalse();
+
+        Mockito.doReturn(Arrays.asList(new IRepositoryFileStore[] { repoFileStore })).when(repoStore).getChildren();
+        Assertions.assertThat(new RepositoryTreeContentProvider().hasChildren(repoStore)).isTrue();
+    }
+
+}
