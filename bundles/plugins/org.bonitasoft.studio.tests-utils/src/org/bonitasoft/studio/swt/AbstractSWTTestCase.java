7252778f15764bde43fb5ed39deb74bd68dc4e58
Romain Bioteau <romain.bioteau@bonitasoft.com>
2014-11-20 13:38:32 +0100

    fix display initialization

--- bundles/plugins/org.bonitasoft.studio.tests-utils/src/org/bonitasoft/studio/swt/AbstractSWTTestCase.java
+++ bundles/plugins/org.bonitasoft.studio.tests-utils/src/org/bonitasoft/studio/swt/AbstractSWTTestCase.java
@@ -39,7 +39,10 @@ public abstract class AbstractSWTTestCase {
     private DefaultRealm defaultRealm;
 
     protected Composite createDisplayAndRealm() {
-        display = new Display();
+        display = Display.getDefault();
+        if (display == null) {
+            display = new Display();
+        }
         headlessShell = new Shell(display);
         headlessShell.setLayout(new GridLayout(1, true));
         final GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
