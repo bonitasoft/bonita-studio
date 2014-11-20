de00e4b25591ef74c50f24daf7d86001d365b86e
apupier <aurelien.pupier@bonitasoft.com>
2014-11-20 13:37:47 +0100

    Add Mockito Runner for unit test

--- bundles/plugins/org.bonitasoft.studio.common/src-test/java/org/bonitasoft/studio/common/properties/AbstractNamePropertySectionContributionTest.java
+++ bundles/plugins/org.bonitasoft.studio.common/src-test/java/org/bonitasoft/studio/common/properties/AbstractNamePropertySectionContributionTest.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2014 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.bonitasoft.studio.common.properties;
 
 import static org.junit.Assert.assertFalse;
@@ -10,10 +24,12 @@ import org.bonitasoft.studio.model.form.Group;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class AbstractNamePropertySectionContributionTest {
 
     private AbstractNamePropertySectionContribution nameContribution;
