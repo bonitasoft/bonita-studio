2ae93afb3a1a2b47ed7f6229851ef381d44131f1
azara <aurelie.zara@bonitasoft.com>
2014-11-20 18:39:53 +0100

    BS-11576 Same name of 2 data in 2 different tasks, data are used in scripts : when removing one data, refactor remove reference of the 2 datas in scripts

--- bundles/plugins/org.bonitasoft.studio.common/src-test/java/org/bonitasoft/studio/common/dialog/OutlineFilterTest.java
+++ bundles/plugins/org.bonitasoft.studio.common/src-test/java/org/bonitasoft/studio/common/dialog/OutlineFilterTest.java
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -18,11 +18,14 @@ package org.bonitasoft.studio.common.dialog;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.bonitasoft.studio.common.emf.tools.ExpressionHelper;
 import org.bonitasoft.studio.common.emf.tools.ModelHelper;
 import org.bonitasoft.studio.model.expression.Expression;
 import org.bonitasoft.studio.model.expression.ExpressionFactory;
+import org.bonitasoft.studio.model.expression.Operation;
 import org.bonitasoft.studio.model.parameter.Parameter;
 import org.bonitasoft.studio.model.parameter.ParameterFactory;
+import org.bonitasoft.studio.model.process.Activity;
 import org.bonitasoft.studio.model.process.Data;
 import org.bonitasoft.studio.model.process.ProcessFactory;
 import org.bonitasoft.studio.model.process.SearchIndex;
@@ -32,98 +35,101 @@ import org.junit.Test;
 
 public class OutlineFilterTest {
 
-	private Expression expressionWithReferencies;
-	private OutlineFilter filter;
+    private Expression expressionWithReferencies;
+    private OutlineFilter filter;
 
-	@Before
-	public void setUp() throws Exception {
-		
-		expressionWithReferencies = ExpressionFactory.eINSTANCE.createExpression();
-		filter = new OutlineFilter();
-		
-	}
+    @Before
+    public void setUp() throws Exception {
 
-	@After
-	public void tearDown() throws Exception {
-	}
+        expressionWithReferencies = ExpressionFactory.eINSTANCE.createExpression();
+        filter = new OutlineFilter();
 
-	
-	
-	@Test
-	public void should_isReferencedInExpression_find_a_element_with_other_element_referenced() throws Exception {
-		// given
-		Data referencedData = ProcessFactory.eINSTANCE.createData();
-		referencedData.setName("myReferencedData");
-		
-		Data otherData = ProcessFactory.eINSTANCE.createData();
-		otherData.setName("myOtherData");
-		
-		expressionWithReferencies.getReferencedElements().add(otherData);
-		expressionWithReferencies.getReferencedElements().add(referencedData);
-		
-		filter.setElementToDisplay(referencedData);
-		
-		// when
-		boolean isElementReferenceFound = ModelHelper.isObjectIsReferencedInExpression(expressionWithReferencies, filter.getElementToDisplay());
-		
-		// then
-		assertThat(isElementReferenceFound).isTrue();
-	}	
-	
-	
-	@Test
-	public void should_isReferencedInExpression_find_a_parameter_with_other_parameter_referenced() throws Exception {
-		// given
-		
-		Parameter parameterReferenced = ParameterFactory.eINSTANCE.createParameter();
-		parameterReferenced.setName("myParameterReferenced");
-		
-		Parameter otherParameterReferenced = ParameterFactory.eINSTANCE.createParameter();
-		otherParameterReferenced.setName("myOtherParameterReferenced");
-		
-		filter.setElementToDisplay(parameterReferenced);
-		
-		expressionWithReferencies.getReferencedElements().add(otherParameterReferenced);
-		expressionWithReferencies.getReferencedElements().add(parameterReferenced);
-		
-		// when
-		boolean isParameterReferenceFound = ModelHelper.isObjectIsReferencedInExpression(expressionWithReferencies, filter.getElementToDisplay());
-		
-		// then
-		assertThat(isParameterReferenceFound).isTrue();
-		
-	}
-	
-	@Test
-	public void should_isReferencedInExpression_find_a_searchIndex_with_other_searchIndex_referenced() throws Exception {
-		// given
-		
-		Expression searchIndexReferencedName = ExpressionFactory.eINSTANCE.createExpression();
-		searchIndexReferencedName.setName("mySearchIndexReferenced");
-		searchIndexReferencedName.setContent(searchIndexReferencedName.getName());
-		
-		SearchIndex searchIndexReferenced = ProcessFactory.eINSTANCE.createSearchIndex();
-		searchIndexReferenced.setName(searchIndexReferencedName);
-		
-		Expression otherSearchIndexReferencedName = ExpressionFactory.eINSTANCE.createExpression();
-		otherSearchIndexReferencedName.setName("myOtherSearchIndexReferenced");
-		otherSearchIndexReferencedName.setContent(otherSearchIndexReferencedName.getName());
-		
-		SearchIndex otherSearchIndexReferenced = ProcessFactory.eINSTANCE.createSearchIndex();
-		otherSearchIndexReferenced.setName(otherSearchIndexReferencedName);
-		
-		filter.setElementToDisplay(searchIndexReferenced);
-		
-		expressionWithReferencies.getReferencedElements().add(otherSearchIndexReferenced);
-		expressionWithReferencies.getReferencedElements().add(searchIndexReferenced);
-		
-		// when
-		boolean isSearchIndexReferenceFound = ModelHelper.isObjectIsReferencedInExpression(expressionWithReferencies, filter.getElementToDisplay());
-		
-		// then
-		assertThat(isSearchIndexReferenceFound).isTrue();
-		
-	}
-	
-	
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+
+    @Test
+    public void should_isReferencedInExpression_find_a_element_with_other_element_referenced() throws Exception {
+        // given
+        final Activity activity = ProcessFactory.eINSTANCE.createActivity();
+        final Operation operation = ExpressionFactory.eINSTANCE.createOperation();
+        activity.getOperations().add(operation);
+        final Data referencedData = ProcessFactory.eINSTANCE.createData();
+        referencedData.setName("myReferencedData");
+        activity.getData().add(referencedData);
+        final Data otherData = ProcessFactory.eINSTANCE.createData();
+        otherData.setName("myOtherData");
+        expressionWithReferencies.getReferencedElements().add(otherData);
+        expressionWithReferencies.getReferencedElements().add(ExpressionHelper.createDependencyFromEObject(referencedData));
+        operation.setRightOperand(expressionWithReferencies);
+
+        filter.setElementToDisplay(referencedData);
+
+        // when
+        final boolean isElementReferenceFound = ModelHelper.isObjectIsReferencedInExpression(expressionWithReferencies, filter.getElementToDisplay());
+
+        // then
+        assertThat(isElementReferenceFound).isTrue();
+    }
+
+
+    @Test
+    public void should_isReferencedInExpression_find_a_parameter_with_other_parameter_referenced() throws Exception {
+        // given
+
+        final Parameter parameterReferenced = ParameterFactory.eINSTANCE.createParameter();
+        parameterReferenced.setName("myParameterReferenced");
+
+        final Parameter otherParameterReferenced = ParameterFactory.eINSTANCE.createParameter();
+        otherParameterReferenced.setName("myOtherParameterReferenced");
+
+        filter.setElementToDisplay(parameterReferenced);
+
+        expressionWithReferencies.getReferencedElements().add(otherParameterReferenced);
+        expressionWithReferencies.getReferencedElements().add(parameterReferenced);
+
+        // when
+        final boolean isParameterReferenceFound = ModelHelper.isObjectIsReferencedInExpression(expressionWithReferencies, filter.getElementToDisplay());
+
+        // then
+        assertThat(isParameterReferenceFound).isTrue();
+
+    }
+
+    @Test
+    public void should_isReferencedInExpression_find_a_searchIndex_with_other_searchIndex_referenced() throws Exception {
+        // given
+
+        final Expression searchIndexReferencedName = ExpressionFactory.eINSTANCE.createExpression();
+        searchIndexReferencedName.setName("mySearchIndexReferenced");
+        searchIndexReferencedName.setContent(searchIndexReferencedName.getName());
+
+        final SearchIndex searchIndexReferenced = ProcessFactory.eINSTANCE.createSearchIndex();
+        searchIndexReferenced.setName(searchIndexReferencedName);
+
+        final Expression otherSearchIndexReferencedName = ExpressionFactory.eINSTANCE.createExpression();
+        otherSearchIndexReferencedName.setName("myOtherSearchIndexReferenced");
+        otherSearchIndexReferencedName.setContent(otherSearchIndexReferencedName.getName());
+
+        final SearchIndex otherSearchIndexReferenced = ProcessFactory.eINSTANCE.createSearchIndex();
+        otherSearchIndexReferenced.setName(otherSearchIndexReferencedName);
+
+        filter.setElementToDisplay(searchIndexReferenced);
+
+        expressionWithReferencies.getReferencedElements().add(otherSearchIndexReferenced);
+        expressionWithReferencies.getReferencedElements().add(searchIndexReferenced);
+
+        // when
+        final boolean isSearchIndexReferenceFound = ModelHelper.isObjectIsReferencedInExpression(expressionWithReferencies, filter.getElementToDisplay());
+
+        // then
+        assertThat(isSearchIndexReferenceFound).isTrue();
+
+    }
+
+
 }
