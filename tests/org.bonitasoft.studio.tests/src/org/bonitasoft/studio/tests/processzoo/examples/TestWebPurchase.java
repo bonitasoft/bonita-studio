feb9667fb606797d98fd02f3e7d4a6bff9beace2
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-01-12 09:40:49 +0100

    refactor isTestingSP

--- tests/org.bonitasoft.studio.tests/src/org/bonitasoft/studio/tests/processzoo/examples/TestWebPurchase.java
+++ tests/org.bonitasoft.studio.tests/src/org/bonitasoft/studio/tests/processzoo/examples/TestWebPurchase.java
@@ -32,7 +32,6 @@ import org.bonitasoft.studio.model.process.Pool;
 import org.bonitasoft.studio.properties.i18n.Messages;
 import org.bonitasoft.studio.test.swtbot.util.SWTBotTestUtil;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
@@ -62,11 +61,11 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 	@Test
 	public void testWebPurchase() throws Exception{
 		SWTBotTestUtil.createNewDiagram(bot);
-		SWTBotEditor botEditor = bot.activeEditor();
+		final SWTBotEditor botEditor = bot.activeEditor();
 		diagramTitle=botEditor.getTitle();
 		final SWTBotGefEditor gmfEditor = bot.gefEditor(botEditor.getTitle());
 
-		SWTBotGefEditPart element =gmfEditor.getEditPart("Step1");
+		final SWTBotGefEditPart element =gmfEditor.getEditPart("Step1");
 		element.click();
 		gmfEditor.clickContextMenu("Delete");
 		gmfEditor.click(200,200);
@@ -86,7 +85,7 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		runProcess(gmfEditor);
 	}
 
-	private void configurePool(SWTBotGefEditor gmfEditor){
+	private void configurePool(final SWTBotGefEditor gmfEditor){
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_PROCESS_GENERAL).show();
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_PROCESS_GENERAL).setFocus();
 		SWTBotTestUtil.selectTabbedPropertyView(bot,"Pool");
@@ -103,8 +102,8 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		SWTBotTestUtil.addNewData(bot,"customerName","Text",false,null);
 		bot.button(Messages.Add).click();
 		SWTBotTestUtil.addNewData(bot,"customerPhone","Text",false,null);
-		Map<String,List<String>> options = new HashMap<String,List<String>>();
-		ArrayList<String> choices = new ArrayList<String>();
+		final Map<String,List<String>> options = new HashMap<String,List<String>>();
+		final ArrayList<String> choices = new ArrayList<String>();
 		choices.add("TV");
 		choices.add("Mobile Phone");
 		choices.add("Laptop");
@@ -114,12 +113,12 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		SWTBotTestUtil.selectTabbedPropertyView(bot, "Actors");
 		bot.button("Add").click();
 		bot.table().click(0,0);
-		SWTBotButton initatorButton = bot.button("Set as initiator");
+		final SWTBotButton initatorButton = bot.button("Set as initiator");
 		bot.waitUntil(Conditions.widgetIsEnabled(initatorButton));
 		initatorButton.click();
 	}
 
-	private void stepGateWayXor(SWTBotGefEditor gmfEditor){
+	private void stepGateWayXor(final SWTBotGefEditor gmfEditor){
 		//  SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Start1",1,new Point(200,100));
 		SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Start1",SWTBotTestUtil.CONTEXTUALPALETTE_GATEWAY,PositionConstants.EAST);
 		SWTBotTestUtil.selectTabbedPropertyView(bot,"General");
@@ -127,7 +126,7 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		SWTBotTestUtil.configureSequenceFlow(bot,"sequenceFlow1","Web Purchase", false, null, null);
 	}
 
-	private void stepSalesReview(SWTBotGefEditor gmfEditor){
+	private void stepSalesReview(final SWTBotGefEditor gmfEditor){
 		// SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Gateway1",0,new Point(350,100));
 		SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Gateway1",SWTBotTestUtil.CONTEXTUALPALETTE_STEP,PositionConstants.EAST);
 		SWTBotTestUtil.selectTabbedPropertyView(bot, "General");
@@ -138,8 +137,8 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		bot.radio(useTaskActors).click();
 		bot.comboBoxWithLabel(selectActorTitle).setSelection(0);
 		SWTBotTestUtil.selectTabbedPropertyView(bot, "Data");
-		Map<String,List<String>> options = new HashMap<String,List<String>>();
-		List<String> choices = new ArrayList<String>();
+		final Map<String,List<String>> options = new HashMap<String,List<String>>();
+		final List<String> choices = new ArrayList<String>();
 		choices.add("Approve");
 		choices.add("Reject");
 		choices.add("More Info");
@@ -152,15 +151,15 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		SWTBotTestUtil.configureSequenceFlow(bot,"customerOrder","Web Purchase", false, "true", ExpressionConstants.SCRIPT_TYPE);
 	}
 
-	private void createSalesReviewForm(SWTBotGefEditor gmfEditor){
+	private void createSalesReviewForm(final SWTBotGefEditor gmfEditor){
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION).show();
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION).setFocus();
 		SWTBotTestUtil.selectTabbedPropertyView(bot, PAGEFLOW_LABEL);
-		SWTBotView properties = bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION);
+		final SWTBotView properties = bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION);
 		properties.bot().button("Add...").click();
 		bot.waitUntil(Conditions.shellIsActive("Add form..."));
 		bot.checkBox("Select all").select();
-		SWTBotShell activeShell = bot.activeShell();
+		final SWTBotShell activeShell = bot.activeShell();
 		bot.button(IDialogConstants.FINISH_LABEL).click();
 		bot.waitUntil(Conditions.shellCloses(activeShell));
 
@@ -218,15 +217,15 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 	}
 
 
-	private void createMoreInfoForm(SWTBotGefEditor gmfEditor){
+	private void createMoreInfoForm(final SWTBotGefEditor gmfEditor){
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION).show();
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION).setFocus();
 		SWTBotTestUtil.selectTabbedPropertyView(bot, PAGEFLOW_LABEL);
-		SWTBotView properties = bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION);
+		final SWTBotView properties = bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION);
 		properties.bot().button("Add...").click();
 		bot.waitUntil(Conditions.shellIsActive("Add form..."));
 		bot.checkBox("Select all").select();
-		SWTBotShell activeShell = bot.activeShell();
+		final SWTBotShell activeShell = bot.activeShell();
 		bot.button(IDialogConstants.FINISH_LABEL).click();
 		bot.waitUntil(Conditions.shellCloses(activeShell));
 		bot.viewById(SWTBotTestUtil.VIEWS_TREE_OVERVIEW).show();
@@ -259,7 +258,7 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		bot.checkBox(2).select();
 	}
 
-	private void stepPay(SWTBotGefEditor gmfEditor){
+	private void stepPay(final SWTBotGefEditor gmfEditor){
 		bot.editorByTitle(diagramTitle).show();
 		bot.editorByTitle(diagramTitle).setFocus();
 		SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Sales Review",SWTBotTestUtil.CONTEXTUALPALETTE_STEP,PositionConstants.EAST);
@@ -284,25 +283,25 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		SWTBotTestUtil.configureSequenceFlow(bot,"if approved","Web Purchase", false, "decision==\"Approve\"", ExpressionConstants.SCRIPT_TYPE);
 	}
 
-	private void createPayForm(SWTBotGefEditor gmfEditor){
+	private void createPayForm(final SWTBotGefEditor gmfEditor){
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION).show();
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION).setFocus();
 		SWTBotTestUtil.selectTabbedPropertyView(bot, PAGEFLOW_LABEL);
-		SWTBotView properties = bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION);
+		final SWTBotView properties = bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_APPLICATION);
 		properties.bot().button("Add...").click();
 		bot.waitUntil(Conditions.shellIsActive("Add form..."));
 		if(SWTBotTestUtil.testingBosSp()){
-			SWTBotButton nextButton = bot.button(IDialogConstants.NEXT_LABEL);
+			final SWTBotButton nextButton = bot.button(IDialogConstants.NEXT_LABEL);
 			bot.waitUntil(Conditions.widgetIsEnabled(nextButton));
 			nextButton.click();
 		}
 		bot.tree().getAllItems()[0].check();
 		bot.tree().getAllItems()[1].check();
 		bot.tree().getAllItems()[2].check();
-		SWTBotShell activeShell = bot.activeShell();
+		final SWTBotShell activeShell = bot.activeShell();
 		bot.button(IDialogConstants.FINISH_LABEL).click();
 		bot.waitUntil(Conditions.shellCloses(activeShell));
-		SWTBotGefEditor formEditor =  bot.gefEditor(bot.activeEditor().getTitle());
+		final SWTBotGefEditor formEditor =  bot.gefEditor(bot.activeEditor().getTitle());
 		formEditor.activateTool("Message");
 		formEditor.click( 500, 90);
 		bot.viewById(SWTBotTestUtil.VIEWS_PROPERTIES_FORM_GENERAL).show();
@@ -318,7 +317,7 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 	}
 
 
-	private void stepReject(SWTBotGefEditor gmfEditor){
+	private void stepReject(final SWTBotGefEditor gmfEditor){
 		SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Sales Review",SWTBotTestUtil.CONTEXTUALPALETTE_STEP,PositionConstants.SOUTH_EAST);
 		//  SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Sales Review",0,new Point(550,180));
 		SWTBotTestUtil.selectTabbedPropertyView(bot, "General");
@@ -327,7 +326,7 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		SWTBotTestUtil.configureSequenceFlow(bot,"if not approved","Web Purchase", false, "decision==\"Reject\"", ExpressionConstants.SCRIPT_TYPE);
 	}
 
-	private void stepExpressDelivery(SWTBotGefEditor gmfEditor){
+	private void stepExpressDelivery(final SWTBotGefEditor gmfEditor){
 		SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor,"Pay",SWTBotTestUtil.CONTEXTUALPALETTE_STEP,PositionConstants.EAST);
 		// SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Pay",0,new Point(800,100));
 		SWTBotTestUtil.selectTabbedPropertyView(bot, "General");
@@ -336,7 +335,7 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		SWTBotTestUtil.configureSequenceFlow(bot,"if yes","Web Purchase", false, "chooseExpressDelivery", ExpressionConstants.VARIABLE_TYPE);
 	}
 
-	private void stepArchive(SWTBotGefEditor gmfEditor){
+	private void stepArchive(final SWTBotGefEditor gmfEditor){
 		//  SWTBotTestUtil.increasePoolWidth(gmfEditor, "Web Purchase");
 		SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor,"Pay", SWTBotTestUtil.CONTEXTUALPALETTE_STEP,PositionConstants.SOUTH_EAST);
 		//        SWTBotTestUtil.selectElementInContextualPaletteAndDragIt(gmfEditor, "Pay",0,new Point(550,155));
@@ -350,20 +349,20 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		SWTBotTestUtil.configureSequenceFlow(bot,"sequenceFlow4","Web Purchase", false, null, null);
 	}
 
-	private void finalEvent(SWTBotGefEditor gmfEditor){
+	private void finalEvent(final SWTBotGefEditor gmfEditor){
 		gmfEditor.activateTool("End");
-		Point targetLocation = SWTBotTestUtil.computeTargetLocation(gmfEditor,"Archive",PositionConstants.EAST);
+		final Point targetLocation = SWTBotTestUtil.computeTargetLocation(gmfEditor,"Archive",PositionConstants.EAST);
 		gmfEditor.click(targetLocation.x,targetLocation.y);
 		SWTBotTestUtil.addSequenceFlow(bot, gmfEditor, "Archive", "End1",PositionConstants.WEST);
 		SWTBotTestUtil.configureSequenceFlow(bot,"sequenceFlow4","Web Purchase", false, null, ExpressionConstants.VARIABLE_TYPE);
 	}
 
-	private void configureProcess(SWTBotGefEditor gmfEditor){
-		IGraphicalEditPart part = (IGraphicalEditPart)gmfEditor.mainEditPart().part();
-		MainProcess model = (MainProcess)part.resolveSemanticElement();
-		Pool pool = (Pool)model.getElements().get(0);
-		String processLabel = pool.getName() +" ("+pool.getVersion()+")";
-		if(testingBosSp()){
+	private void configureProcess(final SWTBotGefEditor gmfEditor){
+		final IGraphicalEditPart part = (IGraphicalEditPart)gmfEditor.mainEditPart().part();
+		final MainProcess model = (MainProcess)part.resolveSemanticElement();
+		final Pool pool = (Pool)model.getElements().get(0);
+		final String processLabel = pool.getName() +" ("+pool.getVersion()+")";
+        if (SWTBotTestUtil.testingBosSp()) {
 			bot.toolbarDropDownButton("Configure").click();
 		}else{
 			bot.toolbarButton("Configure").click();
@@ -378,12 +377,8 @@ public class TestWebPurchase extends SWTBotGefTestCase implements SWTBotConstant
 		bot.button(IDialogConstants.FINISH_LABEL).click();
 	}
 
-	private void runProcess(SWTBotGefEditor gmfEditor) throws Exception{
-		IStatus status = SWTBotTestUtil.selectAndRunFirstPoolFound(bot);
+	private void runProcess(final SWTBotGefEditor gmfEditor) throws Exception{
+		final IStatus status = SWTBotTestUtil.selectAndRunFirstPoolFound(bot);
 		assertTrue(status.getMessage(), status.isOK());
-	}
-
-	private boolean testingBosSp() {
-		return Platform.getBundle("org.bonitasoft.studioEx.console.libs") != null;
 	}
 }
