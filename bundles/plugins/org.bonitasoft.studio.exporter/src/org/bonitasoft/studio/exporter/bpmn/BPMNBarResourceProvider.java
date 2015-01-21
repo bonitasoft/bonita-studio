6aa88fa78f444c936882c7dbb71fa63e11ad0d7e
apupier <aurelien.pupier@bonitasoft.com>
2015-01-21 15:50:03 +0100

    BS-12069: with new Monitoring features, the bpmn file is required so try to export it everytime

--- bundles/plugins/org.bonitasoft.studio.exporter/src/org/bonitasoft/studio/exporter/bpmn/BPMNBarResourceProvider.java
+++ bundles/plugins/org.bonitasoft.studio.exporter/src/org/bonitasoft/studio/exporter/bpmn/BPMNBarResourceProvider.java
@@ -1,22 +1,19 @@
 /**
- * Copyright (C) 2014 Bonitasoft S.A.
+ * Copyright (C) 2014-2015 Bonitasoft S.A.
  * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.exporter.bpmn;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -28,7 +25,6 @@ import org.bonitasoft.studio.model.process.AbstractProcess;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.transaction.RunnableWithResult;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.PlatformUI;
 
 
 public class BPMNBarResourceProvider implements BARResourcesProvider {
@@ -36,12 +32,9 @@ public class BPMNBarResourceProvider implements BARResourcesProvider {
     @Override
     public List<BarResource> addResourcesForConfiguration(final BusinessArchiveBuilder builder, final AbstractProcess process, final Configuration configuration,
             final Set<EObject> excludedObject) throws Exception {
-        if(PlatformUI.isWorkbenchRunning()){
-            final RunnableWithResult<List<BarResource>> runnableWithResult = new AddBpmnBarResourceRunnable(builder, process);
-            Display.getDefault().syncExec(runnableWithResult);
-            return runnableWithResult.getResult();
-        }
-        return Collections.emptyList();
+        final RunnableWithResult<List<BarResource>> runnableWithResult = new AddBpmnBarResourceRunnable(builder, process);
+        Display.getDefault().syncExec(runnableWithResult);
+        return runnableWithResult.getResult();
     }
 
 }
