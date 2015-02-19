dc0e010057ea2e1ceab7b7849284613bb5a45b65
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-02-19 18:24:18 +0100

    remove fest assert and powermock dependencies

--- bundles/plugins/org.bonitasoft.studio.exporter/src-test/java/org/bonitasoft/studio/exporter/bpmn/transfo/BonitaToBPMNTest.java
+++ bundles/plugins/org.bonitasoft.studio.exporter/src-test/java/org/bonitasoft/studio/exporter/bpmn/transfo/BonitaToBPMNTest.java
@@ -5,12 +5,10 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,6 +20,7 @@ import java.math.BigInteger;
 
 import javax.xml.namespace.QName;
 
+import org.assertj.core.api.Assertions;
 import org.bonitasoft.studio.common.emf.tools.ExpressionHelper;
 import org.bonitasoft.studio.model.process.Activity;
 import org.bonitasoft.studio.model.process.Data;
@@ -31,7 +30,6 @@ import org.bonitasoft.studio.model.process.ProcessFactory;
 import org.bonitasoft.studio.model.process.Task;
 import org.bonitasoft.studio.model.process.builders.XMLDataBuilder;
 import org.bonitasoft.studio.model.process.builders.XMLDataTypeBuilder;
-import org.fest.assertions.Assertions;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -40,7 +38,6 @@ import org.omg.spec.bpmn.model.TFormalExpression;
 import org.omg.spec.bpmn.model.TMultiInstanceLoopCharacteristics;
 import org.omg.spec.bpmn.model.TStandardLoopCharacteristics;
 import org.omg.spec.bpmn.model.TTask;
-
 
 public class BonitaToBPMNTest extends BonitaToBPMN {
 
@@ -113,7 +110,6 @@ public class BonitaToBPMNTest extends BonitaToBPMN {
     public void testMultiInstanceParallelWithMinimalAttributesFilled() {
         final Activity bonitaActivity = ProcessFactory.eINSTANCE.createActivity();
         bonitaActivity.setType(MultiInstanceType.PARALLEL);
-
 
         final TActivity bpmTask = (TActivity) bonitaToBPMN.createActivity(bonitaActivity);
         final TMultiInstanceLoopCharacteristics loopCharacteristics = (TMultiInstanceLoopCharacteristics) bpmTask.getLoopCharacteristics();
