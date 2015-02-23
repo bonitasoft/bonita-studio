f915006f6dcd720aef42e0d0a551e6fee6fd51f2
apupier <aurelien.pupier@bonitasoft.com>
2015-02-23 15:29:33 +0100

    BS-12330: apply code style

--- bundles/plugins/org.bonitasoft.studio.exporter/src-test/java/org/bonitasoft/studio/exporter/bpmn/transfo/BonitaToBPMNTest.java
+++ bundles/plugins/org.bonitasoft.studio.exporter/src-test/java/org/bonitasoft/studio/exporter/bpmn/transfo/BonitaToBPMNTest.java
@@ -1,16 +1,14 @@
 /**
- * Copyright (C) 2014 Bonitasoft S.A.
+ * Copyright (C) 2014-2015 Bonitasoft S.A.
  * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -19,13 +17,13 @@ package org.bonitasoft.studio.exporter.bpmn.transfo;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.any;
-import java.lang.reflect.InvocationTargetException;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -43,29 +41,13 @@ import org.bonitasoft.studio.model.process.ProcessFactory;
 import org.bonitasoft.studio.model.process.Task;
 import org.bonitasoft.studio.model.process.builders.XMLDataBuilder;
 import org.bonitasoft.studio.model.process.builders.XMLDataTypeBuilder;
-import org.bonitasoft.studio.model.process.diagram.edit.parts.SequenceFlowEditPart;
 import org.bonitasoft.studio.model.process.diagram.edit.parts.SequenceFlowNameEditPart;
 import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.geometry.Rectangle;
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EOperation;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.FeatureMap;
 import org.eclipse.gef.EditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.ConnectionNodeEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeNodeEditPart;
-import org.eclipse.gmf.runtime.notation.LayoutConstraint;
 import org.eclipse.gmf.runtime.notation.Location;
 import org.eclipse.gmf.runtime.notation.Node;
-import org.eclipse.gmf.runtime.notation.View;
 import org.fest.assertions.Assertions;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -77,25 +59,17 @@ import org.omg.spec.bpmn.di.BPMNLabel;
 import org.omg.spec.bpmn.di.BPMNLabelStyle;
 import org.omg.spec.bpmn.di.BPMNShape;
 import org.omg.spec.bpmn.di.DiFactory;
-import org.omg.spec.bpmn.di.MessageVisibleKind;
 import org.omg.spec.bpmn.model.TActivity;
 import org.omg.spec.bpmn.model.TFormalExpression;
 import org.omg.spec.bpmn.model.TMultiInstanceLoopCharacteristics;
 import org.omg.spec.bpmn.model.TStandardLoopCharacteristics;
 import org.omg.spec.bpmn.model.TTask;
 import org.omg.spec.dd.dc.Font;
-import org.omg.spec.dd.dc.Point;
-import org.omg.spec.dd.di.ExtensionType;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class BonitaToBPMNTest extends BonitaToBPMN {
 
-	
-    
-	private BonitaToBPMN bonitaToBPMN;
-   
-
+    private BonitaToBPMN bonitaToBPMN;
 
     @Before
     public void setUp() throws Exception {
@@ -165,7 +139,6 @@ public class BonitaToBPMNTest extends BonitaToBPMN {
         final Activity bonitaActivity = ProcessFactory.eINSTANCE.createActivity();
         bonitaActivity.setType(MultiInstanceType.PARALLEL);
 
-
         final TActivity bpmTask = (TActivity) bonitaToBPMN.createActivity(bonitaActivity);
         final TMultiInstanceLoopCharacteristics loopCharacteristics = (TMultiInstanceLoopCharacteristics) bpmTask.getLoopCharacteristics();
         Assertions.assertThat(loopCharacteristics).isNotNull();
@@ -183,61 +156,60 @@ public class BonitaToBPMNTest extends BonitaToBPMN {
         assertThat(ref.getLocalPart()).isEqualTo("n0:null");
         assertThat(ref2.getLocalPart()).isEqualTo("n1:null");
     }
-    
+
     @Test
-    public void should_set_sequenceFlow_label(){
-    	SequenceFlowNameEditPart labelPart=mock(SequenceFlowNameEditPart.class);
-    	Node node = mock(Node.class);
-    	Figure figure = mock(Figure.class);
-    	when(labelPart.getNotationView()).thenReturn(node);
-    	when(labelPart.getFigure()).thenReturn(figure);
-    	Rectangle rectangle = new Rectangle(15,30,50,200);
-    	when(figure.getBounds()).thenReturn(rectangle);
-    	Location location = mock(Location.class);
-    	when(node.getLayoutConstraint()).thenReturn(location);
-    	BPMNEdge edge = DiFactory.eINSTANCE.createBPMNEdge();
-    	bonitaToBPMN.setSequenceFLowLabel(labelPart, edge);
-    	assertNotNull(edge.getBPMNLabel());
-    	
+    public void should_set_sequenceFlow_label() {
+        final SequenceFlowNameEditPart labelPart = mock(SequenceFlowNameEditPart.class);
+        final Node node = mock(Node.class);
+        final Figure figure = mock(Figure.class);
+        when(labelPart.getNotationView()).thenReturn(node);
+        when(labelPart.getFigure()).thenReturn(figure);
+        final Rectangle rectangle = new Rectangle(15, 30, 50, 200);
+        when(figure.getBounds()).thenReturn(rectangle);
+        final Location location = mock(Location.class);
+        when(node.getLayoutConstraint()).thenReturn(location);
+        final BPMNEdge edge = DiFactory.eINSTANCE.createBPMNEdge();
+        bonitaToBPMN.setSequenceFLowLabel(labelPart, edge);
+        assertNotNull(edge.getBPMNLabel());
     }
-    
+
     @Test
-    public void should_set_event_label(){
-    	CustomEventLabelEditPart labelPart=mock(CustomEventLabelEditPart.class);
-    	Node node = mock(Node.class);
-    	Figure figure = mock(Figure.class);
-    	when(labelPart.getNotationView()).thenReturn(node);
-    	when(labelPart.getFigure()).thenReturn(figure);
-    	Rectangle rectangle = new Rectangle(15,30,50,200);
-    	when(figure.getBounds()).thenReturn(rectangle);
-    	Location location = mock(Location.class);
-    	when(node.getLayoutConstraint()).thenReturn(location);
-    	when(location.getX()).thenReturn(50);
-    	when(location.getY()).thenReturn(200);
-    	BPMNLabel label = DiFactory.eINSTANCE.createBPMNLabel();
-    	bonitaToBPMN.setEventLabelBounds(labelPart, label);
-    	assertEquals(50,(int)label.getBounds().getX());
-    	assertEquals(200,(int)label.getBounds().getY());
-    	
+    public void should_set_event_label() {
+        final CustomEventLabelEditPart labelPart = mock(CustomEventLabelEditPart.class);
+        final Node node = mock(Node.class);
+        final Figure figure = mock(Figure.class);
+        when(labelPart.getNotationView()).thenReturn(node);
+        when(labelPart.getFigure()).thenReturn(figure);
+        final Rectangle rectangle = new Rectangle(15, 30, 50, 200);
+        when(figure.getBounds()).thenReturn(rectangle);
+        final Location location = mock(Location.class);
+        when(node.getLayoutConstraint()).thenReturn(location);
+        when(location.getX()).thenReturn(50);
+        when(location.getY()).thenReturn(200);
+        final BPMNLabel label = DiFactory.eINSTANCE.createBPMNLabel();
+        bonitaToBPMN.setEventLabelBounds(labelPart, label);
+        assertEquals(50, (int) label.getBounds().getX());
+        assertEquals(200, (int) label.getBounds().getY());
+
     }
-    
+
     @Test
-    public void shouldCreateBPMNShape(){
-    ShapeNodeEditPart shapePart = mock(ShapeNodeEditPart.class);
-    CustomEventLabelEditPart labelPart=mock(CustomEventLabelEditPart.class);
-    ArrayList<EditPart> children = new ArrayList<EditPart>();
-    children.add(labelPart);
-    when(shapePart.getChildren()).thenReturn(children);
-    Font font = mock(Font.class);
-    BPMNLabelStyle labelStyle = DiFactory.eINSTANCE.createBPMNLabelStyle();
-    labelStyle.setId("myLabelStyle");
-    doReturn(font).when(bonitaToBPMN).createFont(shapePart);
-    doReturn(labelStyle).when(bonitaToBPMN).getLabelStyle(font);
-    Map<String,String> map = new HashMap<String,String>();
-    doReturn(map).when(bonitaToBPMN).getShapeColors(shapePart);
-    doNothing().when(bonitaToBPMN).setEventLabelBounds(any(CustomEventLabelEditPart.class), any(BPMNLabel.class));
-    BPMNShape bpmnShape = bonitaToBPMN.createBPMNShape(shapePart);
-    assertNotNull(bpmnShape);
-    assertNotNull(bpmnShape.getBPMNLabel());
+    public void shouldCreateBPMNShape() {
+        final ShapeNodeEditPart shapePart = mock(ShapeNodeEditPart.class);
+        final CustomEventLabelEditPart labelPart = mock(CustomEventLabelEditPart.class);
+        final ArrayList<EditPart> children = new ArrayList<EditPart>();
+        children.add(labelPart);
+        when(shapePart.getChildren()).thenReturn(children);
+        final Font font = mock(Font.class);
+        final BPMNLabelStyle labelStyle = DiFactory.eINSTANCE.createBPMNLabelStyle();
+        labelStyle.setId("myLabelStyle");
+        doReturn(font).when(bonitaToBPMN).createFont(shapePart);
+        doReturn(labelStyle).when(bonitaToBPMN).getLabelStyle(font);
+        final Map<String, String> map = new HashMap<String, String>();
+        doReturn(map).when(bonitaToBPMN).getShapeColors(shapePart);
+        doNothing().when(bonitaToBPMN).setEventLabelBounds(any(CustomEventLabelEditPart.class), any(BPMNLabel.class));
+        final BPMNShape bpmnShape = bonitaToBPMN.createBPMNShape(shapePart);
+        assertNotNull(bpmnShape);
+        assertNotNull(bpmnShape.getBPMNLabel());
     }
 }
