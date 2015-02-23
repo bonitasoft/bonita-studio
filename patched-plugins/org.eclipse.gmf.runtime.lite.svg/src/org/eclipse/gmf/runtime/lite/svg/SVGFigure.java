5ece13c43698c3f6baf06744bed2d12637145925
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-02-23 08:43:36 +0100

    Revert "move to tycho 0.22"
    This reverts commit 5488493bc0c8f03c8d40cd814960c3380273ef44.

--- patched-plugins/org.eclipse.gmf.runtime.lite.svg/src/org/eclipse/gmf/runtime/lite/svg/SVGFigure.java
+++ patched-plugins/org.eclipse.gmf.runtime.lite.svg/src/org/eclipse/gmf/runtime/lite/svg/SVGFigure.java
@@ -1,11 +1,13 @@
 /**
  * Copyright (c) 2008 Borland Software Corporation
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Contributors:
- * Dmitry Stadnik - initial API and implementation
+ *    Dmitry Stadnik - initial API and implementation
  */
 package org.eclipse.gmf.runtime.lite.svg;
 
@@ -68,6 +70,7 @@ public class SVGFigure extends Figure {
             loadDocument();
         }
     }
+
 
     private void loadDocument() {
         transcoder = null;
@@ -137,6 +140,8 @@ public class SVGFigure extends Figure {
         }
         return transcoder == null ? null : transcoder.getDocument();
     }
+
+
 
     /**
      * Returns true if document was loaded without errors; tries to load document if needed.
