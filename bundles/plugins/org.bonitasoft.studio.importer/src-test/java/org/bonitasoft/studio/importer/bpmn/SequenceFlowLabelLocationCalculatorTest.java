abf58ede8b2d67079cea05fda1128e671ce46da4
apupier <aurelien.pupier@bonitasoft.com>
2015-02-26 10:23:34 +0100

    Add missing header

--- bundles/plugins/org.bonitasoft.studio.importer/src-test/java/org/bonitasoft/studio/importer/bpmn/SequenceFlowLabelLocationCalculatorTest.java
+++ bundles/plugins/org.bonitasoft.studio.importer/src-test/java/org/bonitasoft/studio/importer/bpmn/SequenceFlowLabelLocationCalculatorTest.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2015 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.bonitasoft.studio.importer.bpmn;
 
 import static org.junit.Assert.assertEquals;
@@ -10,9 +24,6 @@ import org.omg.spec.bpmn.di.DiFactory;
 import org.omg.spec.dd.dc.Bounds;
 import org.omg.spec.dd.dc.DcFactory;
 import org.omg.spec.dd.dc.Point;
-;
-
-
 
 public class SequenceFlowLabelLocationCalculatorTest {
 
@@ -230,6 +241,5 @@ public class SequenceFlowLabelLocationCalculatorTest {
         assertEquals(0,labelLocation.x());
         assertEquals(300, labelLocation.y());
     }
-
 
 }
