f82fec773a2b27e59fe654e412b24fe481e363da
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-03-20 11:51:52 +0100

    remove unit test

--- bundles/plugins/org.bonitasoft.studio.application/src-test/java/org/bonitasoft/studio/application/BonitaStudioApplicationTest.java
+++ bundles/plugins/org.bonitasoft.studio.application/src-test/java/org/bonitasoft/studio/application/BonitaStudioApplicationTest.java
@@ -5,12 +5,10 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -42,14 +40,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-
 /**
  * @author Romain Bioteau
- *
  */
 @RunWith(MockitoJUnitRunner.class)
 public class BonitaStudioApplicationTest extends AbstractSWTTestCase {
-
 
     private BonitaStudioApplication application;
 
@@ -93,11 +88,6 @@ public class BonitaStudioApplicationTest extends AbstractSWTTestCase {
 
         verify(application).openErrorDialog(display, "1.9");
         verify(application, never()).createAndRunWorkbench(display);
-    }
-
-    @Test
-    public void should_createWorkbenchAdvisor_return_a_BonitaStudioWorkbenchAdvisor() throws Exception {
-        assertThat(application.createWorkbenchAdvisor()).isInstanceOf(BonitaStudioWorkbenchAdvisor.class);
     }
 
     @Test
