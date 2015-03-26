80cdce0a1fb3ace99a0c5c166e4ad4f5e7662172
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-03-26 14:19:27 +0100

    BS-12578 filter business data without simple fields

--- bundles/plugins/org.bonitasoft.studio.properties/src/org/bonitasoft/studio/properties/sections/forms/wizard/SelectFormWizard.java
+++ bundles/plugins/org.bonitasoft.studio.properties/src/org/bonitasoft/studio/properties/sections/forms/wizard/SelectFormWizard.java
@@ -5,32 +5,36 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.properties.sections.forms.wizard;
+
+import static com.google.common.base.Predicates.instanceOf;
+import static com.google.common.collect.Iterables.find;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.bonitasoft.engine.bdm.model.BusinessObject;
+import org.bonitasoft.engine.bdm.model.field.SimpleField;
+import org.bonitasoft.studio.businessobject.core.repository.BusinessObjectModelFileStore;
 import org.bonitasoft.studio.businessobject.core.repository.BusinessObjectModelRepositoryStore;
 import org.bonitasoft.studio.common.NamingUtils;
 import org.bonitasoft.studio.common.emf.tools.ModelHelper;
 import org.bonitasoft.studio.common.log.BonitaStudioLog;
 import org.bonitasoft.studio.common.repository.Repository;
-import org.bonitasoft.studio.common.repository.RepositoryManager;
 import org.bonitasoft.studio.data.provider.DataExpressionProvider;
 import org.bonitasoft.studio.diagram.form.custom.commands.CreateFormCommand;
 import org.bonitasoft.studio.diagram.form.custom.model.WidgetMapping;
 import org.bonitasoft.studio.model.form.Form;
 import org.bonitasoft.studio.model.process.AbstractProcess;
+import org.bonitasoft.studio.model.process.BusinessObjectData;
 import org.bonitasoft.studio.model.process.Data;
 import org.bonitasoft.studio.model.process.Element;
 import org.bonitasoft.studio.model.process.MultiInstanceType;
@@ -51,66 +55,96 @@ import org.eclipse.jface.wizard.Wizard;
 
 public class SelectFormWizard extends Wizard {
 
-
     protected Element pageFlow;
     protected TransactionalEditingDomain editingDomain;
-    protected EStructuralFeature feature;
+    protected EStructuralFeature pageFlowFormFeature;
+    protected final BusinessObjectModelRepositoryStore businessObjectStore;
 
-    public SelectFormWizard(final Element element, final EStructuralFeature feature, final TransactionalEditingDomain editingDomain) {
+    public SelectFormWizard(final Element element, final EStructuralFeature pageFlowFormFeature, final TransactionalEditingDomain editingDomain,
+            final BusinessObjectModelRepositoryStore businessObjectStore) {
         pageFlow = element;
+        this.businessObjectStore = businessObjectStore;
         this.editingDomain = editingDomain;
-        this.feature = feature;
+        this.pageFlowFormFeature = pageFlowFormFeature;
         setWindowTitle(Messages.addFormTitle);
         setDefaultPageImageDescriptor(Pics.getWizban());
     }
 
     @Override
     public void addPages() {
-        final SelectGeneratedWidgetsWizardPage selectGeneratedWidgetsWizardPage = createSelectGeneratedWidgetsWizardPage(generateDefaultFormName(),getAccessibleModelElements());
+        final SelectGeneratedWidgetsWizardPage selectGeneratedWidgetsWizardPage = createSelectGeneratedWidgetsWizardPage(generateDefaultFormName(),
+                getAccessibleModelElements(pageFlow, pageFlowFormFeature));
         addPage(selectGeneratedWidgetsWizardPage);
     }
 
     protected SelectGeneratedWidgetsWizardPage createSelectGeneratedWidgetsWizardPage(final String defaultFormName, final List<EObject> inputElements) {
         final SelectGeneratedWidgetsWizardPage page = new SelectGeneratedWidgetsWizardPage(pageFlow, defaultFormName, inputElements,
-                getBusinessObjectRepositoryStore());
+                businessObjectStore);
         page.setTitle(Messages.createForm_title);
         page.setDescription(Messages.createForm_desc);
         return page;
     }
 
-    protected BusinessObjectModelRepositoryStore getBusinessObjectRepositoryStore() {
-        return RepositoryManager.getInstance().getRepositoryStore(BusinessObjectModelRepositoryStore.class);
-    }
-
-    protected List<EObject> getAccessibleModelElements() {
+    protected List<EObject> getAccessibleModelElements(final Element container, final EStructuralFeature pageFlowFormFeature) {
         final List<EObject> elements = new ArrayList<EObject>();
-        if(pageFlow instanceof PageFlow){
-            final List<Data> allData = ModelHelper.getAccessibleDataInFormsWithNoRestriction(pageFlow, feature);
+        if (container instanceof PageFlow) {
+            final List<Data> allData = ModelHelper.getAccessibleDataInFormsWithNoRestriction(container, pageFlowFormFeature);
             for (final Data currentData : allData) {
                 final EClass eClassData = currentData.getDataType().eClass();
-                if(!ProcessPackage.eINSTANCE.getJavaType().isSuperTypeOf(eClassData)
-                        && !ProcessPackage.eINSTANCE.getXMLType().isSuperTypeOf(eClassData)
-                        && !(ProcessPackage.eINSTANCE.getBusinessObjectType().isSuperTypeOf(eClassData) && currentData.isMultiple())) {
+                if (!aJavaObjectData(eClassData)
+                        && !anXmlData(eClassData)
+                        && !aMultipleBusinessData(currentData)
+                        && !aBusinessDataWithoutSimpleFields(currentData)) {
                     elements.add(currentData);
                 }
             }
-            if (pageFlow instanceof MultiInstantiable) {
-                if (isDataMultiInstantiated((MultiInstantiable) pageFlow)) {
-                    final Data dataFromIteratorExpression = DataExpressionProvider.dataFromIteratorExpression((MultiInstantiable) pageFlow,
-                            ((MultiInstantiable) pageFlow).getIteratorExpression());
-                    if (!ProcessPackage.eINSTANCE.getJavaType().isSuperTypeOf(dataFromIteratorExpression.getDataType().eClass())
-                            && !ProcessPackage.eINSTANCE.getXMLType().isSuperTypeOf(dataFromIteratorExpression.getDataType().eClass())) {
-                        elements.add(dataFromIteratorExpression);
-                    }
-                }
+            if (container instanceof MultiInstantiable) {
+                addMultiInstanceIterator(container, elements);
             }
         }
-        final AbstractProcess parentProcess = ModelHelper.getParentProcess(pageFlow);
-        if(parentProcess instanceof Pool){
+        final AbstractProcess parentProcess = ModelHelper.getParentProcess(container);
+        if (parentProcess instanceof Pool) {
             elements.addAll(((Pool) parentProcess).getDocuments());
         }
 
         return elements;
+    }
+
+    private void addMultiInstanceIterator(final Element container, final List<EObject> elements) {
+        if (isDataMultiInstantiated((MultiInstantiable) container)) {
+            final Data dataFromIteratorExpression = DataExpressionProvider.dataFromIteratorExpression((MultiInstantiable) container,
+                    ((MultiInstantiable) container).getIteratorExpression());
+            final EClass eClassData = dataFromIteratorExpression.getDataType().eClass();
+            if (!aJavaObjectData(eClassData)
+                    && !anXmlData(eClassData)) {
+                elements.add(dataFromIteratorExpression);
+            }
+        }
+    }
+
+    private boolean aJavaObjectData(final EClass eClassData) {
+        return ProcessPackage.eINSTANCE.getJavaType().isSuperTypeOf(eClassData);
+    }
+
+    private boolean anXmlData(final EClass eClassData) {
+        return ProcessPackage.eINSTANCE.getXMLType().isSuperTypeOf(eClassData);
+    }
+
+    private boolean aBusinessDataWithoutSimpleFields(final Data currentData) {
+        return currentData instanceof BusinessObjectData && !hasSimpleField(((BusinessObjectData) currentData).getClassName());
+    }
+
+    private boolean hasSimpleField(final String className) {
+        final BusinessObjectModelFileStore modelFileStore = businessObjectStore.getChildByQualifiedName(className);
+        final BusinessObject bo = modelFileStore.getBusinessObject(className);
+        if (bo != null) {
+            return find(bo.getFields(), instanceOf(SimpleField.class), null) != null;
+        }
+        return true;
+    }
+
+    private boolean aMultipleBusinessData(final Data currentData) {
+        return currentData instanceof BusinessObjectData && currentData.isMultiple();
     }
 
     protected boolean isDataMultiInstantiated(final MultiInstantiable element) {
@@ -120,15 +154,15 @@ public class SelectFormWizard extends Wizard {
 
     protected String generateDefaultFormName() {
         final String baseName = pageFlow.getName();
-        final int i = ((List<?>) pageFlow.eGet(feature)).size();
-        for (final Iterator<?> iterator = ((List<?>) pageFlow.eGet(feature)).iterator(); iterator.hasNext();) {
+        final int i = ((List<?>) pageFlow.eGet(pageFlowFormFeature)).size();
+        for (final Iterator<?> iterator = ((List<?>) pageFlow.eGet(pageFlowFormFeature)).iterator(); iterator.hasNext();) {
             final Form form = (Form) iterator.next();
-            if(! form.getName().equals(baseName+i)){
-                return baseName+(i<=0?"":"_"+i);
+            if (!form.getName().equals(baseName + i)) {
+                return baseName + (i <= 0 ? "" : "_" + i);
             }
 
         }
-        return baseName+(i<=0?"":"_"+i);
+        return baseName + (i <= 0 ? "" : "_" + i);
     }
 
     @Override
@@ -141,9 +175,9 @@ public class SelectFormWizard extends Wizard {
         final String name = selectGeneratedWidgetsWizardPage.getFormName();
         selectGeneratedWidgetsWizardPage.setMessage(null);
         boolean allreadyExists = false;
-        for (final Iterator<?> iterator = ((List<?>)pageFlow.eGet(feature)).iterator(); iterator.hasNext();) {
+        for (final Iterator<?> iterator = ((List<?>) pageFlow.eGet(pageFlowFormFeature)).iterator(); iterator.hasNext();) {
             final Form form = (Form) iterator.next();
-            allreadyExists = form.getName().equals(NamingUtils.toJavaIdentifier(name,true));
+            allreadyExists = form.getName().equals(NamingUtils.toJavaIdentifier(name, true));
             if (allreadyExists) {
                 selectGeneratedWidgetsWizardPage.setMessage(Messages.error_allreadyExists, IMessageProvider.ERROR);
                 return false;
@@ -171,10 +205,11 @@ public class SelectFormWizard extends Wizard {
 
     protected CreateFormCommand getCreateFormCommand(final String name,
             final List<? extends WidgetMapping> widgesMappings) {
-        if(widgesMappings == null){
-            return new CreateFormCommand(pageFlow, feature, name, getSelectGeneratedWidgetsWizardPage().getFormDescription(), editingDomain);
-        }else{
-            return new CreateFormCommand(pageFlow, feature, name, getSelectGeneratedWidgetsWizardPage().getFormDescription(), widgesMappings, editingDomain);
+        if (widgesMappings == null) {
+            return new CreateFormCommand(pageFlow, pageFlowFormFeature, name, getSelectGeneratedWidgetsWizardPage().getFormDescription(), editingDomain);
+        } else {
+            return new CreateFormCommand(pageFlow, pageFlowFormFeature, name, getSelectGeneratedWidgetsWizardPage().getFormDescription(), widgesMappings,
+                    editingDomain);
         }
     }
 
