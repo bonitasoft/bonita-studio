0afa0f682c804c0b137ee1e9412e9403c15183ba
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-03-31 13:25:20 +0200

    fix unit test

--- bundles/plugins/org.bonitasoft.studio.data/src-test/java/org/bonitasoft/studio/data/operation/RefactorDataOperationTest.java
+++ bundles/plugins/org.bonitasoft.studio.data/src-test/java/org/bonitasoft/studio/data/operation/RefactorDataOperationTest.java
@@ -5,12 +5,10 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -41,7 +39,6 @@ import org.junit.Test;
 
 /**
  * @author Romain Bioteau
- *
  */
 public class RefactorDataOperationTest {
 
@@ -96,7 +93,7 @@ public class RefactorDataOperationTest {
         final RefactorDataOperation refacorDataOperation = new RefactorDataOperation(RefactoringOperationType.UPDATE);
         refacorDataOperation.setAskConfirmation(false);// Skip UI
         refacorDataOperation.setEditingDomain(domain);
-        refacorDataOperation.setContainer(parentProcess);
+        refacorDataOperation.setDataContainer(parentProcess);
         refacorDataOperation.addItemToRefactor(newData, myData);
         refacorDataOperation.run(null);
         ExpressionAssert.assertThat(leftOperand).hasName(newData.getName());
@@ -120,7 +117,7 @@ public class RefactorDataOperationTest {
         final RefactorDataOperation refacorDataOperation = new RefactorDataOperation(RefactoringOperationType.REMOVE);
         refacorDataOperation.setAskConfirmation(false);// Skip UI
         refacorDataOperation.setEditingDomain(domain);
-        refacorDataOperation.setContainer(parentProcess);
+        refacorDataOperation.setDataContainer(parentProcess);
         refacorDataOperation.addItemToRefactor(null, myData);
         refacorDataOperation.run(null);
         ExpressionAssert.assertThat(rightOperand).hasContent("hello ${     }");
