8132397b62aa29b43dc0b009f6f2a3b88329aba9
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-04-09 15:09:32 +0200

    fix typo

--- bundles/plugins/org.bonitasoft.studio.pagedesigner/src-test/java/org/bonitasoft/studio/pagedesigner/ui/property/section/control/FormReferenceExpressionValidatorTest.java
+++ bundles/plugins/org.bonitasoft.studio.pagedesigner/src-test/java/org/bonitasoft/studio/pagedesigner/ui/property/section/control/FormReferenceExpressionValidatorTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 BonitaSoft S.A.
+ * Copyright (C) 2015 Bonitasoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
