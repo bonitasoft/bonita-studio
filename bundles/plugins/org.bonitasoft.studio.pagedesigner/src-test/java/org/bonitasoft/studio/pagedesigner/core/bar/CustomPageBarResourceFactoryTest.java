db7a3f2b8e4d7f8f1146c18d652f0cd9437ecd4f
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-04-17 14:12:42 +0200

    fix compilation issues

--- bundles/plugins/org.bonitasoft.studio.pagedesigner/src-test/java/org/bonitasoft/studio/pagedesigner/core/bar/CustomPageBarResourceFactoryTest.java
+++ bundles/plugins/org.bonitasoft.studio.pagedesigner/src-test/java/org/bonitasoft/studio/pagedesigner/core/bar/CustomPageBarResourceFactoryTest.java
@@ -18,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.doReturn;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.URL;
 
@@ -58,7 +59,7 @@ public class CustomPageBarResourceFactoryTest {
     @Before
     public void setUp() throws Exception {
         doReturn(new URL("http://localhost:8080/page-designer/export/")).when(pageDesignerURLFactory).exportPage(notNull(String.class));
-        is = ByteStreams.newInputStreamSupplier(ByteStreams.toByteArray(this.getClass().getResourceAsStream("/page-Step1.zip"))).getInput();
+        is = new ByteArrayInputStream(ByteStreams.toByteArray(this.getClass().getResourceAsStream("/page-Step1.zip")));
         doReturn(is).when(customPageBarResourceFactory).get(notNull(String.class));
     }
 
