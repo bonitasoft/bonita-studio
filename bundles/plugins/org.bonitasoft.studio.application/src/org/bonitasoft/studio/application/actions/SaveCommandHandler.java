13de9774482c4ebf07f92812bb9d19ffb213b219
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-04-29 08:46:35 +0200

    inline

--- bundles/plugins/org.bonitasoft.studio.application/src/org/bonitasoft/studio/application/actions/SaveCommandHandler.java
+++ bundles/plugins/org.bonitasoft.studio.application/src/org/bonitasoft/studio/application/actions/SaveCommandHandler.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (C) 2009-2012 BonitaSoft S.A.
- * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * Copyright (C) 2009-2015 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
@@ -273,7 +273,7 @@ public class SaveCommandHandler extends SaveHandler {
                 final RenameDiagramOperation renameDiagramOperation = new RenameDiagramOperation();
                 renameDiagramOperation.setEditor((DiagramEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor());
                 renameDiagramOperation.setDiagramToDuplicate(proc);
-                Identifier identifier = nameDialog.getIdentifier();
+                final Identifier identifier = nameDialog.getIdentifier();
                 renameDiagramOperation.setNewDiagramName(identifier.getName());
                 renameDiagramOperation.setNewDiagramVersion(nameDialog.getIdentifier().getVersion());
                 renameDiagramOperation.setPoolsRenamed(nameDialog.getPools());
@@ -301,8 +301,7 @@ public class SaveCommandHandler extends SaveHandler {
 
     @Override
     public boolean isEnabled() {
-        final boolean res = super.isEnabled() || isDirty();
-        return res;
+        return super.isEnabled() || isDirty();
     }
 
     protected boolean isDirty() {
