cdf1e3f4de4f8b5e253b86ad9e8d4e161d892b70
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-04-29 09:16:54 +0200

    BS-13227 Fix default selection regarding selection

--- bundles/plugins/org.bonitasoft.studio.diagram.custom/src/org/bonitasoft/studio/diagram/custom/part/BonitaDataPropertiesBrowserPage.java
+++ bundles/plugins/org.bonitasoft.studio.diagram.custom/src/org/bonitasoft/studio/diagram/custom/part/BonitaDataPropertiesBrowserPage.java
@@ -1,6 +1,16 @@
 package org.bonitasoft.studio.diagram.custom.part;
 
+import static com.google.common.base.Predicates.and;
+import static com.google.common.base.Predicates.instanceOf;
+import static com.google.common.base.Predicates.not;
+
 import org.bonitasoft.studio.common.views.BonitaPropertiesBrowserPage;
+import org.bonitasoft.studio.model.process.Activity;
+import org.bonitasoft.studio.model.process.SendTask;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertySheetPageContributor;
 
 public class BonitaDataPropertiesBrowserPage extends
@@ -16,8 +26,19 @@ public class BonitaDataPropertiesBrowserPage extends
      * @see org.bonitasoft.studio.common.views.BonitaPropertiesBrowserPage#defaultSelectedTabIndex()
      */
     @Override
-    protected int defaultSelectedTabIndex() {
-        return 1;
+    protected int defaultSelectedTabIndex(final ISelection input) {
+        return activitySelected(input) ? 1 : 0;
+    }
+
+    private boolean activitySelected(final ISelection input) {
+        if (!input.isEmpty() && input instanceof IStructuredSelection) {
+            final Object selectedElement = ((IStructuredSelection) input).getFirstElement();
+            if (selectedElement instanceof IAdaptable) {
+                final EObject element = (EObject) ((IAdaptable) selectedElement).getAdapter(EObject.class);
+                return and(instanceOf(Activity.class), not(instanceOf(SendTask.class))).apply(element);
+            }
+        }
+        return false;
     }
 
     @Override
