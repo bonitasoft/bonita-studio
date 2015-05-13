cb46e3532a2f05e58fde9dc002571744e8f7c8ab
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-05-13 16:31:38 +0200

    BS-13332 Add a constraint that explain that studio use a fallback autogenerated form if no target form is defined

---
+++ bundles/plugins/org.bonitasoft.studio.validation/src/org/bonitasoft/studio/validation/constraints/process/InternalFormMappingConstraint.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2010-2015 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.validation.constraints.process;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import org.bonitasoft.studio.model.expression.Expression;
+import org.bonitasoft.studio.model.process.FormMapping;
+import org.bonitasoft.studio.model.process.FormMappingType;
+import org.bonitasoft.studio.validation.constraints.AbstractLiveValidationMarkerConstraint;
+import org.bonitasoft.studio.validation.i18n.Messages;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.IValidationContext;
+
+/**
+ * @author Romain Bioteau
+ */
+public class InternalFormMappingConstraint extends AbstractLiveValidationMarkerConstraint {
+
+    public static final String ID = "org.bonitasoft.studio.validation.constraints.internalFormMapping";
+
+    @Override
+    protected String getConstraintId() {
+        return ID;
+    }
+
+    @Override
+    protected IStatus performBatchValidation(final IValidationContext ctx) {
+        final EObject eObj = ctx.getTarget();
+        checkArgument(eObj instanceof FormMapping);
+        final FormMapping formMapping = (FormMapping) eObj;
+        if (FormMappingType.INTERNAL == formMapping.getType()) {
+            return doValidateInternalMapping(ctx, formMapping);
+        }
+        return ctx.createSuccessStatus();
+    }
+
+    private IStatus doValidateInternalMapping(final IValidationContext ctx, final FormMapping formMapping) {
+        final Expression targetForm = formMapping.getTargetForm();
+        if (!targetForm.hasContent()) {
+            return ctx.createFailureStatus(Messages.emptyFormMappingWarning);
+        }
+        return ctx.createSuccessStatus();
+    }
+}
