928054c6c19d0430b2b326f204a00f4df60408ae
Romain Bioteau <romain.bioteau@bonitasoft.com>
2015-05-19 09:22:03 +0200

    fix testErrorEvent

--- bundles/plugins/org.bonitasoft.studio.common/src/org/bonitasoft/studio/common/log/BonitaStudioLog.java
+++ bundles/plugins/org.bonitasoft.studio.common/src/org/bonitasoft/studio/common/log/BonitaStudioLog.java
@@ -129,6 +129,13 @@ public class BonitaStudioLog {
         }
     }
 
+    public static void debug(final String message, final Throwable throwable, final String bundleId) {
+        final Logger logger = getLogger(bundleId);
+        if (logger != null) {
+            logger.info(message, throwable);
+        }
+    }
+
     public static boolean isLoggable(final int level) {
         final Logger logger = getLogger(Activator.PLUGIN_ID);
         if (logger != null) {
