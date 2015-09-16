fffdf371974203b15450705f2bf6c428eb3416e7
azara <aurelie.zara@bonitasoft.com>
2015-09-16 15:44:17 +0200

    BS-14312 as dave I want to view or/and edit the script when creating a contract from business variable on a pool
    create a page with an expression viewer that display the generated
    script according to the selected data.

---
+++ bundles/plugins/org.bonitasoft.studio.contract/src/org/bonitasoft/studio/contract/ui/wizard/GeneratedScriptEditionPage.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright (C) 2015 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.contract.ui.wizard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bonitasoft.studio.common.ExpressionConstants;
+import org.bonitasoft.studio.common.jface.BonitaErrorDialog;
+import org.bonitasoft.studio.common.log.BonitaStudioLog;
+import org.bonitasoft.studio.common.repository.RepositoryAccessor;
+import org.bonitasoft.studio.contract.core.mapping.FieldToContractInputMapping;
+import org.bonitasoft.studio.contract.core.mapping.RootContractInputGenerator;
+import org.bonitasoft.studio.contract.core.mapping.expression.FieldToContractInputMappingExpressionBuilder;
+import org.bonitasoft.studio.contract.core.mapping.operation.FieldToContractInputMappingOperationBuilder;
+import org.bonitasoft.studio.contract.core.mapping.operation.OperationCreationException;
+import org.bonitasoft.studio.contract.i18n.Messages;
+import org.bonitasoft.studio.expression.editor.filter.AvailableExpressionTypeFilter;
+import org.bonitasoft.studio.expression.editor.viewer.ExpressionViewer;
+import org.bonitasoft.studio.expression.editor.viewer.GroovyOnlyExpressionViewer;
+import org.bonitasoft.studio.model.process.BusinessObjectData;
+import org.bonitasoft.studio.model.process.Element;
+import org.eclipse.core.databinding.Binding;
+import org.eclipse.core.databinding.observable.list.IListChangeListener;
+import org.eclipse.core.databinding.observable.list.ListChangeEvent;
+import org.eclipse.core.databinding.observable.list.WritableList;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * @author aurelie
+ */
+public class GeneratedScriptEditionPage extends WizardPage {
+
+    private final WritableValue selectedDataObservable;
+    private final WritableValue rootNameObservable;
+    private final WritableList fieldToContactInputMappingsObservable;
+    private final FieldToContractInputMappingOperationBuilder operationBuilder;
+    private final RepositoryAccessor repositoryAccessor;
+    private final FieldToContractInputMappingExpressionBuilder expressionBuilder;
+    private Binding binding;
+    private BusinessObjectData dataCopy;
+
+    //private final BusinessObjectData dataCopy;
+
+    /**
+     * @param pageName
+     */
+    protected GeneratedScriptEditionPage(final WritableValue rootNameObservable, final WritableList fieldToContactInputMappingsObservable,
+            final WritableValue selectedDataObservable, final RepositoryAccessor repositoryAccessor,
+            final FieldToContractInputMappingOperationBuilder operationBuilder, final FieldToContractInputMappingExpressionBuilder expressionBuilder) {
+        super(GeneratedScriptEditionPage.class.getName());
+        this.rootNameObservable = rootNameObservable;
+        this.fieldToContactInputMappingsObservable = fieldToContactInputMappingsObservable;
+        this.selectedDataObservable = selectedDataObservable;
+        this.operationBuilder = operationBuilder;
+        this.expressionBuilder = expressionBuilder;
+        this.repositoryAccessor = repositoryAccessor;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+     */
+    @Override
+    public void createControl(final Composite parent) {
+
+        final Composite composite = new Composite(parent, SWT.NONE);
+        composite.setLayout(GridLayoutFactory.fillDefaults().numColumns(1).margins(10, 10).create());
+        composite.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+
+        final ExpressionViewer viewer = new GroovyOnlyExpressionViewer(composite, SWT.BORDER);
+
+        viewer.getControl().setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create());
+        viewer.addFilter(new AvailableExpressionTypeFilter(ExpressionConstants.SCRIPT_TYPE, ExpressionConstants.QUERY_TYPE,
+                ExpressionConstants.CONTRACT_INPUT_TYPE, ExpressionConstants.PARAMETER_TYPE));
+        viewer.addEditorFilter(ExpressionConstants.CONTRACT_INPUT_TYPE, ExpressionConstants.PARAMETER_TYPE);
+        generateExpressionScript(viewer);
+
+        fieldToContactInputMappingsObservable.addListChangeListener(new IListChangeListener() {
+
+            @Override
+            public void handleListChange(final ListChangeEvent event) {
+                generateExpressionScript(viewer);
+                viewer.refresh();
+            }
+        });
+
+        setControl(composite);
+    }
+
+    /**
+     * @param dbc
+     * @param viewer
+     */
+    protected void generateExpressionScript(final ExpressionViewer viewer) {
+        final EObject container = ((Element) selectedDataObservable.getValue()).eContainer();
+        viewer.setContext(container);
+        viewer.setInput(container);
+        final RootContractInputGenerator rootContractInputGenerator = createRootContractInputGenerator();
+        if (!fieldToContactInputMappingsObservable.isEmpty()) {
+            try {
+                rootContractInputGenerator.buildForInstanciation((BusinessObjectData) selectedDataObservable.getValue());
+                viewer.setSelection(new StructuredSelection(rootContractInputGenerator.getInitialValueExpression()));
+            } catch (final OperationCreationException e) {
+                BonitaStudioLog.error("Failed to create Operations from contract", e);
+                new BonitaErrorDialog(getShell(), Messages.errorTitle, Messages.contractFromDataCreationErrorMessage, e).open();
+            }
+        }
+    }
+
+    private RootContractInputGenerator createRootContractInputGenerator() {
+        final List<FieldToContractInputMapping> mappings = new ArrayList<FieldToContractInputMapping>();
+
+        for (final Object mapping : fieldToContactInputMappingsObservable) {
+            mappings.add((FieldToContractInputMapping) mapping);
+        }
+        return new RootContractInputGenerator((String) rootNameObservable.getValue(), mappings,
+                repositoryAccessor, operationBuilder, expressionBuilder);
+    }
+
+}
