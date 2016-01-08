24165b1b4d0d4fbee9bb9845789c10b40ed88c72
Romain Bioteau <romain.bioteau@gmail.com>
2016-01-08 11:02:46 +0100

    wait for repository initialization before starting engine

--- bundles/plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/Repository.java
+++ bundles/plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/Repository.java
@@ -62,6 +62,7 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceDescription;
+import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -226,11 +227,17 @@ public class Repository implements IRepository, IJavaContainer {
             BonitaStudioLog.error(e);
         }
         try {
-            projectManifestFactory.createProjectManifest(project, monitor);
-            initBonitaHome(monitor);
-            initRepositoryStores(monitor);
-            enableBuild();
-            bonitaBPMProjectClasspath.create(this, monitor);
+            workspace.run(new IWorkspaceRunnable() {
+
+                @Override
+                public void run(IProgressMonitor monitor) throws CoreException {
+                    projectManifestFactory.createProjectManifest(project, monitor);
+                    initBonitaHome(monitor);
+                    initRepositoryStores(monitor);
+                    enableBuild();
+                    bonitaBPMProjectClasspath.create(Repository.this, monitor);
+                }
+            }, monitor);
         } catch (final CoreException e) {
             BonitaStudioLog.error(e);
         }
