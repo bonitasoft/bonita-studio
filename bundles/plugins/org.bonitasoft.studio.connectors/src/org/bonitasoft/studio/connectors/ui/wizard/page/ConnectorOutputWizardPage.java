ea76aefa05b6d29fb1d90872ffa2f1a252c00e4c
Romain Bioteau <romain.bioteau@gmail.com>
2016-03-15 16:38:56 +0100

    fix merge

--- bundles/plugins/org.bonitasoft.studio.connectors/src/org/bonitasoft/studio/connectors/ui/wizard/page/ConnectorOutputWizardPage.java
+++ bundles/plugins/org.bonitasoft.studio.connectors/src/org/bonitasoft/studio/connectors/ui/wizard/page/ConnectorOutputWizardPage.java
@@ -1,26 +1,11 @@
 /**
  * Copyright (C) 2009 BonitaSoft S.A.
  * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
-<<<<<<< HEAD
- *
-=======
->>>>>>> refs/remotes/origin/master
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2.0 of the License, or
- * (at your option) any later version.
-<<<<<<< HEAD
- *
-=======
->>>>>>> refs/remotes/origin/master
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
-<<<<<<< HEAD
- *
-=======
->>>>>>> refs/remotes/origin/master
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
