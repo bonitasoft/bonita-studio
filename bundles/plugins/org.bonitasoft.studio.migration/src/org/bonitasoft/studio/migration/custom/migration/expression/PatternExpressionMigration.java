4a4ddfff0b241f1145341374ec8ab8d7ce936ff4
Romain Bioteau <romain.bioteau@gmail.com>
2016-04-06 14:37:20 +0200

    fix(v5Migration) Pattern expression migration
    Remove additional ${}
    
    fixes BS-15207

--- bundles/plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/custom/migration/expression/PatternExpressionMigration.java
+++ bundles/plugins/org.bonitasoft.studio.migration/src/org/bonitasoft/studio/migration/custom/migration/expression/PatternExpressionMigration.java
@@ -17,7 +17,6 @@
 package org.bonitasoft.studio.migration.custom.migration.expression;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.bonitasoft.studio.common.ExpressionConstants;
@@ -26,11 +25,6 @@ import org.eclipse.emf.edapt.migration.MigrationException;
 import org.eclipse.emf.edapt.spi.migration.Instance;
 import org.eclipse.emf.edapt.spi.migration.Metamodel;
 import org.eclipse.emf.edapt.spi.migration.Model;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.Document;
-import org.eclipse.jface.text.FindReplaceDocumentAdapter;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
 
 /**
  * @author Romain Bioteau
@@ -38,12 +32,12 @@ import org.eclipse.jface.text.IRegion;
  */
 public class PatternExpressionMigration extends CustomMigration {
 
-	private Map<String, String> expressions = new HashMap<String, String>();
+	private final Map<String, String> expressions = new HashMap<String, String>();
 
 	@Override
 	public void migrateBefore(Model model, Metamodel metamodel)
 			throws MigrationException {
-		for(Instance expression : model.getAllInstances("expression.Expression")){
+		for(final Instance expression : model.getAllInstances("expression.Expression")){
 			if(ExpressionConstants.PATTERN_TYPE.equals(expression.get("type"))){
 				final String content = expression.get("content");
 				if(content != null && !content.isEmpty()){
@@ -56,10 +50,9 @@ public class PatternExpressionMigration extends CustomMigration {
 	@Override
 	public void migrateAfter(Model model, Metamodel metamodel)
 			throws MigrationException {
-		for(Instance expression : model.getAllInstances("expression.Expression")){
+		for(final Instance expression : model.getAllInstances("expression.Expression")){
 			if(expressions.containsKey(expression.getUuid())){
-				String content = replaceSpecialCharacter(expressions.get(expression.getUuid()));
-				expression.set("content", addDelimiters(content,expression));
+                expression.set("content", replaceSpecialCharacter(expressions.get(expression.getUuid())));
 			}
 		}
 	}
@@ -71,74 +64,6 @@ public class PatternExpressionMigration extends CustomMigration {
 	 */
 	private String replaceSpecialCharacter(String content) {
 		return content.replaceAll("&nbsp;", " ");
-	}
-
-	private String addDelimiters(String content, Instance expression) {
-		IDocument document = new Document();
-		document.set(content);
-		StringBuilder patternExpressionContent = new StringBuilder();
-		FindReplaceDocumentAdapter finder = new FindReplaceDocumentAdapter(document);
-		int lenght = content.length();
-		int i = 0;
-		List<Instance> dependencies = expression.get("referencedElements");
-		for (final Instance dep : dependencies) {
-			try {
-				String depName  = dep.get("name");
-				IRegion index = null;
-				index = finder.find(i, depName, true, true, true, false);
-				while(index != null && index.getOffset() <  lenght){
-					if(isNotEscapeWord(content, index.getOffset())){
-						patternExpressionContent.append(content.substring(i, index.getOffset()));
-						if(alreadyDep(content, index.getLength(), index.getOffset())){
-							patternExpressionContent.append(depName);
-						}else{
-							patternExpressionContent.append("${");
-							patternExpressionContent.append(depName);
-							patternExpressionContent.append("}");
-
-						}
-					}else{
-						patternExpressionContent.append(content.substring(i, index.getOffset()-1));
-						patternExpressionContent.append(content.substring(index.getOffset(),index.getOffset()+index.getLength()));
-					}
-					i = index.getOffset() + index.getLength();
-					if(i < lenght){
-						index = finder.find(i, depName, true, true, true, false);
-					}else{
-						index = null;
-					}
-				}
-			} catch (BadLocationException e) {
-				// Ignore
-			}
-			if(i < lenght){
-				patternExpressionContent.append(content.substring(i, lenght));
-			}
-			content = patternExpressionContent.toString();
-			lenght = content.length();
-			i = 0;
-			patternExpressionContent = new StringBuilder();
-			document.set(content);
-			finder = new FindReplaceDocumentAdapter(document);
-		}
-		return content;
-	}
-
-	private boolean isNotEscapeWord(String content, int offset) {
-		if(offset-1>-1){
-			return content.charAt(offset-1) != '\\';
-		}
-		return true;
-	}
-	
-	private boolean alreadyDep(String content, int lenghtName, int offset){
-		int start = offset-2;
-		int end = offset+lenghtName;
-		if(start>-1 && end<content.length()){
-			return (content.substring(start).startsWith("${") && content.substring(end).startsWith("}")); 
-		}
-		return false;
-		
 	}
 
 }
