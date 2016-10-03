5b8ca8907a1e1bbc1ba753d2c93b7a0cbb1d6752
Romain Bioteau <romain.bioteau@gmail.com>
2016-10-03 13:28:08 +0200

    fix(RESTConnector) Integrate alpha03 version

--- bundles/plugins/org.bonitasoft.studio.intro/src/org/bonitasoft/studio/intro/actions/ImportAction.java
+++ bundles/plugins/org.bonitasoft.studio.intro/src/org/bonitasoft/studio/intro/actions/ImportAction.java
@@ -1,19 +1,16 @@
 /**
  * Copyright (C) 2009 BonitaSoft S.A.
  * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.intro.actions;
 
@@ -21,24 +18,40 @@ import java.util.Properties;
 
 import org.bonitasoft.studio.common.log.BonitaStudioLog;
 import org.bonitasoft.studio.importer.handler.ImportHandler;
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.NotEnabledException;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.intro.IIntroSite;
 import org.eclipse.ui.intro.config.IIntroAction;
 
 /**
  * @author Romain Bioteau
- *
  */
 public class ImportAction implements IIntroAction {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
-	 */
-	public void run(IIntroSite introSite, Properties param) {
-		try {
-			new ImportHandler().execute(null);
-		} catch (Exception e) {
-			BonitaStudioLog.error(e);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+     */
+    public void run(IIntroSite introSite, Properties param) {
+        IWorkbenchPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
+        IHandlerService handlerService = (IHandlerService) part.getSite().getService(IHandlerService.class);
+        ICommandService cmdService = (ICommandService) part.getSite().getService(ICommandService.class);
+        // Do not replace by static link since this command does not resolve to the same between BOS and SP
+        Command open = cmdService.getCommand("org.bonitasoft.studio.application.importCommand");//$NON-NLS-1$
+        ExecutionEvent executionEvent = handlerService.createExecutionEvent(open, null);
+        try {
+            open.executeWithChecks(executionEvent);
+        } catch (ExecutionException | NotDefinedException | NotEnabledException | NotHandledException e) {
+            BonitaStudioLog.error(e);
+        }
+    }
 
 }
