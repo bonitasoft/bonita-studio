07b6691e933a9b647efd1a8015423491282e1087
Romain Bioteau <romain.bioteau@bonitasoft.com>
2016-10-04 16:33:38 +0200

    fix(Alfresco) Update Alfresco Connector to 2.0.0 (#193)
    * Display CMS icon category
    * Merge duplicated definition

--- bundles/plugins/org.bonitasoft.studio.importer.bar/src/org/bonitasoft/studio/importer/bar/custom/migration/connector/mapper/CreateFolderAlfrescoConnectorMapper.java
+++ bundles/plugins/org.bonitasoft.studio.importer.bar/src/org/bonitasoft/studio/importer/bar/custom/migration/connector/mapper/CreateFolderAlfrescoConnectorMapper.java
@@ -6,7 +6,7 @@ import org.bonitasoft.studio.connectors.extension.IConnectorDefinitionMapper;
 public class CreateFolderAlfrescoConnectorMapper extends
 		AbstractConnectorDefinitionMapper implements IConnectorDefinitionMapper {
 	
-	private static final String CREATE_FOLDER ="Alfresco34CreateFolderByPath";
+    private static final String CREATE_FOLDER = "AlfrescoCreateFolderByPath";
 	private static final String LEGACY_CREATE_FOLDER="AlfrescoCreateFolderByPath";
 
 	@Override
--- bundles/plugins/org.bonitasoft.studio.importer.bar/src/org/bonitasoft/studio/importer/bar/custom/migration/connector/mapper/UploadFileAlfrescoConnectorMapper.java
+++ bundles/plugins/org.bonitasoft.studio.importer.bar/src/org/bonitasoft/studio/importer/bar/custom/migration/connector/mapper/UploadFileAlfrescoConnectorMapper.java
@@ -6,7 +6,7 @@ import org.bonitasoft.studio.connectors.extension.IConnectorDefinitionMapper;
 public class UploadFileAlfrescoConnectorMapper extends
 		AbstractConnectorDefinitionMapper implements IConnectorDefinitionMapper {
 
-	private static final String UPLOAD_FILE_ALFRESCO_ID="Alfresco34UploadFileByPath";
+    private static final String UPLOAD_FILE_ALFRESCO_ID = "AlfrescoUploadFileByPath";
 	private static final String LEGACY_UPLOAD_FILE_ALFRESCO_ID="AlfrescoUploadFileByPath";
 	
 	
