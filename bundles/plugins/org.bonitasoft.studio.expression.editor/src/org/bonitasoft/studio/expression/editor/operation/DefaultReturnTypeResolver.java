fcbc481fd1a09743695a903394b94a21da226012
Romain Bioteau <romain.bioteau@bonitasoft.com>
2016-10-06 15:19:11 +0200

    fix(Operation): improve reordering performance (#194)

--- bundles/plugins/org.bonitasoft.studio.expression.editor/src/org/bonitasoft/studio/expression/editor/operation/DefaultReturnTypeResolver.java
+++ bundles/plugins/org.bonitasoft.studio.expression.editor/src/org/bonitasoft/studio/expression/editor/operation/DefaultReturnTypeResolver.java
@@ -17,16 +17,18 @@ package org.bonitasoft.studio.expression.editor.operation;
 import org.bonitasoft.engine.bpm.document.DocumentValue;
 import org.bonitasoft.studio.common.ExpressionConstants;
 import org.bonitasoft.studio.model.expression.Operation;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
 
 public class DefaultReturnTypeResolver {
 
-    private final Operation operation;
+    private final IObservableValue operationObservable;
 
-    public DefaultReturnTypeResolver(final Operation operation) {
-        this.operation = operation;
+    public DefaultReturnTypeResolver(final IObservableValue operationObservable) {
+        this.operationObservable = operationObservable;
     }
 
     public String guessRightOperandReturnType() {
+        Operation operation = (Operation) operationObservable.getValue();
         final String type = operation.getOperator().getType();
         if (ExpressionConstants.SET_DOCUMENT_OPERATOR.equals(type)) {
             return DocumentValue.class.getName();
