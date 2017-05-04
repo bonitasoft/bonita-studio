65862f4a824a45ce5ce7cc00b89aabc124e255af
Adrien <adrien.lachambre@bonitasoft.com>
2017-05-04 08:46:23 +0200

    feat(la) profile menus (#439)
    * feat(la) profile menus
    
    * creation of menus for create / open / delete / export / import
    profiles, and empty handlers for those menus
    
    closes [BS-16528](https://bonitasoft.atlassian.net/browse/BS-16528)
    
    * refactoring studio.la(.ex) :
    
    	* package for application (all files have been moved)
    	* package for profile

---
+++ bundles/plugins/org.bonitasoft.studio.la/src-test/java/org/bonitasoft/studio/la/application/core/ExportApplicationRunnableTest.java
@@ -0,0 +1,43 @@
+package org.bonitasoft.studio.la.application.core;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bonitasoft.studio.common.repository.model.IRepositoryFileStore;
+import org.bonitasoft.studio.la.application.core.ExportApplicationRunnable;
+import org.bonitasoft.studio.la.application.repository.ApplicationFileStore;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ExportApplicationRunnableTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Test
+    public void should_export_applications() throws Exception {
+        final File folder = temporaryFolder.newFolder();
+
+        final ApplicationFileStore appication1 = mock(ApplicationFileStore.class);
+        when(appication1.getName()).thenReturn("app1");
+        final ApplicationFileStore appication2 = mock(ApplicationFileStore.class);
+        when(appication2.getName()).thenReturn("app2");
+
+        final List<IRepositoryFileStore> list = new ArrayList<>();
+        list.add(appication1);
+        list.add(appication2);
+
+        final ExportApplicationRunnable operation = new ExportApplicationRunnable(folder.getPath(), list);
+        operation.run(new NullProgressMonitor());
+
+        verify(appication1).export(folder.getPath());
+        verify(appication2).export(folder.getPath());
+    }
+}
