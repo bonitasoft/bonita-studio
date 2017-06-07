91ab47a878737a4827c19aa944f200b052681016
Adrien <adrien.lachambre@bonitasoft.com>
2017-06-07 16:29:39 +0200

    fix(la) defect fixed in 7.5.0 (#521)
    * because of refactoring, a fix has been forgot when we merged master
    into dev
    * local token duplication:
    [BS-16635](https://bonitasoft.atlassian.net/browse/BS-16635)

--- bundles/plugins/org.bonitasoft.studio.la/src-test/java/org/bonitasoft/studio/la/application/ui/validator/ApplicationTokenUnicityValidatorTest.java
+++ bundles/plugins/org.bonitasoft.studio.la/src-test/java/org/bonitasoft/studio/la/application/ui/validator/ApplicationTokenUnicityValidatorTest.java
@@ -14,7 +14,6 @@ import org.bonitasoft.engine.business.application.xml.ApplicationNodeContainer;
 import org.bonitasoft.studio.common.repository.RepositoryAccessor;
 import org.bonitasoft.studio.la.application.repository.ApplicationFileStore;
 import org.bonitasoft.studio.la.application.repository.ApplicationRepositoryStore;
-import org.bonitasoft.studio.la.application.ui.validator.ApplicationTokenUnicityValidator;
 import org.junit.Test;
 
 public class ApplicationTokenUnicityValidatorTest {
@@ -35,11 +34,19 @@ public class ApplicationTokenUnicityValidatorTest {
     public void should_validate_uniqueness_with_current_token() throws Exception {
         final RepositoryAccessor repositoryAccessor = initRepositoryAccessor();
         ApplicationNodeContainer nodeContainer = new ApplicationNodeContainer();
-        final ApplicationTokenUnicityValidator validator = new ApplicationTokenUnicityValidator(repositoryAccessor,
+        nodeContainer.addApplication(ApplicationNodeBuilder.newApplication("token3", "appName", "1.0").create());
+        ApplicationTokenUnicityValidator validator = new ApplicationTokenUnicityValidator(repositoryAccessor,
                 nodeContainer, "filename.xml", "token2");
 
         assertThat(validator.validate("token1")).isNotOK();
-        assertThat(validator.validate("token2")).isOK();
+        assertThat(validator.validate("token2")).isNotOK();
+        assertThat(validator.validate("token3")).isNotOK();
+
+        validator = new ApplicationTokenUnicityValidator(repositoryAccessor,
+                nodeContainer, "filename.xml", "token3");
+
+        assertThat(validator.validate("token1")).isNotOK();
+        assertThat(validator.validate("token2")).isNotOK();
         assertThat(validator.validate("token3")).isOK();
     }
 
@@ -60,6 +67,8 @@ public class ApplicationTokenUnicityValidatorTest {
         workingCopy.addApplication(ApplicationNodeBuilder.newApplication("workingcpy_token1", "name1", "1.0").create());
         workingCopy.addApplication(ApplicationNodeBuilder.newApplication("workingcpy_token2", "name2", "1.0").create());
         workingCopy.addApplication(ApplicationNodeBuilder.newApplication("token1", "name3", "1.0").create());
+        workingCopy.addApplication(ApplicationNodeBuilder.newApplication("duplicatedToken", "name4", "1.0").create());
+        workingCopy.addApplication(ApplicationNodeBuilder.newApplication("duplicatedToken", "name5", "1.0").create());
 
         ApplicationTokenUnicityValidator validator = new ApplicationTokenUnicityValidator(repositoryAccessor,
                 workingCopy, "filename.xml", "workingcpy_token2");
@@ -67,9 +76,11 @@ public class ApplicationTokenUnicityValidatorTest {
         assertThat(validator.validate("workingcpy_token1")).isNotOK();
         assertThat(validator.validate("workingcpy_token2")).isOK();
 
-        validator = new ApplicationTokenUnicityValidator(repositoryAccessor,
-                workingCopy, "myApp.xml");
+        validator = new ApplicationTokenUnicityValidator(repositoryAccessor, workingCopy, "myApp.xml");
         assertThat(validator.validate("token2")).isOK();
+
+        validator = new ApplicationTokenUnicityValidator(repositoryAccessor, workingCopy, "myApp.xml", "duplicatedToken");
+        assertThat(validator.validate("duplicatedToken")).isNotOK();
     }
 
     private RepositoryAccessor initRepositoryAccessor() throws Exception {
