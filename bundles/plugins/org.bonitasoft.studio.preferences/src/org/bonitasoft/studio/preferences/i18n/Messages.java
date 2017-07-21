babfd72e3fc42f2e6b24923d3f8c649fb8794948
Romain Bioteau <romain.bioteau@bonitasoft.com>
2017-07-21 11:18:09 +0200

    tr(refactorPlugin) merge browser into preferences

--- bundles/plugins/org.bonitasoft.studio.preferences/src/org/bonitasoft/studio/preferences/i18n/Messages.java
+++ bundles/plugins/org.bonitasoft.studio.preferences/src/org/bonitasoft/studio/preferences/i18n/Messages.java
@@ -5,14 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.preferences.i18n;
 
@@ -67,7 +65,9 @@ public class Messages extends NLS {
     public static String showPassword;
     public static String defaultAppliThemeLabel;
     public static String validateBeforeRun;
-	public static String BonitaPreferenceDialog_DBConnectors;
-	public static String askRenameDiagram;
-	public static String doNotDisplayConnectorDefConfirmationMessage;
+    public static String BonitaPreferenceDialog_DBConnectors;
+    public static String askRenameDiagram;
+    public static String doNotDisplayConnectorDefConfirmationMessage;
+    public static String noBrowserFoundTitle;
+    public static String noBrowserFoundMsg;
 }
