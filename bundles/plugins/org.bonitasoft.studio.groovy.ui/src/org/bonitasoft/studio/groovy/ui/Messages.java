b1d56e715f0af582dbfa6255b9e7028672fae09b
Adrien <adrien.lachambre@bonitasoft.com>
2017-08-24 13:38:36 +0200

    fix(groovy script max length) (#663)
    * add a dialog when max length is reached
    
    closes [BS-17049](https://bonitasoft.atlassian.net/browse/BS-17049)

--- bundles/plugins/org.bonitasoft.studio.groovy.ui/src/org/bonitasoft/studio/groovy/ui/Messages.java
+++ bundles/plugins/org.bonitasoft.studio.groovy.ui/src/org/bonitasoft/studio/groovy/ui/Messages.java
@@ -1,17 +1,14 @@
 /**
  * Copyright (C) 2009 BonitaSoft S.A.
  * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
- * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -114,6 +111,10 @@ public final class Messages extends NLS {
     public static String wrongNameLowCase;
 
     public static String warningAssigningAVariableWithSameNameAsProcessVariable;
+
+    public static String maxScriptLengthTitle;
+
+    public static String maxScriptLength;
 
     private Messages() {
         // Do not instantiate
