32ef9208b73c1f999bbfc88570c30b56252dd5cb
Adrien <adrien.lachambre@bonitasoft.com>
2017-09-28 13:54:04 +0200

    feat(access control) export access control file (#733)
    * extract in two abstract classes the export logic used in editors
    * refactor export feature for applications and profiles
    * add contribution item in access control editor to export access
    control file
    
    closes [BS-17159](https://bonitasoft.atlassian.net/browse/BS-17159)

--- bundles/plugins/org.bonitasoft.studio.la/src/org/bonitasoft/studio/la/application/handler/ExportApplicationFileAction.java
+++ bundles/plugins/org.bonitasoft.studio.la/src/org/bonitasoft/studio/la/application/handler/ExportApplicationFileAction.java
@@ -8,73 +8,43 @@
  *******************************************************************************/
 package org.bonitasoft.studio.la.application.handler;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Optional;
-
-import org.bonitasoft.studio.common.jface.FileActionDialog;
 import org.bonitasoft.studio.common.jface.MessageDialogWithPrompt;
-import org.bonitasoft.studio.common.repository.model.IRepositoryFileStore;
 import org.bonitasoft.studio.la.LivingApplicationPlugin;
 import org.bonitasoft.studio.la.i18n.Messages;
-import org.bonitasoft.studio.ui.dialog.ExceptionDialogHandler;
+import org.bonitasoft.studio.ui.editors.core.AbstractExportAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
-public class ExportApplicationFileAction {
+public class ExportApplicationFileAction extends AbstractExportAction {
 
     public static final String DO_NOT_SHOW_EXPORT_MESSAGE_DIALOG = "DO_NOT_SHOW_EXPORT_MESSAGE_DIALOG";
 
-    public Optional<IRepositoryFileStore> export(Shell shell, IRepositoryFileStore fileStore) {
-        final Optional<String> optionalPath = getPath(shell, fileStore);
-        if (optionalPath.isPresent()) {
-            File targetFile = Paths.get(optionalPath.get()).toFile();
-            if (targetFile.exists() && !FileActionDialog.overwriteQuestion(targetFile.getName())) {
-                return Optional.empty();
-            }
-            final IPreferenceStore preferenceStore = LivingApplicationPlugin.getDefault().getPreferenceStore();
-            if (!preferenceStore.getBoolean(DO_NOT_SHOW_EXPORT_MESSAGE_DIALOG)) {
-                MessageDialogWithPrompt.open(MessageDialog.WARNING,
-                        shell,
-                        Messages.exportApplicationDescriptorTitle,
-                        Messages.exportApplicationDescriptorMessage,
-                        Messages.doNotShowMeAgain,
-                        false,
-                        preferenceStore,
-                        DO_NOT_SHOW_EXPORT_MESSAGE_DIALOG,
-                        SWT.NONE);
-            }
-
-            try {
-                Files.copy(fileStore.getResource().getLocation().toFile().toPath(), targetFile.toPath(),
-                        StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                new ExceptionDialogHandler().openErrorDialog(Display.getCurrent().getActiveShell(),
-                        Messages.exportFailedTitle, e);
-            }
-
-            MessageDialog.openInformation(Display.getCurrent().getActiveShell(), Messages.exportDoneTitle,
-                    String.format(Messages.exportDoneMessage, targetFile.getAbsolutePath()));
-            return Optional.of(fileStore);
-
+    @Override
+    protected void openExportMessageDialog(Shell shell) {
+        final IPreferenceStore preferenceStore = LivingApplicationPlugin.getDefault().getPreferenceStore();
+        if (!preferenceStore.getBoolean(DO_NOT_SHOW_EXPORT_MESSAGE_DIALOG)) {
+            MessageDialogWithPrompt.open(MessageDialog.WARNING,
+                    shell,
+                    Messages.exportApplicationDescriptorTitle,
+                    Messages.exportApplicationDescriptorMessage,
+                    Messages.doNotShowMeAgain,
+                    false,
+                    preferenceStore,
+                    DO_NOT_SHOW_EXPORT_MESSAGE_DIALOG,
+                    SWT.NONE);
         }
-        return Optional.empty();
-
     }
 
-    protected Optional<String> getPath(Shell shell, IRepositoryFileStore fStore) {
-        final FileDialog fd = new FileDialog(shell, SWT.SAVE | SWT.SHEET);
-        fd.setFileName(fStore.getName());
-        fd.setFilterExtensions(new String[] { "*.xml" });
-        fd.setText(Messages.exportApplicationDescriptor);
-        return Optional.ofNullable(fd.open());
+    @Override
+    protected String getExportDoneMessage() {
+        return Messages.exportDoneMessage;
+    }
+
+    @Override
+    protected String getExportTitle() {
+        return Messages.exportApplicationDescriptor;
     }
 
 }
