ee8696dc672c175c6e1b0de29f81656e0abff974
Adrien <adrien.lachambre@bonitasoft.com>
2017-11-17 09:14:05 +0100

    feat(access control) import bdm access control file (#840)
    * add a menu to import bdm access control file
    * refactor other importer to reuse code and avoid duplications
    
    closes [BS-17118](https://bonitasoft.atlassian.net/browse/BS-17118)

--- bundles/plugins/org.bonitasoft.studio.la/src/org/bonitasoft/studio/la/application/ui/control/ImportApplicationPage.java
+++ bundles/plugins/org.bonitasoft.studio.la/src/org/bonitasoft/studio/la/application/ui/control/ImportApplicationPage.java
@@ -14,103 +14,36 @@
  */
 package org.bonitasoft.studio.la.application.ui.control;
 
-import java.io.File;
-import java.util.Optional;
+import java.util.List;
 
 import org.bonitasoft.studio.common.repository.RepositoryAccessor;
-import org.bonitasoft.studio.la.LivingApplicationPlugin;
 import org.bonitasoft.studio.la.application.repository.ApplicationRepositoryStore;
 import org.bonitasoft.studio.la.application.ui.validator.ApplicationXMLContentValidator;
 import org.bonitasoft.studio.la.i18n.Messages;
 import org.bonitasoft.studio.la.ui.validator.ImportFileStoreConflictsValidator;
-import org.bonitasoft.studio.ui.databinding.UpdateStrategyFactory;
-import org.bonitasoft.studio.ui.databinding.UpdateValueStrategyFactory;
-import org.bonitasoft.studio.ui.validator.EmptyInputValidator;
-import org.bonitasoft.studio.ui.validator.MultiValidator;
-import org.bonitasoft.studio.ui.validator.PathValidator;
-import org.bonitasoft.studio.ui.widget.TextWidget;
-import org.bonitasoft.studio.ui.wizard.ControlSupplier;
-import org.eclipse.core.databinding.DataBindingContext;
-import org.eclipse.core.databinding.beans.PojoObservables;
-import org.eclipse.core.databinding.observable.value.IObservableValue;
-import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.wizard.IWizardContainer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.Shell;
+import org.bonitasoft.studio.ui.page.AbstractImportPage;
+import org.eclipse.core.databinding.validation.IValidator;
 
-public class ImportApplicationPage implements ControlSupplier {
+public class ImportApplicationPage extends AbstractImportPage {
 
     public static final String XML_EXTENSION = "*.xml";
-    private static final String LAST_IMPORT_PATH = null;
-
-    private final RepositoryAccessor repositoryAccessor;
-    private TextWidget importerWidget;
-    private String filePath;
 
     public ImportApplicationPage(RepositoryAccessor repositoryAccessor) {
-        this.repositoryAccessor = repositoryAccessor;
+        super(repositoryAccessor);
     }
 
     @Override
-    public Control createControl(Composite parent, IWizardContainer wizardContainer, DataBindingContext ctx) {
-        final Composite importerComposite = new Composite(parent, SWT.NONE);
-        importerComposite.setLayout(GridLayoutFactory.fillDefaults().margins(10, 11).create());
-        importerComposite.setLayoutData(GridDataFactory.fillDefaults().create());
-
-        final IObservableValue filePathObservable = PojoObservables.observeValue(this, "filePath");
-
-        importerWidget = new TextWidget.Builder()
-                .withLabel(Messages.importApplicationDescriptorLabel)
-                .labelAbove()
-                .fill()
-                .grabHorizontalSpace()
-                .readOnly()
-                .withButton(Messages.browse)
-                .onClickButton(e -> openFileDialog(parent.getShell()).ifPresent(importerWidget::setText))
-                .bindTo(filePathObservable)
-                .withTargetToModelStrategy(fileUpdateStrategy())
-                .inContext(ctx)
-                .createIn(importerComposite);
-
-        return importerComposite;
+    protected List<IValidator> getValidators() {
+        List<IValidator> validators = super.getValidators();
+        validators.add(new ApplicationXMLContentValidator(Messages.notAnApplicationError));
+        validators.add(new ImportFileStoreConflictsValidator(
+                repositoryAccessor.getRepositoryStore(ApplicationRepositoryStore.class)));
+        return validators;
     }
 
-    private UpdateValueStrategyFactory fileUpdateStrategy() {
-        return UpdateStrategyFactory.updateValueStrategy()
-                .withValidator(new MultiValidator.Builder().havingValidators(
-                        new EmptyInputValidator.Builder().withMessage(Messages.filePathNotEmpty).create(),
-                        new PathValidator.Builder().withMessage(Messages.fileDoesntExist).create(),
-                        new ApplicationXMLContentValidator(Messages.notAnApplicationError),
-                        new ImportFileStoreConflictsValidator(
-                                repositoryAccessor.getRepositoryStore(ApplicationRepositoryStore.class))));
-    }
-
-    private Optional<String> openFileDialog(Shell shell) {
-        final FileDialog fd = new FileDialog(shell, SWT.OPEN | SWT.SINGLE);
-        fd.setText(Messages.importApplicationDescriptorLabel);
-        fd.setFilterPath(getLastPath());
-        fd.setFilterExtensions(new String[] { XML_EXTENSION });
-        return Optional.ofNullable(fd.open());
-    }
-
-    private String getLastPath() {
-        String path = LivingApplicationPlugin.getDefault().getDialogSettings().get(LAST_IMPORT_PATH);
-        if (path == null || !new File(path).exists()) {
-            path = System.getProperty("user.home");
-        }
-        return path;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
+    @Override
+    protected String[] getExtensions() {
+        return new String[] { XML_EXTENSION };
     }
 
 }
