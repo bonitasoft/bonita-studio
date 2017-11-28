7e4c0803a56b624196631ce1f65ed107f127f13d
Adrien <adrien.lachambre@bonitasoft.com>
2017-11-28 12:55:55 +0100

    feat(access control) swtbot test on access control (#860)
    * * implementation of a swtbot api to create business object model
    * add a swtbot test which define access control using the editor
    
    closes [BS-17590](https://bonitasoft.atlassian.net/browse/BS-17590)

--- tests/org.bonitasoft.studio.tests/src/org/bonitasoft/studio/tests/businessobject/CreateDeployExportBusinessObjectIT.java
+++ tests/org.bonitasoft.studio.tests/src/org/bonitasoft/studio/tests/businessobject/CreateDeployExportBusinessObjectIT.java
@@ -10,13 +10,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Stream;
 
 import org.bonitasoft.engine.bdm.model.BusinessObject;
 import org.bonitasoft.engine.bdm.model.BusinessObjectModel;
@@ -27,25 +24,19 @@ import org.bonitasoft.studio.businessobject.core.repository.BusinessObjectModelF
 import org.bonitasoft.studio.businessobject.core.repository.BusinessObjectModelRepositoryStore;
 import org.bonitasoft.studio.businessobject.i18n.Messages;
 import org.bonitasoft.studio.businessobject.ui.DateTypeLabels;
-import org.bonitasoft.studio.businessobject.ui.wizard.editingsupport.FieldTypeEditingSupport;
 import org.bonitasoft.studio.common.repository.Repository;
 import org.bonitasoft.studio.common.repository.RepositoryManager;
 import org.bonitasoft.studio.engine.BOSEngineManager;
+import org.bonitasoft.studio.swtbot.framework.application.BotApplicationWorkbenchWindow;
+import org.bonitasoft.studio.swtbot.framework.bdm.DefineBdmWizardBot;
 import org.bonitasoft.studio.swtbot.framework.rule.SWTGefBotRule;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
 import org.eclipse.swtbot.eclipse.gef.finder.SWTGefBot;
-import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.eclipse.swtbot.swt.finder.keyboard.Keyboard;
-import org.eclipse.swtbot.swt.finder.keyboard.KeyboardFactory;
-import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
-import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotCCombo;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTable;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -60,8 +51,6 @@ public class CreateDeployExportBusinessObjectIT {
 
     @Rule
     public SWTGefBotRule rule = new SWTGefBotRule(bot);
-
-    private Keyboard keyboard;
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
@@ -78,7 +67,6 @@ public class CreateDeployExportBusinessObjectIT {
             layout = "MAC_" + layout;
         }
         SWTBotPreferences.KEYBOARD_LAYOUT = layout;
-        keyboard = KeyboardFactory.getSWTKeyboard();
         tmpFile = temporaryFolder.newFile("bdm.zip");
 
         bdmStore = RepositoryManager.getInstance().getRepositoryStore(BusinessObjectModelRepositoryStore.class);
@@ -98,82 +86,58 @@ public class CreateDeployExportBusinessObjectIT {
 
     @Test
     public void shouldCreateAndPublishABusinessObject() throws Exception {
-        // Create a business object
-        bot.menu("Development").menu("Business Data Model").menu("Define...").click();
-        bot.waitUntil(Conditions.shellIsActive("Manage Business Data Model"));
+        BotApplicationWorkbenchWindow workBenchBot = new BotApplicationWorkbenchWindow(bot);
 
-        bot.textWithLabel("Package").setText("org.model.test");
+        DefineBdmWizardBot bdmWizardBot = workBenchBot.defineBDM();
+        bdmWizardBot.withPackage("org.model.test")
+                .addBusinessObject("Employee")
+                .addAttribute("Employee", "firstName", FieldType.STRING.name())
+                .setAttributeLength("Employee", "firstName", "25")
+                .setMandatory("Employee", "firstName")
+                .addAttribute("Employee", "lastaNme", FieldType.STRING.name())
+                .addAttribute("Employee", "birthDate", DateTypeLabels.DATE_ONLY)
+                .addAttribute("Employee", "age", FieldType.INTEGER.name())
+                .addAttribute("Employee", "married", FieldType.BOOLEAN.name())
+                .addAttribute("Employee", "resume", FieldType.TEXT.name())
+                .addAttribute("Employee", "salary", FieldType.DOUBLE.name())
+                .addAttribute("Employee", "skills", FieldType.STRING.name())
+                .setMultiple("Employee", "skills")
+                .addAttribute("Employee", "manager", "Employee")
+                .setRelationType("Employee", "manager", "Aggregation");
 
-        // Add Employee Business Object
-        final String listBOGroupTitle = "List of Business Objects";
-        bot.buttonInGroup("Add", listBOGroupTitle).click();
-        SWTBotTable table = bot.tableInGroup(listBOGroupTitle);
-        table.click(0, 0);
-        bot.text("BusinessObject1").typeText("Employee");
-        keyboard.pressShortcut(Keystrokes.CR);
+        bdmWizardBot.addConstraint("Employee", "FIRSTLASTNAMEUNIQUE", "firstName -- STRING", "lastaNme -- STRING");
+        bdmWizardBot.addIndex("Employee", "NAMEINDEX", "firstName -- STRING", "lastaNme -- STRING");
 
-        // Add attributes
-        addAttribute("Employee", "firstName", FieldType.STRING.name(), 0);
-        setAttributeLength("Employee", "firstName", "25");
-        setMandatory("Employee", "firstName");
+        bdmWizardBot.deploy();
 
-        addAttribute("Employee", "lastaNme", FieldType.STRING.name(), 1);
-        addAttribute("Employee", "birthDate", DateTypeLabels.DATE_ONLY, 2);
-        addAttribute("Employee", "age", FieldType.INTEGER.name(), 3);
-        addAttribute("Employee", "married", FieldType.BOOLEAN.name(), 4);
-        addAttribute("Employee", "resume", FieldType.TEXT.name(), 5);
-        addAttribute("Employee", "salary", FieldType.DOUBLE.name(), 6);
-        addAttribute("Employee", "skills", FieldType.STRING.name(), 7);
-        setMultiple("Employee", "skills");
-
-        addAttribute("Employee", "manager", "Employee", 8);
-        setRelationType("Employee", "manager", "Aggregation");
-
-        // Add constraint
-        addConstraint("Employee", "FIRSTLASTNAMEUNIQUE", new String[] { "firstName -- STRING", "lastaNme -- STRING" }, 0);
-
-        // Add index
-        addIndex("Employee", "NAMEINDEX", new String[] { "firstName -- STRING", "lastaNme -- STRING" }, 0);
+        bdmWizardBot = workBenchBot.defineBDM();
+        bdmWizardBot.renameAttribute("Employee", "lastaNme", "lastName");
 
         SWTBotShell activeShell = bot.activeShell();
-        bot.button(IDialogConstants.FINISH_LABEL).click();
-        bot.waitUntil(Conditions.shellIsActive(Messages.bdmDeployedTitle), 30000);
-        bot.button(IDialogConstants.OK_LABEL).click();
-
-        // Edit business object
-        bot.menu("Development").menu("Business Data Model").menu("Define...").click();
-        table = bot.tableInGroup(listBOGroupTitle);
-
-        final SWTBotTable attributeTable = bot.tableInGroup("Employee");
-        attributeTable.click(1, 0);
-        bot.textInGroup("lastaNme", "Employee").selectAll().typeText("lastName");
-        keyboard.pressShortcut(Keystrokes.CR);
-        activeShell = bot.activeShell();
         bot.button(IDialogConstants.FINISH_LABEL).click();
         bot.button(IDialogConstants.OK_LABEL).click();
         bot.waitUntil(Conditions.shellIsActive("Validation failed"));
         bot.button(IDialogConstants.OK_LABEL).click();
         activeShell.setFocus();
-        editConstraint("Employee", new String[] { "firstName -- STRING", "lastName -- STRING" }, 0);
-        editIndex("Employee", "NAMEINDEX", new String[] { "lastName -- STRING" }, 0);
+        bdmWizardBot.editConstraint("Employee", "FIRSTLASTNAMEUNIQUE", "firstName -- STRING", "lastName -- STRING");
+        bdmWizardBot.editIndex("Employee", "NAMEINDEX", "lastName -- STRING");
 
-        // Add custom query
         final Map<String, String> queryParam = new HashMap<>();
         queryParam.put("maxSalary", Double.class.getName());
-        addCustomQuery("Employee", "findByMaxSalary", "SELECT e FROM Employee e WHERE e.salary < :maxSalary", queryParam,
-                "Multiple (java.util.List)", 0);
+        bdmWizardBot.addCustomQuery("Employee",
+                "findByMaxSalary",
+                "SELECT e FROM Employee e WHERE e.salary < :maxSalary",
+                queryParam,
+                "Multiple (java.util.List)",
+                0);
 
         bot.button(IDialogConstants.FINISH_LABEL).click();
         bot.button(IDialogConstants.OK_LABEL).click();
         bot.waitUntil(Conditions.shellIsActive(Messages.bdmDeployedTitle), 30000);
         bot.button(IDialogConstants.OK_LABEL).click();
 
-        // Validate model content
         validateBDMContent();
-
-        // Export business data model
         exportBDM();
-
     }
 
     protected void validateBDMContent() {
@@ -233,178 +197,6 @@ public class CreateDeployExportBusinessObjectIT {
                         List.class.getName()));
         assertThat(employeeBusinessObject.getQueries().get(0).getQueryParameters()).extracting("name", "className")
                 .containsExactly(tuple("maxSalary", Double.class.getName()));
-    }
-
-    protected void addCustomQuery(final String boName, final String queryName, final String content,
-            final Map<String, String> queryParam,
-            final String returnType, final int queryIndex) {
-        bot.tabItem("Queries").activate();
-        bot.radio("Custom").click();
-        bot.buttonInGroup("Add", boName).click();
-        final SWTBotTable table = bot.tableInGroup(boName);
-
-        table.click(queryIndex, 1);
-        bot.textInGroup("query1", boName).typeText(queryName);
-
-        table.click(queryIndex, 2);
-        SWTBotShell activeShell = bot.activeShell();
-        bot.button("...").click();
-        bot.waitUntil(Conditions.shellIsActive("Create query"));
-        bot.styledText().setText(content);
-
-        final SWTBotTable paramTableBot = bot.table();
-        final int rowCount = paramTableBot.rowCount();
-        final List<String> items = new ArrayList<>();
-        for (int i = 0; i < rowCount; i++) {
-            items.add(paramTableBot.getTableItem(i).getText());
-        }
-        if (!items.isEmpty()) {
-            paramTableBot.select(items.toArray(new String[items.size()]));
-        }
-        bot.button("Delete").click();
-
-        int index = 0;
-        for (final Entry<String, String> paramEntry : queryParam.entrySet()) {
-            bot.button("Add").click();
-            paramTableBot.click(index, 0);
-            bot.text("param1").typeText(paramEntry.getKey());
-
-            paramTableBot.click(index, 1);
-            bot.ccomboBox(String.class.getName()).setSelection(paramEntry.getValue());
-            keyboard.pressShortcut(Keystrokes.CR);
-            index++;
-        }
-
-        bot.comboBoxWithLabel("Result type").setSelection(returnType);
-        bot.button(IDialogConstants.OK_LABEL).click();
-        activeShell.setFocus();
-    }
-
-    protected void setMandatory(final String boName, final String attributeName) {
-        bot.tabItem("Attributes").activate();
-        final SWTBotTable attributeTable = bot.tableInGroup(boName);
-        final int attributeIndex = attributeTable.indexOf(attributeName, 0);
-        attributeTable.click(attributeIndex, 3);
-    }
-
-    protected void setMultiple(final String boName, final String attributeName) {
-        bot.tabItem("Attributes").activate();
-        final SWTBotTable attributeTable = bot.tableInGroup(boName);
-        final int attributeIndex = attributeTable.indexOf(attributeName, 0);
-        attributeTable.click(attributeIndex, 2);
-    }
-
-    protected void setAttributeLength(final String boName, final String attributeName, final String length) {
-        bot.tabItem("Attributes").activate();
-        final SWTBotTable attributeTable = bot.tableInGroup(boName);
-        final int attributeIndex = attributeTable.indexOf(attributeName, 0);
-        attributeTable.select(attributeIndex);
-        bot.ccomboBoxInGroup("Details for " + attributeName).setText(length);
-    }
-
-    protected void setRelationType(final String boName, final String attributeName, final String relationType) {
-        bot.tabItem("Attributes").activate();
-        final SWTBotTable attributeTable = bot.tableInGroup(boName);
-        final int attributeIndex = attributeTable.indexOf(attributeName, 0);
-        attributeTable.select(attributeIndex);
-        bot.comboBoxInGroup("Details for " + attributeName).setSelection(relationType);
-    }
-
-    protected void addAttribute(final String boName, final String attributeName, final String type,
-            final int attributeIndex) {
-        SWTBotShell activeShell = bot.activeShell();
-        bot.tabItem("Attributes").activate();
-        bot.buttonInGroup("Add", boName).click();
-        final SWTBotTable attributeTable = bot.tableInGroup(boName);
-
-        attributeTable.click(attributeIndex, 0);
-        bot.textInGroup("attribute1", boName).setText(attributeName);
-        attributeTable.click(attributeIndex, 1);
-
-        SWTBot activeBot = activeShell.bot();
-        SWTBotCCombo ccomboBoxInGroup = activeBot.ccomboBoxWithId(FieldTypeEditingSupport.TYPE_COMBO_EDITOR_ID);
-        activeBot.waitUntil(new DefaultCondition() {
-
-            @Override
-            public boolean test() throws Exception {
-                return Stream.of(ccomboBoxInGroup.items()).anyMatch(type::equals);
-            }
-
-            @Override
-            public String getFailureMessage() {
-                return String.format("No attribute type '%s' found in combo", type);
-            }
-        });
-        ccomboBoxInGroup.setSelection(type);
-    }
-
-    protected void addConstraint(final String boName, final String constraintName, final String[] selectFields,
-            final int constraintIndex) {
-        bot.tabItem("Unique constraints").activate();
-        bot.buttonInGroup("Add", boName).click();
-        final SWTBotTable table = bot.tableInGroup(boName);
-
-        table.click(constraintIndex, 0);
-        bot.textInGroup("UNIQUE_CONSTRAINT_1", boName).typeText(constraintName);
-
-        table.click(constraintIndex, 1);
-        SWTBotShell activeShell = bot.activeShell();
-        bot.button("...").click();
-        bot.waitUntil(Conditions.shellIsActive("Select attributes"));
-        for (final String f : selectFields) {
-            bot.table().getTableItem(f).check();
-        }
-        bot.button(IDialogConstants.OK_LABEL).click();
-        activeShell.setFocus();
-    }
-
-    protected void editConstraint(final String boName, final String[] selectFields, final int constraintIndex) {
-        bot.tabItem("Unique constraints").activate();
-        final SWTBotTable table = bot.tableInGroup(boName);
-        table.click(constraintIndex, 0);
-        table.click(constraintIndex, 1);
-        SWTBotShell activeShell = bot.activeShell();
-        bot.button("...").click();
-        bot.waitUntil(Conditions.shellIsActive("Select attributes"));
-        for (final String f : selectFields) {
-            bot.table().getTableItem(f).check();
-        }
-        bot.button(IDialogConstants.OK_LABEL).click();
-        activeShell.setFocus();
-    }
-
-    protected void addIndex(final String boName, final String indexName, final String[] selectFields, final int indexIndex) {
-        bot.tabItem("Indexes").activate();
-        bot.buttonInGroup("Add", boName).click();
-        final SWTBotTable table = bot.tableInGroup(boName);
-
-        table.click(indexIndex, 0);
-        bot.textInGroup("INDEX_1", boName).typeText(indexName);
-
-        table.click(indexIndex, 1);
-        SWTBotShell activeShell = bot.activeShell();
-        bot.button("...").click();
-        bot.waitUntil(Conditions.shellIsActive("Select attributes for " + indexName));
-        bot.tableWithLabel("Available attributes").select(selectFields);
-        bot.button("Add").click();
-        bot.button(IDialogConstants.OK_LABEL).click();
-        activeShell.setFocus();
-    }
-
-    protected void editIndex(final String boName, final String indexName, final String[] selectFields,
-            final int indexIndex) {
-        bot.tabItem("Indexes").activate();
-        final SWTBotTable table = bot.tableInGroup(boName);
-        table.click(indexIndex, 1);
-        SWTBotShell activeShell = bot.activeShell();
-        bot.button("...").click();
-        bot.waitUntil(Conditions.shellIsActive("Select attributes for " + indexName));
-        bot.tableWithLabel("Available attributes").select(selectFields);
-        bot.button("Add").click();
-        bot.tableWithLabel("Indexed attributes").select(selectFields);
-        bot.button("Up").click();
-        bot.button(IDialogConstants.OK_LABEL).click();
-        activeShell.setFocus();
     }
 
     protected void exportBDM() {
