e50cc56a9def392396f22111e42d77ff12615220
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-01-04 14:24:50 +0100

    feat(accessControl) improve refactoring report (#916)
    Closes BS-17811

--- bundles/plugins/org.bonitasoft.studio.common/src/org/bonitasoft/studio/common/jface/dialog/TypedLabelProvider.java
+++ bundles/plugins/org.bonitasoft.studio.common/src/org/bonitasoft/studio/common/jface/dialog/TypedLabelProvider.java
@@ -2,11 +2,14 @@ package org.bonitasoft.studio.common.jface.dialog;
 
 import org.eclipse.swt.graphics.Image;
 
-
-public interface TypedLabelProvider<T>  {
+public interface TypedLabelProvider<T> {
 
     String getText(T element);
-  
+
     Image getImage(T element);
-    
+
+    default String getToolTipText(T element) {
+        return null;
+    }
+
 }
