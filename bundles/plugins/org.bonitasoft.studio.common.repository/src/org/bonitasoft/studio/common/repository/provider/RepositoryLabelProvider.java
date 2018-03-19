9c6d6fce6b8b8fe1c2d821a08ec7b5146e37e8bd
Adrien <adrien.lachambre@bonitasoft.com>
2018-03-19 09:00:00 +0100

    feat(team) add tooltips on swich repo dialog (#1062)
    closes [BS-18190](https://bonitasoft.atlassian.net/browse/BS-18190)

--- bundles/plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/provider/RepositoryLabelProvider.java
+++ bundles/plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/provider/RepositoryLabelProvider.java
@@ -11,31 +11,34 @@ import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.team.svn.core.SVNTeamPlugin;
 
 public class RepositoryLabelProvider extends StyledCellLabelProvider {
 
-
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
      * @see org.eclipse.jface.viewers.IColorProvider#getBackground(java.lang.Object)
      */
     public Color getBackground(final Object element) {
         return null;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
      * @see org.eclipse.jface.viewers.IColorProvider#getForeground(java.lang.Object)
      */
     public Color getForeground(final Object element) {
-        final IRepository repository = (IRepository) element ;
-        if(RepositoryManager.getInstance().getCurrentRepository().equals(element)){
+        final IRepository repository = (IRepository) element;
+        if (RepositoryManager.getInstance().getCurrentRepository().equals(element)) {
             return ColorConstants.gray;
-        }else if (!ProductVersion.sameMinorVersion(repository.getVersion())){
+        } else if (!ProductVersion.sameMinorVersion(repository.getVersion())) {
             return ColorConstants.red;
         }
         return null;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
      * @see org.eclipse.jface.viewers.IFontProvider#getFont(java.lang.Object)
      */
     public Font getFont(final Object element) {
@@ -46,11 +49,22 @@ public class RepositoryLabelProvider extends StyledCellLabelProvider {
         return element.getDisplayName();
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
      * @see org.eclipse.jface.viewers.LabelProvider#getImage(java.lang.Object)
      */
     public Image getImage(final IRepository element) {
         return element.getIcon();
+    }
+
+    @Override
+    public String getToolTipText(Object element) {
+        IRepository repo = (IRepository) element;
+        return repo.isShared(SVNTeamPlugin.NATURE_ID)
+                ? Messages.sharedWithSvn
+                : repo.isShared()
+                        ? Messages.sharedWithGit
+                        : Messages.localRepository;
     }
 
     @Override
@@ -59,13 +73,13 @@ public class RepositoryLabelProvider extends StyledCellLabelProvider {
         final StyledString styledString = new StyledString();
 
         styledString.append(getText(element), null);
-        if(RepositoryManager.getInstance().getCurrentRepository().equals(element)){
-            styledString.append(" -- ",StyledString.QUALIFIER_STYLER) ;
+        if (RepositoryManager.getInstance().getCurrentRepository().equals(element)) {
+            styledString.append(" -- ", StyledString.QUALIFIER_STYLER);
             styledString.append(Messages.current, StyledString.DECORATIONS_STYLER);
         }
         cell.setForeground(getForeground(element));
         cell.setText(styledString.getString());
-        cell.setImage(getImage(element)) ;
+        cell.setImage(getImage(element));
         cell.setStyleRanges(styledString.getStyleRanges());
     }
 }
