fb2f813ac44d4b756d6e1841e0739ab38e84d6de
Adrien <adrien.lachambre@bonitasoft.com>
2018-06-28 16:16:05 +0200

    feat(uuid) pool uuid (#1224)
    * keep uuid on messages / call activities when renaming a target pool
    
    closes [BS-18500](https://bonitasoft.atlassian.net/browse/BS-18500)

--- bundles/plugins/org.bonitasoft.studio.diagram.custom/src/org/bonitasoft/studio/diagram/custom/refactoring/ProcessNamingTools.java
+++ bundles/plugins/org.bonitasoft.studio.diagram.custom/src/org/bonitasoft/studio/diagram/custom/refactoring/ProcessNamingTools.java
@@ -5,14 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.diagram.custom.refactoring;
 
@@ -26,6 +24,7 @@ import java.util.Set;
 import org.bonitasoft.studio.common.ConfigurationIdProvider;
 import org.bonitasoft.studio.common.ExpressionConstants;
 import org.bonitasoft.studio.common.NamingUtils;
+import org.bonitasoft.studio.common.emf.tools.EMFModelUpdater;
 import org.bonitasoft.studio.common.emf.tools.ModelHelper;
 import org.bonitasoft.studio.common.log.BonitaStudioLog;
 import org.bonitasoft.studio.common.repository.RepositoryManager;
@@ -58,142 +57,156 @@ import org.eclipse.ui.progress.IProgressService;
 
 /**
  * @author Romain Bioteau
- *
  */
 public class ProcessNamingTools {
 
-	private EditingDomain editingDomain;
-	private DiagramRepositoryStore diagramStore;
+    private EditingDomain editingDomain;
+    private DiagramRepositoryStore diagramStore;
 
-	public ProcessNamingTools(EditingDomain editingDomain){
-		this.editingDomain = editingDomain;
-		this.diagramStore = (DiagramRepositoryStore) RepositoryManager.getInstance().getRepositoryStore(DiagramRepositoryStore.class);
-	}
-	
-	protected Expression createStringExpression(String value) {
-		Expression exp = ExpressionFactory.eINSTANCE.createExpression();
-		exp.setName(value);
-		exp.setContent(value);
-		exp.setReturnType(String.class.getName());
-		exp.setType(ExpressionConstants.CONSTANT_TYPE);
-		exp.setReturnTypeFixed(true);
-		return exp;
-	}
+    public ProcessNamingTools(EditingDomain editingDomain) {
+        this.editingDomain = editingDomain;
+        this.diagramStore = RepositoryManager.getInstance().getRepositoryStore(DiagramRepositoryStore.class);
+    }
 
+    protected Expression createStringExpression(String value) {
+        Expression exp = ExpressionFactory.eINSTANCE.createExpression();
+        exp.setName(value);
+        exp.setContent(value);
+        exp.setReturnType(String.class.getName());
+        exp.setType(ExpressionConstants.CONSTANT_TYPE);
+        exp.setReturnTypeFixed(true);
+        return exp;
+    }
 
-	public void changeProcessNameAndVersion(final AbstractProcess process,final String name, final String version) {
-		proceedForPools(process, name, process.getName(), ((AbstractProcess) process).getVersion(), version);
-		if(process instanceof MainProcess){
-			editingDomain.getCommandStack().execute(SetCommand.create(editingDomain, process, ProcessPackage.Literals.ABSTRACT_PROCESS__AUTHOR, System.getProperty("user.name", "Unknown")));
-			editingDomain.getCommandStack().execute(SetCommand.create(editingDomain, process, ProcessPackage.Literals.MAIN_PROCESS__CONFIG_ID, ConfigurationIdProvider.getConfigurationIdProvider().getConfigurationId((MainProcess) process)));
-		}
-	}
-	
-	public void proceedForPools(final Element pool,final String newPoolName,final String oldPoolName,final String oldVersion,final String newVersion) {
-		List<AbstractProcess> processes = diagramStore.getAllProcesses() ;
-		StringBuilder activitiesToUpdateMsg = new StringBuilder() ;
-		final Set<CallActivity> toUpdate = new HashSet<CallActivity>();
-		for(AbstractProcess process : processes){
-			List<CallActivity> callActivities = ModelHelper.getAllItemsOfType(process, ProcessPackage.Literals.CALL_ACTIVITY) ;
-			for(CallActivity sub : callActivities){
-				String subprocessName = null;
-				if(sub.getCalledActivityName() != null
-						&& sub.getCalledActivityName().getContent() != null){
-					subprocessName = sub.getCalledActivityName().getContent();
-				}
-				String subprocessVersion = null;
-				if(sub.getCalledActivityVersion() != null
-						&& sub.getCalledActivityVersion().getContent() != null){
-					subprocessVersion = sub.getCalledActivityVersion().getContent();
-				}
+    public void changeProcessNameAndVersion(final AbstractProcess process, final String name, final String version) {
+        proceedForPools(process, name, process.getName(), process.getVersion(), version);
+        if (process instanceof MainProcess) {
+            editingDomain.getCommandStack().execute(SetCommand.create(editingDomain, process,
+                    ProcessPackage.Literals.ABSTRACT_PROCESS__AUTHOR, System.getProperty("user.name", "Unknown")));
+            editingDomain.getCommandStack()
+                    .execute(SetCommand.create(editingDomain, process, ProcessPackage.Literals.MAIN_PROCESS__CONFIG_ID,
+                            ConfigurationIdProvider.getConfigurationIdProvider().getConfigurationId((MainProcess) process)));
+        }
+    }
 
-				if(subprocessName != null && subprocessName.equals(oldPoolName) //same pool name
-						&& ((subprocessVersion == null || subprocessVersion.isEmpty() /*ie Latest*/) || subprocessVersion.equals(oldVersion))){ //same version or Latest
-					activitiesToUpdateMsg.append(sub.getName()+" ("+process.getName()+")");
-					activitiesToUpdateMsg.append(SWT.CR) ;
-					toUpdate.add(sub) ;
-				}
-			}
-		}
+    public void proceedForPools(final Element pool, final String newPoolName, final String oldPoolName,
+            final String oldVersion, final String newVersion) {
+        List<AbstractProcess> processes = diagramStore.getAllProcesses();
+        StringBuilder activitiesToUpdateMsg = new StringBuilder();
+        final Set<CallActivity> toUpdate = new HashSet<>();
+        for (AbstractProcess process : processes) {
+            List<CallActivity> callActivities = ModelHelper.getAllItemsOfType(process,
+                    ProcessPackage.Literals.CALL_ACTIVITY);
+            for (CallActivity sub : callActivities) {
+                String subprocessName = null;
+                if (sub.getCalledActivityName() != null
+                        && sub.getCalledActivityName().getContent() != null) {
+                    subprocessName = sub.getCalledActivityName().getContent();
+                }
+                String subprocessVersion = null;
+                if (sub.getCalledActivityVersion() != null
+                        && sub.getCalledActivityVersion().getContent() != null) {
+                    subprocessVersion = sub.getCalledActivityVersion().getContent();
+                }
 
-		CompoundCommand cc = new CompoundCommand("Rename pool") ;
-		cc.append(SetCommand.create(editingDomain,pool, ProcessPackage.eINSTANCE.getElement_Name(), newPoolName)) ;
-		cc.append(SetCommand.create(editingDomain, pool, ProcessPackage.eINSTANCE.getAbstractProcess_Version(), newVersion)) ;
-		editingDomain.getCommandStack().execute(cc) ;
+                if (subprocessName != null && subprocessName.equals(oldPoolName) //same pool name
+                        && ((subprocessVersion == null || subprocessVersion.isEmpty() /* ie Latest */)
+                                || subprocessVersion.equals(oldVersion))) { //same version or Latest
+                    activitiesToUpdateMsg.append(sub.getName() + " (" + process.getName() + ")");
+                    activitiesToUpdateMsg.append(SWT.CR);
+                    toUpdate.add(sub);
+                }
+            }
+        }
 
-		if(!toUpdate.isEmpty() && MessageDialog.openQuestion(Display.getDefault().getActiveShell(), Messages.updateReferencesTitle, Messages.bind(Messages.updateReferencesMsg,activitiesToUpdateMsg.toString()))){
-			IProgressService service = PlatformUI.getWorkbench().getProgressService() ;
-			try {
-				service.run(true,false, new IRunnableWithProgress() {
+        CompoundCommand cc = new CompoundCommand("Rename pool");
+        cc.append(SetCommand.create(editingDomain, pool, ProcessPackage.eINSTANCE.getElement_Name(), newPoolName));
+        cc.append(SetCommand.create(editingDomain, pool, ProcessPackage.eINSTANCE.getAbstractProcess_Version(), newVersion));
+        editingDomain.getCommandStack().execute(cc);
 
-					@Override
-					public void run(IProgressMonitor monitor) throws InvocationTargetException,InterruptedException {
-						monitor.beginTask(Messages.updatingReferences, IProgressMonitor.UNKNOWN) ;
-						for(CallActivity sub : toUpdate){
-							String subprocessName = null;
-							if(sub.getCalledActivityName() != null
-									&& sub.getCalledActivityName().getContent() != null){
-								subprocessName = sub.getCalledActivityName().getContent();
-							}
-							String subprocessVersion = null;
-							if(sub.getCalledActivityVersion() != null
-									&& sub.getCalledActivityVersion().getContent() != null){
-								subprocessVersion = sub.getCalledActivityVersion().getContent();
-							}
-							if(subprocessName != null){
+        if (!toUpdate.isEmpty()
+                && MessageDialog.openQuestion(Display.getDefault().getActiveShell(), Messages.updateReferencesTitle,
+                        Messages.bind(Messages.updateReferencesMsg, activitiesToUpdateMsg.toString()))) {
+            IProgressService service = PlatformUI.getWorkbench().getProgressService();
+            try {
+                service.run(true, false, new IRunnableWithProgress() {
 
-								CompoundCommand cc = new CompoundCommand("Update pool references") ;
-								CallActivity toModify = sub ;
-								TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(toModify) ;
-								boolean loadResource = domain == null ;
-								Resource res = null ;
-								if(loadResource){
-									URI uri = EcoreUtil.getURI(sub) ;
-									domain = WorkspaceEditingDomainFactory.INSTANCE.createEditingDomain() ;
-									res = domain.getResourceSet().createResource(uri) ;
+                    @Override
+                    public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+                        monitor.beginTask(Messages.updatingReferences, IProgressMonitor.UNKNOWN);
+                        for (CallActivity sub : toUpdate) {
+                            String subprocessName = null;
+                            if (sub.getCalledActivityName() != null
+                                    && sub.getCalledActivityName().getContent() != null) {
+                                subprocessName = sub.getCalledActivityName().getContent();
+                            }
+                            String subprocessVersion = null;
+                            if (sub.getCalledActivityVersion() != null
+                                    && sub.getCalledActivityVersion().getContent() != null) {
+                                subprocessVersion = sub.getCalledActivityVersion().getContent();
+                            }
+                            if (subprocessName != null) {
 
-									try {
-										res.load(Collections.emptyMap()) ;
-									} catch (IOException e) {
-										BonitaStudioLog.error(e) ;
-									}
+                                CompoundCommand cc = new CompoundCommand("Update pool references");
+                                CallActivity toModify = sub;
+                                TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(toModify);
+                                boolean loadResource = domain == null;
+                                Resource res = null;
+                                if (loadResource) {
+                                    URI uri = EcoreUtil.getURI(sub);
+                                    domain = WorkspaceEditingDomainFactory.INSTANCE.createEditingDomain();
+                                    res = domain.getResourceSet().createResource(uri);
 
-									MainProcess mainProc = (MainProcess) res.getContents().get(0) ;
-									for(EObject subproc : ModelHelper.getAllItemsOfType(mainProc, ProcessPackage.Literals.CALL_ACTIVITY)){
-										if(ModelHelper.getEObjectID(subproc).equals(EcoreUtil.getURI(sub).fragment())){
-											toModify = (CallActivity) subproc ;
-											break ;
-										}
-									}
-								}
+                                    try {
+                                        res.load(Collections.emptyMap());
+                                    } catch (IOException e) {
+                                        BonitaStudioLog.error(e);
+                                    }
 
-								if(!subprocessName.equals(NamingUtils.convertToId(newPoolName))){
-									cc.append(SetCommand.create(domain, toModify, ProcessPackage.Literals.CALL_ACTIVITY__CALLED_ACTIVITY_NAME, createStringExpression(newPoolName))) ;
-								}
-								if(subprocessVersion != null  && newVersion != null && !(newVersion.equals(oldVersion))){
-									cc.append(SetCommand.create(domain, toModify, ProcessPackage.Literals.CALL_ACTIVITY__CALLED_ACTIVITY_VERSION, createStringExpression(newVersion))) ;
-								}
-								domain.getCommandStack().execute(cc) ;
+                                    MainProcess mainProc = (MainProcess) res.getContents().get(0);
+                                    for (EObject subproc : ModelHelper.getAllItemsOfType(mainProc,
+                                            ProcessPackage.Literals.CALL_ACTIVITY)) {
+                                        if (ModelHelper.getEObjectID(subproc).equals(EcoreUtil.getURI(sub).fragment())) {
+                                            toModify = (CallActivity) subproc;
+                                            break;
+                                        }
+                                    }
+                                }
 
+                                if (!subprocessName.equals(NamingUtils.convertToId(newPoolName))) {
+                                    Expression calledActivityNameExpression = toModify.getCalledActivityName();
+                                    EMFModelUpdater<EObject> updater = new EMFModelUpdater<>()
+                                            .from(calledActivityNameExpression);
+                                    updater.editWorkingCopy(createStringExpression(newPoolName));
+                                    cc.append(updater.createUpdateCommand(domain));
+                                }
+                                if (subprocessVersion != null && newVersion != null && !(newVersion.equals(oldVersion))) {
+                                    Expression calledActivityVersionExpression = toModify.getCalledActivityVersion();
+                                    EMFModelUpdater<EObject> updater = new EMFModelUpdater<>()
+                                            .from(calledActivityVersionExpression);
+                                    updater.editWorkingCopy(createStringExpression(newVersion));
+                                    cc.append(updater.createUpdateCommand(domain));
+                                }
+                                domain.getCommandStack().execute(cc);
 
-								if(loadResource && res != null){
-									try {
-										res.save(Collections.emptyMap()) ;
-									} catch (IOException e) {
-										BonitaStudioLog.error(e) ;
-									}
-									domain.dispose();
-								}
-							}
-						}
+                                if (loadResource && res != null) {
+                                    try {
+                                        res.save(Collections.emptyMap());
+                                    } catch (IOException e) {
+                                        BonitaStudioLog.error(e);
+                                    }
+                                    domain.dispose();
+                                }
+                            }
+                        }
 
-					}
-				});
-			} catch (InvocationTargetException e) {
-				BonitaStudioLog.error(e) ;
-			} catch (InterruptedException e) {
-				BonitaStudioLog.error(e) ;
-			}
-		}
-	}
+                    }
+                });
+            } catch (InvocationTargetException e) {
+                BonitaStudioLog.error(e);
+            } catch (InterruptedException e) {
+                BonitaStudioLog.error(e);
+            }
+        }
+    }
 }
