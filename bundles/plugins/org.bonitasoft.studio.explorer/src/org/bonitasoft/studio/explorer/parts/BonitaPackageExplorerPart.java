f86f3be84ac24d9a5de8980f1787d213cc400940
Adrien <adrien.lachambre@bonitasoft.com>
2018-08-02 10:06:05 +0200

    feat(package explorer) open wizards (#1259)
    * feat(package explorer) open wizards
    
    * open edition wizards for bdm and orga if the user try to open one of
    those files from the package explorer
    
    closes [BS-18637](https://bonitasoft.atlassian.net/browse/BS-18637)

--- bundles/plugins/org.bonitasoft.studio.explorer/src/org/bonitasoft/studio/explorer/parts/BonitaPackageExplorerPart.java
+++ bundles/plugins/org.bonitasoft.studio.explorer/src/org/bonitasoft/studio/explorer/parts/BonitaPackageExplorerPart.java
@@ -14,11 +14,22 @@
  */
 package org.bonitasoft.studio.explorer.parts;
 
+import java.lang.reflect.Field;
 import java.util.Objects;
+import java.util.Optional;
 
+import org.apache.commons.lang.reflect.FieldUtils;
 import org.bonitasoft.studio.common.repository.RepositoryManager;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.core.internal.resources.File;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.ListenerList;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart;
+import org.eclipse.jface.viewers.IOpenListener;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
@@ -29,15 +40,60 @@ public class BonitaPackageExplorerPart extends PackageExplorerPart {
 
     public static final String VIEW_ID = "org.bonitasoft.studio.explorer";
 
+    private static final String MANAGE_ORGA_COMMAND = "org.bonitasoft.studio.organization.manage";
+    private static final String MANAGE_BDM_COMMAND = "org.bonitasoft.studio.businessobject.manage";
+
+    private ECommandService eCommandService;
+    private EHandlerService eHandlerService;
+
     @Override
     public void createPartControl(Composite parent) {
         super.createPartControl(parent);
+        eCommandService = getSite().getService(ECommandService.class);
+        eHandlerService = getSite().getService(EHandlerService.class);
         IProject currentProject = RepositoryManager.getInstance().getCurrentRepository().getProject();
 
         TreeViewer treeViewer = getTreeViewer();
         treeViewer.addFilter(onlyCurrentProject(currentProject));
 
+        try {
+            Field listenersField = StructuredViewer.class.getDeclaredField("openListeners");
+            listenersField.setAccessible(true);
+            ListenerList<IOpenListener> openListeners = (ListenerList<IOpenListener>) FieldUtils.readField(listenersField,
+                    treeViewer);
+            Optional<IOpenListener> initialListener = openListeners.stream().findFirst();
+            openListeners.clear();
+            openListeners.add(createOpenListener(initialListener));
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
         // TODO: filter unwanted folder, add popup menu filter ect
+    }
+
+    /**
+     * Overwrite the initial behavior to open our wizards editor for the BDM and the Organization.
+     */
+    private IOpenListener createOpenListener(Optional<IOpenListener> initialListener) {
+        return e -> {
+            TreeSelection treeSelection = (TreeSelection) e.getSelection();
+            if (treeSelection.getFirstElement() instanceof File) {
+                File file = (File) treeSelection.getFirstElement();
+                if (Objects.equals(file.getName(), ("bom.xml"))) {
+                    openWizard(MANAGE_BDM_COMMAND);
+                    return;
+                } else if (file.getName().endsWith(".organization")) {
+                    openWizard(MANAGE_ORGA_COMMAND);
+                    return;
+                }
+            }
+            initialListener.ifPresent(listener -> listener.open(e));
+        };
+    }
+
+    private void openWizard(String command) {
+        ParameterizedCommand openCommand = ParameterizedCommand.generateCommand(
+                eCommandService.getCommand(command), null);
+        eHandlerService.executeHandler(openCommand);
     }
 
     private ViewerFilter onlyCurrentProject(IProject currentProject) {
