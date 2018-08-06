d3a129adec267ac9e816db38de462103c7afb893
Adrien <adrien.lachambre@bonitasoft.com>
2018-08-06 14:02:03 +0200

    feat(diagram) open diagram (#1262)
    * Open diagrams using IDE.opensEditors(..), else the diagram is open
    with a strange URI, which leads to some issues with the package explorer
    (diagrams can be opened twice because the first URI isn't the correct
    one)
    
    closes [BS-18655](https://bonitasoft.atlassian.net/browse/BS-18655)

--- bundles/plugins/org.bonitasoft.studio.diagram.custom/src/org/bonitasoft/studio/diagram/custom/repository/DiagramFileStore.java
+++ bundles/plugins/org.bonitasoft.studio.diagram.custom/src/org/bonitasoft/studio/diagram/custom/repository/DiagramFileStore.java
@@ -58,24 +58,20 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.emf.common.ui.URIEditorInput;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
-import org.eclipse.gmf.runtime.common.ui.services.editor.EditorService;
 import org.eclipse.gmf.runtime.diagram.core.listener.DiagramEventBroker;
 import org.eclipse.gmf.runtime.diagram.core.listener.NotificationListener;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
-import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
@@ -88,6 +84,7 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicates;
@@ -267,8 +264,7 @@ public class DiagramFileStore extends EMFFileStore implements IRepositoryFileSto
             final MainProcess content = getContent();
             Assert.isLegal(content != null);
             Assert.isLegal(emfResource != null && emfResource.isLoaded());
-            final Diagram diagram = ModelHelper.getDiagramFor(content, emfResource);
-            part = EditorService.getInstance().openEditor(new URIEditorInput(EcoreUtil.getURI(diagram)));
+            part = IDE.openEditor(getActivePage(), getResource());
             if (part instanceof DiagramEditor) {
                 final DiagramEditor editor = (DiagramEditor) part;
                 final MainProcess mainProcess = (MainProcess) editor.getDiagramEditPart().resolveSemanticElement();
@@ -287,6 +283,10 @@ public class DiagramFileStore extends EMFFileStore implements IRepositoryFileSto
             BonitaStudioLog.error(e);
         }
         return part;
+    }
+
+    private IWorkbenchPage getActivePage() {
+        return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
     }
 
     protected IWorkbenchPage closeOpenedEditorWithoutSaving() {
