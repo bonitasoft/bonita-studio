7f54c2dc2e3a74b999cb83801a44ec92d6c2f70d
Adrien <adrien.lachambre@bonitasoft.com>
2018-08-08 15:34:28 +0200

    feat(package explorer) refactoring (#1267)
    * feat(package explorer) refactoring
    
    * extract the first filter logic into a dedicated class and add a test
    
    closes [BS-18665](https://bonitasoft.atlassian.net/browse/BS-18665)
    
    * * headers

---
+++ bundles/plugins/org.bonitasoft.studio.explorer/src/org/bonitasoft/studio/explorer/filters/CurrentProjectFilter.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2018 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.explorer.filters;
+
+import java.util.Objects;
+
+import org.bonitasoft.studio.common.repository.RepositoryAccessor;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+public class CurrentProjectFilter extends ViewerFilter {
+
+    private RepositoryAccessor repositoryAccessor;
+
+    public CurrentProjectFilter(RepositoryAccessor repositoryAccessor) {
+        this.repositoryAccessor = repositoryAccessor;
+    }
+
+    @Override
+    public boolean select(Viewer viewer, Object parentElement, Object element) {
+        if (element instanceof IProject) {
+            IProject project = (IProject) element;
+            IProject currentProject = repositoryAccessor.getCurrentRepository().getProject();
+            return Objects.equals(currentProject, project);
+        }
+        return true;
+    }
+
+}
