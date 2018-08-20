3a729e6e3747335c39d45529e8cfaa2ac9b6ab86
Adrien <adrien.lachambre@bonitasoft.com>
2018-08-20 13:31:00 +0200

    feat(project explorer) open wizards (#1271)
    * open the bdm wizard or the orga wizard when the user double click on
    one of thoses files in the explorer
    * extract command search logic in an OpenCommandProvider class
    * use file store instead of file name to decide if a file should be
    opened with a given command
    * open the wizard editor for connector definition
    * add an optional parameter to the edit connector def handler, in order
    to be able to open directly the wizard
    * for all resources, we use fileStore.open when we are able to find a
    filestore for this file to open
    * doOPen methods have been implemented for some resources in order to
    open the wizard
    * implement missing doOpen methods (connectors impl, actors filter, data
    type)

--- bundles/plugins/org.bonitasoft.studio.actors/src/org/bonitasoft/studio/actors/repository/ActorFilterDefFileStore.java
+++ bundles/plugins/org.bonitasoft.studio.actors/src/org/bonitasoft/studio/actors/repository/ActorFilterDefFileStore.java
@@ -5,29 +5,32 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.actors.repository;
 
 import org.bonitasoft.studio.actors.ActorsPlugin;
+import org.bonitasoft.studio.actors.ui.wizard.FilterDefinitionWizard;
+import org.bonitasoft.studio.common.repository.RepositoryManager;
 import org.bonitasoft.studio.common.repository.store.AbstractEMFRepositoryStore;
 import org.bonitasoft.studio.connector.model.definition.AbstractDefFileStore;
 import org.bonitasoft.studio.connector.model.definition.ConnectorDefinition;
+import org.bonitasoft.studio.connector.model.definition.dialog.ConnectorDefinitionWizardDialog;
+import org.bonitasoft.studio.connector.model.i18n.DefinitionResourceProvider;
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPart;
 import org.osgi.framework.Bundle;
-
 
 /**
  * @author Romain Bioteau
  * @author Baptiste Mesta
- *
  */
 public class ActorFilterDefFileStore extends AbstractDefFileStore {
 
@@ -44,12 +47,12 @@ public class ActorFilterDefFileStore extends AbstractDefFileStore {
     public String getDisplayName() {
         ActorFilterDefRepositoryStore store = (ActorFilterDefRepositoryStore) getParentStore();
         ConnectorDefinition def = getContent();
-        if(def != null){
+        if (def != null) {
             String defName = store.getResourceProvider().getConnectorDefinitionLabel(def);
-            if(defName == null){
+            if (defName == null) {
                 defName = def.getId();
             }
-            return defName +" ("+def.getVersion()+")";
+            return defName + " (" + def.getVersion() + ")";
         }
         return super.getDisplayName();
     }
@@ -58,9 +61,22 @@ public class ActorFilterDefFileStore extends AbstractDefFileStore {
     public Image getIcon() {
         ActorFilterDefRepositoryStore store = (ActorFilterDefRepositoryStore) getParentStore();
         ConnectorDefinition def = getContent();
-        if(def != null){
+        if (def != null) {
             return store.getResourceProvider().getDefinitionIcon(def);
         }
+        return null;
+    }
+
+    @Override
+    protected IWorkbenchPart doOpen() {
+        final ActorFilterDefRepositoryStore defStore = RepositoryManager.getInstance()
+                .getRepositoryStore(ActorFilterDefRepositoryStore.class);
+        final DefinitionResourceProvider messageProvider = DefinitionResourceProvider.getInstance(defStore,
+                ActorsPlugin.getDefault().getBundle());
+        FilterDefinitionWizard wizard = new FilterDefinitionWizard(getContent(), messageProvider);
+        WizardDialog wd = new ConnectorDefinitionWizardDialog(Display.getCurrent().getActiveShell(), wizard,
+                messageProvider);
+        wd.open();
         return null;
     }
 }
