4d30b41060bd9b5314e456763e3a400d81018ab6
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-09-05 13:40:26 +0200

    fix codesign macOS

---
+++ all-in-one/sign-macosx.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+filename=$1
+url=$2
+workDir=$3
+zipFile="$filename.zip"
+
+cd $workDir
+
+zip -q $zipFile -r $filename
+curl --request POST -F exeFile=@$zipFile $url > /tmp/$zipFile
+cat /tmp/$zipFile > $zipFile
+rm -r $filename
+unzip -q $zipFile
+rm $zipFile
