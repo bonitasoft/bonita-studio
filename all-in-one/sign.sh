338c985df5969b7f11b2d2b77aa08e86ad8a5618
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-09-06 09:25:31 +0200

    fix codesign

--- all-in-one/sign.sh
+++ all-in-one/sign.sh
@@ -6,10 +6,15 @@ output=$3
 workDir=$4
 
 cd $workDir
-if [ -f $filename ]; then 
+echo "cd into $workDir"
+if [ -f $filename ]; then
+    echo "Signing $filename..."
     curl --request POST -F exeFile=@$filename $url > /tmp/$filename
     cat /tmp/$filename > $output
     if [ $filename != $output ]; then
         rm $filename
+        echo "rm $filename"
     fi
+else
+   echo "$filename not found in $workDir. Codesign skipped."
 fi
