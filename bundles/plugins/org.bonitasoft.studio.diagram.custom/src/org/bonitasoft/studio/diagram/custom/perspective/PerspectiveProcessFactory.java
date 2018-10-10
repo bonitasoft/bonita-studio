ddad2d1faff5f994c6d6702dcb5b8872f22f544f
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-10-10 14:43:13 +0200

    feat(process) rearrange process perspective (#1375)
    * change view place

--- bundles/plugins/org.bonitasoft.studio.diagram.custom/src/org/bonitasoft/studio/diagram/custom/perspective/PerspectiveProcessFactory.java
+++ bundles/plugins/org.bonitasoft.studio.diagram.custom/src/org/bonitasoft/studio/diagram/custom/perspective/PerspectiveProcessFactory.java
@@ -17,34 +17,33 @@ package org.bonitasoft.studio.diagram.custom.perspective;
 import org.bonitasoft.studio.common.perspectives.AbstractPerspectiveFactory;
 import org.bonitasoft.studio.common.perspectives.BonitaPerspectivesUtils;
 import org.bonitasoft.studio.common.repository.RepositoryManager;
-import org.bonitasoft.studio.migration.model.report.Report;
 import org.bonitasoft.studio.model.process.diagram.part.ProcessDiagramEditor;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 
-/**
- * @author Aurelien Pupier
- */
+
 public class PerspectiveProcessFactory extends AbstractPerspectiveFactory {
 
-    public static String PROCESS_PERSPECTIVE_ID = "org.bonitasoft.studio.perspective.process";
+    public static final String PROCESS_PERSPECTIVE_ID = "org.bonitasoft.studio.perspective.process";
 
     @Override
     public void createInitialLayout(final IPageLayout layout) {
         // Editors are placed for free.
         final String editorArea = layout.getEditorArea();
-        // Bottom left.
-        final IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, (float) 0.65, //$NON-NLS-1$
-                editorArea);
-        bottomLeft.addView("org.bonitasoft.studio.views.overview.tree");
-        bottomLeft.addView("org.bonitasoft.studio.views.overview");
 
+        final IFolderLayout left = layout.createFolder(
+                "left",
+                IPageLayout.LEFT,
+                getExplorerViewRatio(),
+                editorArea);
+        left.addView("org.bonitasoft.studio.application.project.explorer");
+        left.addView("org.bonitasoft.studio.views.overview.tree");
+
+        layout.createFolder("right", IPageLayout.RIGHT, 100, editorArea);
         // Bottom right.
-        final IFolderLayout bottomRight = layout.createFolder("bottomRight", IPageLayout.RIGHT, (float) 0.3, //$NON-NLS-1$
-                "bottomLeft");
+        final IFolderLayout bottomRight = layout.createFolder("bottom", IPageLayout.BOTTOM, //$NON-NLS-1$
+                (float) 0.7, editorArea);
         bottomRight.addView("org.bonitasoft.studio.views.properties.process.general");
         bottomRight.addView("org.bonitasoft.studio.views.properties.process.data");
         bottomRight.addView("org.bonitasoft.studio.views.properties.process.execution");
@@ -54,59 +53,19 @@ public class PerspectiveProcessFactory extends AbstractPerspectiveFactory {
             bottomRight.addView(viewId);
         }
         bottomRight.addView("org.bonitasoft.studio.validation.view");
+        bottomRight.addView("org.bonitasoft.studio.views.overview");
         if (RepositoryManager.getInstance().getCurrentRepository().isShared("org.eclipse.egit.core.GitProvider")) {
             bottomRight.addView("org.eclipse.egit.ui.StagingView");
             bottomRight.addPlaceholder("org.eclipse.team.ui.GenericHistoryView");
         }
 
-        createLeftViewFolder(layout, editorArea);
     }
 
-    protected void configureIntroView(final IPageLayout layout) {
-        layout.getViewLayout("org.eclipse.ui.internal.introview").setCloseable(false);
-        layout.getViewLayout("org.eclipse.ui.internal.introview").setMoveable(false);
-    }
-
-    protected void createLeftViewFolder(final IPageLayout layout, final String editorArea) {
-        final IFolderLayout left = layout.createFolder(
-                "left",
-                IPageLayout.LEFT,
-                getExplorerViewRatio(),
-                editorArea);
-        left.addView("org.bonitasoft.studio.application.project.explorer");
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.bonitasoft.studio.common.perspectives.AbstractPerspectiveFactory#isRelevantFor(org.eclipse.ui.IEditorPart)
-     */
     @Override
     public boolean isRelevantFor(final IEditorPart part) {
-        if (part instanceof ProcessDiagramEditor) {
-            return !isOngoingMigration((ProcessDiagramEditor) part);
-        }
-        return false;
+        return part instanceof ProcessDiagramEditor;
     }
 
-    protected boolean isOngoingMigration(final ProcessDiagramEditor processDiagramEditor) {
-        final EObject element = processDiagramEditor.getDiagramEditPart().resolveSemanticElement();
-        if (element != null) {
-            final Resource resource = element.eResource();
-            if (resource != null) {
-                for (final EObject root : resource.getContents()) {
-                    if (root instanceof Report) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.bonitasoft.studio.common.perspectives.AbstractPerspectiveFactory#getID()
-     */
     @Override
     public String getID() {
         return PROCESS_PERSPECTIVE_ID;
