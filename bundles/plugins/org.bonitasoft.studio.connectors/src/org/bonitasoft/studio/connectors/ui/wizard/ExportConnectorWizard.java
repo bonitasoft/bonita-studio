1c21452d458795a5836f838d53a16159657f9ddc
Adrien <adrien.lachambre@bonitasoft.com>
2018-10-17 13:32:21 +0200

    feat(project explorer) import export connectors from explorer (#1399)
    * Import is only available on the impl folder
    * Export from the impl folder -> all implementations are available
    * Export from a given impl -> Only this def is available
    * Export from a given def -> all the impl of this def are available
    
    closes [BS-18958](https://bonitasoft.atlassian.net/browse/BS-18958)

--- bundles/plugins/org.bonitasoft.studio.connectors/src/org/bonitasoft/studio/connectors/ui/wizard/ExportConnectorWizard.java
+++ bundles/plugins/org.bonitasoft.studio.connectors/src/org/bonitasoft/studio/connectors/ui/wizard/ExportConnectorWizard.java
@@ -2,28 +2,26 @@
 /**
  * Copyright (C) 2009 BonitaSoft S.A.
  * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.connectors.ui.wizard;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
 
 import org.bonitasoft.studio.common.log.BonitaStudioLog;
 import org.bonitasoft.studio.common.repository.RepositoryManager;
-import org.bonitasoft.studio.common.repository.store.AbstractRepositoryStore;
+import org.bonitasoft.studio.common.repository.model.IRepositoryStore;
+import org.bonitasoft.studio.connector.model.definition.ConnectorDefinition;
 import org.bonitasoft.studio.connector.model.definition.IDefinitionRepositoryStore;
 import org.bonitasoft.studio.connector.model.implementation.ConnectorImplementation;
 import org.bonitasoft.studio.connector.model.implementation.provider.ConnectorImplementationContentProvider;
@@ -38,40 +36,64 @@ import org.bonitasoft.studio.pics.Pics;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IContentProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.widgets.Display;
 
 /**
  * @author Mickael Istria
- *
  */
 public class ExportConnectorWizard extends Wizard {
 
     private ExportConnectorWizardPage exportConnectorWizardPage;
+    protected Optional<ConnectorImplementation> connectorImplToExport = Optional.empty();
+    protected Optional<ConnectorDefinition> connectorDefToExport = Optional.empty();
 
-    public ExportConnectorWizard(){
-        setDefaultPageImageDescriptor(Pics.getWizban()) ;
+    public ExportConnectorWizard() {
+        setDefaultPageImageDescriptor(Pics.getWizban());
         setWindowTitle(Messages.exportConnectorTitle);
-        setNeedsProgressMonitor(true) ;
+        setNeedsProgressMonitor(true);
+    }
+
+    public ExportConnectorWizard(ConnectorImplementation connectorToExport) {
+        this();
+        this.connectorImplToExport = Optional.ofNullable(connectorToExport);
+    }
+
+    public ExportConnectorWizard(ConnectorDefinition connectorDefToExport) {
+        this();
+        this.connectorDefToExport = Optional.ofNullable(connectorDefToExport);
     }
 
     @Override
     public void addPages() {
-        exportConnectorWizardPage = new ExportConnectorWizardPage(getPageTitle(),getPageDescription(),getContentProvider(),getLabelProvider(),getDefRepositoryStore());
+        exportConnectorWizardPage = new ExportConnectorWizardPage(getPageTitle(), getPageDescription(), getContentProvider(),
+                getLabelProvider(), getDefRepositoryStore());
         addPage(exportConnectorWizardPage);
     }
 
     protected LabelProvider getLabelProvider() {
-        return new ConnectorImplementationLabelProvider((IDefinitionRepositoryStore)RepositoryManager.getInstance().getRepositoryStore(ConnectorDefRepositoryStore.class),ConnectorPlugin.getDefault().getBundle());
+        return new ConnectorImplementationLabelProvider(
+                RepositoryManager.getInstance().getRepositoryStore(ConnectorDefRepositoryStore.class),
+                ConnectorPlugin.getDefault().getBundle());
     }
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
-	protected IContentProvider getContentProvider() {
-    	ConnectorImplRepositoryStore repositoryStore = RepositoryManager.getInstance().getRepositoryStore(ConnectorImplRepositoryStore.class);
-        return new ConnectorImplementationContentProvider((AbstractRepositoryStore)repositoryStore,false);
+    protected IContentProvider getContentProvider() {
+        if (connectorImplToExport.isPresent()) {
+            return (IStructuredContentProvider) inputElement -> new ConnectorImplementation[] {
+                    connectorImplToExport.get() };
+        }
+        IRepositoryStore repositoryStore = RepositoryManager.getInstance()
+                .getRepositoryStore(ConnectorImplRepositoryStore.class);
+        if (connectorDefToExport.isPresent()) {
+            ConnectorDefinition definition = connectorDefToExport.get();
+            return new ConnectorImplementationContentProvider(repositoryStore, definition.getId(),
+                    definition.getVersion());
+        }
+        return new ConnectorImplementationContentProvider(repositoryStore, false);
     }
 
     protected String getPageDescription() {
@@ -81,62 +103,60 @@ public class ExportConnectorWizard extends Wizard {
     protected String getPageTitle() {
         return Messages.selectConnectorImplementationToExportTitle;
     }
-    
-    protected IDefinitionRepositoryStore getDefRepositoryStore(){
-    	return (IDefinitionRepositoryStore)RepositoryManager.getInstance().getRepositoryStore(ConnectorDefRepositoryStore.class);
+
+    protected IDefinitionRepositoryStore getDefRepositoryStore() {
+        return RepositoryManager.getInstance().getRepositoryStore(ConnectorDefRepositoryStore.class);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
      * @see org.eclipse.jface.wizard.Wizard#performFinish()
      */
     @Override
     public boolean performFinish() {
         try {
-            getContainer().run(true, false, new IRunnableWithProgress() {
-
-                @Override
-                public void run(IProgressMonitor progressMonitor) throws InvocationTargetException, InterruptedException {
-                    progressMonitor.beginTask(Messages.exporting, IProgressMonitor.UNKNOWN) ;
-                     String destPathFile=exportConnectorWizardPage.getDestFilePath();
-                    if (destPathFile.endsWith(File.separator)){
-                    	destPathFile=destPathFile+exportConnectorWizardPage.getDestFileName();
-                    } else {
-                    	destPathFile=destPathFile+File.separator+exportConnectorWizardPage.getDestFileName();                  }
-                    final ExportConnectorArchiveOperation operation = createExportOperation(exportConnectorWizardPage.getSelectedImplementation(), exportConnectorWizardPage.isIncludeSources(), exportConnectorWizardPage.isAddDependencies(), destPathFile) ;
-                    final IStatus status = operation.run(progressMonitor) ;
-                    displayResult(status) ;
+            getContainer().run(true, false, progressMonitor -> {
+                progressMonitor.beginTask(Messages.exporting, IProgressMonitor.UNKNOWN);
+                String destPathFile = exportConnectorWizardPage.getDestFilePath();
+                if (destPathFile.endsWith(File.separator)) {
+                    destPathFile = destPathFile + exportConnectorWizardPage.getDestFileName();
+                } else {
+                    destPathFile = destPathFile + File.separator + exportConnectorWizardPage.getDestFileName();
                 }
-            }) ;
+                final ExportConnectorArchiveOperation operation = createExportOperation(
+                        exportConnectorWizardPage.getSelectedImplementation(), exportConnectorWizardPage.isIncludeSources(),
+                        exportConnectorWizardPage.isAddDependencies(), destPathFile);
+                final IStatus status = operation.run(progressMonitor);
+                displayResult(status);
+            });
 
-        } catch (Exception e){
-        	BonitaStudioLog.error(e) ;
+        } catch (Exception e) {
+            BonitaStudioLog.error(e);
         }
-        
 
-        return true ;
+        return true;
     }
 
     protected void displayResult(final IStatus status) {
-        Display.getDefault().syncExec(new Runnable() {
-
-            @Override
-            public void run() {
-                if(!status.isOK()){
-                    MessageDialog.openError(Display.getDefault().getActiveShell(), Messages.exportStatusTitle, status.getMessage()) ;
-                }else{
-                    MessageDialog.openInformation(Display.getDefault().getActiveShell(), Messages.exportStatusTitle,Messages.exportSuccessfulMsg) ;
-                }
+        Display.getDefault().syncExec(() -> {
+            if (!status.isOK()) {
+                MessageDialog.openError(Display.getDefault().getActiveShell(), Messages.exportStatusTitle,
+                        status.getMessage());
+            } else {
+                MessageDialog.openInformation(Display.getDefault().getActiveShell(), Messages.exportStatusTitle,
+                        Messages.exportSuccessfulMsg);
             }
-        }) ;
+        });
     }
 
-    protected ExportConnectorArchiveOperation createExportOperation(ConnectorImplementation impl,boolean addSources,boolean addDependencies,String targetPath){
-        final ExportConnectorArchiveOperation operation = new ExportConnectorArchiveOperation() ;
-        operation.setImplementation(impl) ;
-        operation.setIncludeSources(addSources) ;
-        operation.setTargetPath(targetPath) ;
-        operation.setAddDependencies(addDependencies) ;
-        return operation ;
+    protected ExportConnectorArchiveOperation createExportOperation(ConnectorImplementation impl, boolean addSources,
+            boolean addDependencies, String targetPath) {
+        final ExportConnectorArchiveOperation operation = new ExportConnectorArchiveOperation();
+        operation.setImplementation(impl);
+        operation.setIncludeSources(addSources);
+        operation.setTargetPath(targetPath);
+        operation.setAddDependencies(addDependencies);
+        return operation;
     }
 
 }
