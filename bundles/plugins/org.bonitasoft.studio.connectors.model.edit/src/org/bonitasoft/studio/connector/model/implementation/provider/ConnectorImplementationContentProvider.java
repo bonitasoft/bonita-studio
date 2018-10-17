91b9258ce861a7151f0e9d94d2ad419d3c8f8ed3
Adrien <adrien.lachambre@bonitasoft.com>
2018-10-17 13:32:21 +0200

    feat(project explorer) import export connectors from explorer (#1399)
    * Import is only available on the impl folder
    * Export from the impl folder -> all implementations are available
    * Export from a given impl -> Only this def is available
    * Export from a given def -> all the impl of this def are available
    
    closes [BS-18958](https://bonitasoft.atlassian.net/browse/BS-18958)

--- bundles/plugins/org.bonitasoft.studio.connectors.model.edit/src/org/bonitasoft/studio/connector/model/implementation/provider/ConnectorImplementationContentProvider.java
+++ bundles/plugins/org.bonitasoft.studio.connectors.model.edit/src/org/bonitasoft/studio/connector/model/implementation/provider/ConnectorImplementationContentProvider.java
@@ -5,14 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.connector.model.implementation.provider;
 
@@ -27,10 +25,8 @@ import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
-
 /**
  * @author Romain Bioteau
- *
  */
 public class ConnectorImplementationContentProvider implements IStructuredContentProvider {
 
@@ -39,60 +35,70 @@ public class ConnectorImplementationContentProvider implements IStructuredConten
     private String definitionId;
     private String definitionVersion;
 
-    public ConnectorImplementationContentProvider(IRepositoryStore<IRepositoryFileStore> store ,boolean userImplementationOnly){
-        Assert.isTrue(store instanceof IImplementationRepositoryStore) ;
-        this.store = store ;
-        this.userImplementationOnly = userImplementationOnly ;
+    public ConnectorImplementationContentProvider(IRepositoryStore<IRepositoryFileStore> store,
+            boolean userImplementationOnly) {
+        Assert.isTrue(store instanceof IImplementationRepositoryStore);
+        this.store = store;
+        this.userImplementationOnly = userImplementationOnly;
     }
 
-    public ConnectorImplementationContentProvider(IRepositoryStore<IRepositoryFileStore> store, String definitionId, String definitionVersion) {
-        Assert.isTrue(store instanceof IImplementationRepositoryStore) ;
-        this.store = store ;
-        userImplementationOnly = false ;
-        this.definitionId = definitionId ;
-        this.definitionVersion = definitionVersion ;
+    public ConnectorImplementationContentProvider(IRepositoryStore<IRepositoryFileStore> store, String definitionId,
+            String definitionVersion) {
+        Assert.isTrue(store instanceof IImplementationRepositoryStore);
+        this.store = store;
+        userImplementationOnly = false;
+        this.definitionId = definitionId;
+        this.definitionVersion = definitionVersion;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
      * @see org.eclipse.jface.viewers.IContentProvider#dispose()
      */
     @Override
-    public void dispose() {}
+    public void dispose() {
+    }
 
-    /* (non-Javadoc)
-     * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object,
+     * java.lang.Object)
      */
     @Override
-    public void inputChanged(Viewer arg0, Object arg1, Object arg2) {}
+    public void inputChanged(Viewer arg0, Object arg1, Object arg2) {
+    }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
      * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
      */
     @Override
     public Object[] getElements(Object element) {
-        final List<ConnectorImplementation> connectorImplList = ((IImplementationRepositoryStore) store).getImplementations() ;
-        if(userImplementationOnly){
-            List<ConnectorImplementation> toRemove = new ArrayList<ConnectorImplementation>() ;
-            for(ConnectorImplementation implementation : connectorImplList){
-                if(implementation != null && implementation.eResource() != null){
-                    String path =  implementation.eResource().getURI().toFileString() ;
-                    if(!path.contains(store.getResource().getLocation().toFile().getAbsolutePath())){
-                        toRemove.add(implementation) ;
+        final List<ConnectorImplementation> connectorImplList = ((IImplementationRepositoryStore) store)
+                .getImplementations();
+        if (userImplementationOnly) {
+            List<ConnectorImplementation> toRemove = new ArrayList<>();
+            for (ConnectorImplementation implementation : connectorImplList) {
+                if (implementation != null && implementation.eResource() != null) {
+                    String path = implementation.eResource().getURI().toFileString();
+                    if (!path.contains(store.getResource().getLocation().toFile().getAbsolutePath())) {
+                        toRemove.add(implementation);
                     }
                 }
             }
-            connectorImplList.removeAll(toRemove) ;
+            connectorImplList.removeAll(toRemove);
         }
-        if(definitionId != null){
-            final List<ConnectorImplementation> toRemove = new ArrayList<ConnectorImplementation>() ;
-            for(ConnectorImplementation implementation : connectorImplList){
-                if(implementation != null){
-                    if(!implementation.getDefinitionId().equals(definitionId) || !implementation.getDefinitionVersion().equals(definitionVersion)){
-                        toRemove.add(implementation) ;
+        if (definitionId != null) {
+            final List<ConnectorImplementation> toRemove = new ArrayList<>();
+            for (ConnectorImplementation implementation : connectorImplList) {
+                if (implementation != null) {
+                    if (!implementation.getDefinitionId().equals(definitionId)
+                            || !implementation.getDefinitionVersion().equals(definitionVersion)) {
+                        toRemove.add(implementation);
                     }
                 }
             }
-            connectorImplList.removeAll(toRemove) ;
+            connectorImplList.removeAll(toRemove);
         }
         return connectorImplList.toArray(new ConnectorImplementation[connectorImplList.size()]);
     }
