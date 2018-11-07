0d8d63dd2f865a07b91bec6cce40fcabb7611a98
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-11-07 08:42:49 +0000

    fix(git) clone repository (#1458)
    * some rest api extension where not connected to git after cloning
    * migration was triggered twice

--- bundles/plugins/org.bonitasoft.studio.groovy/src/org/bonitasoft/studio/groovy/GroovyDocumentUtil.java
+++ bundles/plugins/org.bonitasoft.studio.groovy/src/org/bonitasoft/studio/groovy/GroovyDocumentUtil.java
@@ -1,19 +1,16 @@
 /**
  * Copyright (C) 2009 BonitaSoft S.A.
  * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
- * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.bonitasoft.studio.groovy;
@@ -24,7 +21,6 @@ import java.util.List;
 
 import org.bonitasoft.studio.common.log.BonitaStudioLog;
 import org.bonitasoft.studio.common.repository.Repository;
-import org.bonitasoft.studio.common.repository.RepositoryManager;
 import org.bonitasoft.studio.common.repository.model.IRepositoryFileStore;
 import org.bonitasoft.studio.groovy.library.FunctionsRepositoryFactory;
 import org.bonitasoft.studio.groovy.library.GroovyFunction;
@@ -48,18 +44,17 @@ import org.eclipse.jdt.core.JavaModelException;
 
 /**
  * @author Romain Bioteau
- *
  */
 public class GroovyDocumentUtil {
 
-    private static List<String> defaultMethodsNames ;
+    private static List<String> defaultMethodsNames;
 
-    public static  List<String> getDeclaredMethodsName(GroovyCompilationUnit unit) {
+    public static List<String> getDeclaredMethodsName(GroovyCompilationUnit unit) {
 
-        List<IMethod> methods = getModuleMethods(unit) ;
-        List<String> methodNames = new ArrayList<String>();
+        List<IMethod> methods = getModuleMethods(unit);
+        List<String> methodNames = new ArrayList<>();
 
-        for(IMethod m : methods){
+        for (IMethod m : methods) {
             methodNames.add(m.getElementName());
         }
         return methodNames;
@@ -67,14 +62,14 @@ public class GroovyDocumentUtil {
     }
 
     @SuppressWarnings("restriction")
-    public static  List<IMethod> getModuleMethods(GroovyCompilationUnit unit) {
-        List<IMethod> methods = new ArrayList<IMethod>();
+    public static List<IMethod> getModuleMethods(GroovyCompilationUnit unit) {
+        List<IMethod> methods = new ArrayList<>();
 
         try {
             IType[] types = unit.getAllTypes();
-            for(IType t : types){
-                for(IMethod m : t.getMethods()){
-                    if(!m.isBinary() && !m.isMainMethod() && !m.isConstructor() && !m.getElementName().equals("run")) {
+            for (IType t : types) {
+                for (IMethod m : t.getMethods()) {
+                    if (!m.isBinary() && !m.isMainMethod() && !m.isConstructor() && !m.getElementName().equals("run")) {
                         methods.add(m);
                     }
                 }
@@ -84,21 +79,19 @@ public class GroovyDocumentUtil {
             BonitaStudioLog.error(e);
         }
 
-        return methods ;
+        return methods;
 
     }
 
-
     public static VariableScope getVariableScope(GroovyCompilationUnit unit) {
-        if(unit != null){
-            CompletionNodeFinder finder = new CompletionNodeFinder(0,0,0,"",""); //$NON-NLS-1$ //$NON-NLS-2$
+        if (unit != null) {
+            CompletionNodeFinder finder = new CompletionNodeFinder(0, 0, 0, "", ""); //$NON-NLS-1$ //$NON-NLS-2$
             ContentAssistContext assistContext = finder.findContentAssistContext(unit);
 
-            org.codehaus.groovy.ast.ASTNode astNode = null ;
-            if(assistContext != null){
-                astNode = assistContext.containingCodeBlock ;
+            org.codehaus.groovy.ast.ASTNode astNode = null;
+            if (assistContext != null) {
+                astNode = assistContext.containingCodeBlock;
             }
-
 
             if (astNode instanceof BlockStatement) {
                 return ((BlockStatement) astNode).getVariableScope();
@@ -109,13 +102,13 @@ public class GroovyDocumentUtil {
                 if (method != null && method.getCode() instanceof BlockStatement) {
                     return ((BlockStatement) method.getCode()).getVariableScope();
                 }
-            } else if( astNode instanceof ImportNode){
-                int i = 0 ;
-                while (astNode instanceof ImportNode){
-                    finder = new CompletionNodeFinder(i,0,0,"",""); //$NON-NLS-1$ //$NON-NLS-2$
+            } else if (astNode instanceof ImportNode) {
+                int i = 0;
+                while (astNode instanceof ImportNode) {
+                    finder = new CompletionNodeFinder(i, 0, 0, "", ""); //$NON-NLS-1$ //$NON-NLS-2$
                     assistContext = finder.findContentAssistContext(unit);
-                    astNode = assistContext.containingCodeBlock ;
-                    i++ ;
+                    astNode = assistContext.containingCodeBlock;
+                    i++;
                 }
                 if (astNode instanceof ClassNode && ((ClassNode) astNode).isScript()) {
                     // use scope of the run method
@@ -124,7 +117,7 @@ public class GroovyDocumentUtil {
                     if (method != null && method.getCode() instanceof BlockStatement) {
                         return ((BlockStatement) method.getCode()).getVariableScope();
                     }
-                }else if(astNode instanceof BlockStatement){
+                } else if (astNode instanceof BlockStatement) {
                     return ((BlockStatement) astNode).getVariableScope();
                 }
             }
@@ -136,64 +129,51 @@ public class GroovyDocumentUtil {
     public static void addToVariableScope(GroovyCompilationUnit unit, List<FieldNode> nodes) {
         VariableScope scope = getVariableScope(unit);
 
-        while (scope != null){
-            for(FieldNode f : nodes){
+        while (scope != null) {
+            for (FieldNode f : nodes) {
                 scope.putDeclaredVariable(f);
             }
-            scope = scope.getParent() ;
+            scope = scope.getParent();
         }
     }
 
-
-
     public static void refreshUserLibrary(Repository repository) {
-        try{
+        try {
             FunctionsRepositoryFactory.getUserFunctionCatgory().removeAllFunctions();
             GroovyRepositoryStore store = repository.getRepositoryStore(GroovyRepositoryStore.class);
-            IJavaProject javaProject = RepositoryManager.getInstance().getCurrentRepository().getJavaProject() ;
-            for(IRepositoryFileStore artifact : store.getChildren()){
-                IType type = javaProject.findType(artifact.getDisplayName()) ;
-                if(type != null){
-                    for(IMethod m :type.getMethods()){
-                        if(m.getFlags() == (Flags.AccStatic | Flags.AccPublic) ){
-                            FunctionsRepositoryFactory.getUserFunctionCatgory().addFunction(new GroovyFunction(m,FunctionsRepositoryFactory.getUserFunctionCatgory()));
+            IJavaProject javaProject = repository.getJavaProject();
+            for (IRepositoryFileStore artifact : store.getChildren()) {
+                IType type = javaProject.findType(artifact.getDisplayName());
+                if (type != null) {
+                    for (IMethod m : type.getMethods()) {
+                        if (m.getFlags() == (Flags.AccStatic | Flags.AccPublic)) {
+                            FunctionsRepositoryFactory.getUserFunctionCatgory()
+                                    .addFunction(new GroovyFunction(m, FunctionsRepositoryFactory.getUserFunctionCatgory()));
                         }
                     }
                 }
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             GroovyPlugin.logError(e);
         }
     }
 
-    public synchronized static List<String> getDefaultMethodsName() {
-        if(defaultMethodsNames == null){
-            defaultMethodsNames = new ArrayList<String>();
-            for(Method m : DefaultGroovyMethods.class.getDeclaredMethods()){
+    public static synchronized List<String> getDefaultMethodsName() {
+        if (defaultMethodsNames == null) {
+            defaultMethodsNames = new ArrayList<>();
+            for (Method m : DefaultGroovyMethods.class.getDeclaredMethods()) {
                 defaultMethodsNames.add(m.getName());
             }
         }
         return defaultMethodsNames;
     }
 
-    //	public static void deleteGroovyType(DataType dataType) {
-    //		GroovyScriptRepository.getInstance().deleteGroovyType(dataType);
-    //	}
-    //
-    //	public static void createGroovyType(DataType dataType, List<String> literals) {
-    //		GroovyScriptRepository.getInstance().createGroovyTypes(dataType,literals);
-    //		ExtensionProjectUtil.refresh(new NullProgressMonitor());
-    //	}
-
-
-
     public static List<String> getVariableName(List<FieldNode> processVariables) {
-        List<String> result = new ArrayList<String>();
-        for(FieldNode f : processVariables){
+        List<String> result = new ArrayList<>();
+        for (FieldNode f : processVariables) {
             result.add(f.getName());
         }
         return result;
     }
-
 
 }
