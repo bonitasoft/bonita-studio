e4ab087656598c5a36f92edc8ac0cfe1e75d9363
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-11-21 16:13:26 +0100

    fix(migration) add missing variable dependencies (#1488)
    Closes BS-19095

---
+++ bundles/plugins/org.bonitasoft.studio.validation/src/org/bonitasoft/studio/validation/constraints/ExpressionDependenciesConstraint.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2012 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.validation.constraints;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bonitasoft.studio.common.ExpressionConstants;
+import org.bonitasoft.studio.common.emf.tools.ModelHelper;
+import org.bonitasoft.studio.expression.editor.provider.ExpressionTypeLabelProvider;
+import org.bonitasoft.studio.model.expression.Expression;
+import org.bonitasoft.studio.validation.i18n.Messages;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.IValidationContext;
+
+
+public class ExpressionDependenciesConstraint extends AbstractLiveValidationMarkerConstraint {
+
+    private static final String CONSTRAINT_ID = "org.bonitasoft.studio.validation.constraint.expressionDependencies";
+
+    private final ExpressionTypeLabelProvider expressionTypeLabelProvider;
+
+    private static Set<String> supportedTypes;
+    static {
+        supportedTypes = new HashSet<String>();
+        supportedTypes.add(ExpressionConstants.VARIABLE_TYPE);
+        supportedTypes.add(ExpressionConstants.PARAMETER_TYPE);
+    }
+
+    public ExpressionDependenciesConstraint() {
+        expressionTypeLabelProvider = new ExpressionTypeLabelProvider();
+    }
+
+    ExpressionDependenciesConstraint(final ExpressionTypeLabelProvider expressionTypeLabelProvider) {
+        this.expressionTypeLabelProvider = expressionTypeLabelProvider;
+    }
+
+    @Override
+    protected IStatus performBatchValidation(final IValidationContext context) {
+        final Expression expression = (Expression) context.getTarget();
+        if (shouldValidateExpression(expression)) {
+            return evaluateExpression(context, expression);
+        }
+        return context.createSuccessStatus();
+    }
+
+    private boolean shouldValidateExpression(final Expression expression) {
+        return expression.hasContent()
+                && !ModelHelper.isAnExpressionCopy(expression)
+                && appliesTo(expression.getType());
+    }
+
+    private boolean appliesTo(final String type) {
+        return supportedTypes.contains(type);
+    }
+
+    private IStatus evaluateExpression(final IValidationContext context, final EObject eObj) {
+        final Expression expression = (Expression) eObj;
+        final String type = expression.getType();
+        if (expression.getReferencedElements().isEmpty()) {
+            return context.createFailureStatus(Messages.bind(Messages.unresolvedDependenciesFor, expression.getName(),
+                    expressionTypeLabelProvider.getText(type)));
+        }
+        return context.createSuccessStatus();
+    }
+
+    @Override
+    protected String getConstraintId() {
+        return CONSTRAINT_ID;
+    }
+
+}
