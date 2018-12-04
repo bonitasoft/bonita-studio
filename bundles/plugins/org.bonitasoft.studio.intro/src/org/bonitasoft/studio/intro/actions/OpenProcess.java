337ba7af8c089372be6a0aee25f02fff0e7be28b
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-12-04 11:27:49 +0100

    remove stack in welcomwe page

--- bundles/plugins/org.bonitasoft.studio.intro/src/org/bonitasoft/studio/intro/actions/OpenProcess.java
+++ bundles/plugins/org.bonitasoft.studio.intro/src/org/bonitasoft/studio/intro/actions/OpenProcess.java
@@ -4,11 +4,6 @@ import java.util.Properties;
 
 import org.bonitasoft.studio.common.log.BonitaStudioLog;
 import org.eclipse.core.commands.Command;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.NotEnabledException;
-import org.eclipse.core.commands.NotHandledException;
-import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
@@ -18,31 +13,19 @@ import org.eclipse.ui.intro.config.IIntroAction;
 
 public class OpenProcess implements IIntroAction {
 
-
-	public void run(IIntroSite site, Properties param) {
-		try {
-			
-			IWorkbenchPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
-
-			IHandlerService handlerService = (IHandlerService) part.getSite().getService(IHandlerService.class);
-			ICommandService cmdService = (ICommandService)part.getSite().getService( ICommandService.class);
-			// Do not replace by static link since this command does not resolve to the same between BOS and SP
-			Command open = cmdService.getCommand("org.bonitasoft.studio.diagram.command.openDiagram");//$NON-NLS-1$
-			ExecutionEvent executionEvent = handlerService.createExecutionEvent(open, null);
-			try {
-				open.executeWithChecks(executionEvent);
-			} catch (ExecutionException e) {
-				BonitaStudioLog.error(e);
-			} catch (NotDefinedException e) {
-				BonitaStudioLog.error(e);
-			} catch (NotEnabledException e) {
-				BonitaStudioLog.error(e);
-			} catch (NotHandledException e) {
-				BonitaStudioLog.error(e);
-			}
-		} catch (Exception e) {
-			BonitaStudioLog.error(e);
-		}
-	}
+    public void run(IIntroSite site, Properties param) {
+        try {
+            IWorkbenchPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
+            IHandlerService handlerService = (IHandlerService) part.getSite().getService(IHandlerService.class);
+            ICommandService cmdService = (ICommandService) part.getSite().getService(ICommandService.class);
+            // Do not replace by static link since this command does not resolve to the same between BOS and SP
+            Command open = cmdService.getCommand("org.bonitasoft.studio.diagram.command.openDiagram");//$NON-NLS-1$
+            if (open.isEnabled()) {
+                open.executeWithChecks(handlerService.createExecutionEvent(open, null));
+            }
+        } catch (Exception e) {
+            BonitaStudioLog.error(e);
+        }
+    }
 
 }
