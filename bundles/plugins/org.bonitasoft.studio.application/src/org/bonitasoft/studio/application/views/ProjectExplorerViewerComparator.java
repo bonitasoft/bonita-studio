ec3c0d2fe3bb69fbd425081dd0dc30ac71d30185
Adrien <adrien.lachambre@bonitasoft.com>
2019-08-01 10:42:00 +0200

    feat(project explorer) sort web pages according to their type (#1814)
    [BST-420](https://bonitasoft.atlassian.net/browse/BST-420)

--- bundles/plugins/org.bonitasoft.studio.application/src/org/bonitasoft/studio/application/views/ProjectExplorerViewerComparator.java
+++ bundles/plugins/org.bonitasoft.studio.application/src/org/bonitasoft/studio/application/views/ProjectExplorerViewerComparator.java
@@ -3,20 +3,27 @@ package org.bonitasoft.studio.application.views;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
+import org.bonitasoft.studio.application.views.provider.UIDArtifactFilters;
+import org.bonitasoft.studio.common.repository.Repository;
 import org.bonitasoft.studio.common.repository.RepositoryManager;
+import org.bonitasoft.studio.common.repository.filestore.FileStoreFinder;
 import org.bonitasoft.studio.common.repository.model.IRepositoryStore;
+import org.bonitasoft.studio.designer.core.repository.WebPageFileStore;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Adapters;
-import org.eclipse.jdt.ui.JavaElementSorter;
+import org.eclipse.jdt.ui.JavaElementComparator;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
-import org.eclipse.jface.viewers.ViewerSorter;
 
 public class ProjectExplorerViewerComparator extends ViewerComparator {
 
-    private ViewerSorter defaultSorter = new JavaElementSorter();
+    private ViewerComparator defaultSorter = new JavaElementComparator();
+    private FileStoreFinder fileStoreFinder = new FileStoreFinder();
+
     private static Map<String, Integer> ORDER = new HashMap<>();
     static {
         ORDER.put("organizations", 1);
@@ -45,12 +52,37 @@ public class ProjectExplorerViewerComparator extends ViewerComparator {
 
     @Override
     public int compare(Viewer viewer, Object e1, Object e2) {
-        IProject project = RepositoryManager.getInstance().getCurrentRepository().getProject();
+        Repository currentRepository = RepositoryManager.getInstance().getCurrentRepository();
+        IProject project = currentRepository.getProject();
         IResource resource = Adapters.adapt(e1, IResource.class);
         if (resource != null && !Objects.equals(resource.getProject(), project)) {
             return defaultSorter.compare(viewer, e1, e2);
         }
+        if (comparingWebPages(e1, e2)) {
+            return compareWebPages(currentRepository, viewer, resource, Adapters.adapt(e2, IFolder.class));
+        }
         return super.compare(viewer, e1, e2);
+    }
+
+    private int compareWebPages(Repository currentRepository, Viewer viewer, IResource webPage1, IResource webPage2) {
+        Optional<WebPageFileStore> fileStore1 = fileStoreFinder.findFileStore(webPage1,
+                currentRepository).filter(WebPageFileStore.class::isInstance).map(WebPageFileStore.class::cast);
+        Optional<WebPageFileStore> fileStore2 = fileStoreFinder.findFileStore(webPage2,
+                currentRepository).filter(WebPageFileStore.class::isInstance).map(WebPageFileStore.class::cast);;
+        if (fileStore1.isPresent() && fileStore2.isPresent()) {
+            return fileStore1.get().compareTo(fileStore2.get());
+        }
+        return super.compare(viewer, webPage1, webPage2);
+    }
+
+    private boolean comparingWebPages(Object e1, Object e2) {
+        IFolder project1 = Adapters.adapt(e1, IFolder.class);
+        IFolder project2 = Adapters.adapt(e2, IFolder.class);
+        if (project1 != null && project2 != null) {
+            return UIDArtifactFilters.isUIDArtifactFrom(project1, "web_page")
+                    && UIDArtifactFilters.isUIDArtifactFrom(project2, "web_page");
+        }
+        return false;
     }
 
     @Override
