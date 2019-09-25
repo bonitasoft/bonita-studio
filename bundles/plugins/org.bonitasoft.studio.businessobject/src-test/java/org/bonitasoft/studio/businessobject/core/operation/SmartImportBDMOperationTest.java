8e0dcef2831f7ff244444e15087543b157875146
Adrien <adrien.lachambre@bonitasoft.com>
2019-09-25 18:03:32 +0200

    feat(Merge BDM) ignore duplicates (#1900)
    [BST-550](https://bonitasoft.atlassian.net/browse/BST-550)

--- bundles/plugins/org.bonitasoft.studio.businessobject/src-test/java/org/bonitasoft/studio/businessobject/core/operation/SmartImportBDMOperationTest.java
+++ bundles/plugins/org.bonitasoft.studio.businessobject/src-test/java/org/bonitasoft/studio/businessobject/core/operation/SmartImportBDMOperationTest.java
@@ -4,18 +4,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.bonitasoft.engine.bdm.model.BusinessObject;
 import org.bonitasoft.engine.bdm.model.BusinessObjectModel;
+import org.bonitasoft.studio.model.businessObject.FieldBuilder.SimpleFieldBuilder;
 import org.junit.Test;
 
 public class SmartImportBDMOperationTest {
 
     @Test
-    public void should_merge_models() {
+    public void should_merge_disjoint_models() {
         BusinessObjectModel model1 = new BusinessObjectModel();
         BusinessObjectModel model2 = new BusinessObjectModel();
         BusinessObject bo1 = new BusinessObject("com.company.Person");
         BusinessObject bo2 = new BusinessObject("org.company.Employee");
         model1.addBusinessObject(bo1);
         model2.addBusinessObject(bo2);
+        new SmartImportBDMOperation(null, null).performImport(model1, model2);
+        assertThat(model1.getBusinessObjects()).containsExactly(bo1, bo2);
+    }
+
+    @Test
+    public void should_merge_models_without_duplicating_duplicates() {
+        BusinessObjectModel model1 = new BusinessObjectModel();
+        BusinessObjectModel model2 = new BusinessObjectModel();
+        BusinessObject bo1 = new BusinessObject("com.company.Person");
+        bo1.addField(SimpleFieldBuilder.aStringField("field").build());
+        BusinessObject bo2 = new BusinessObject("org.company.Employee");
+        BusinessObject bo3 = new BusinessObject("com.company.Person");
+        bo3.addField(SimpleFieldBuilder.aStringField("field").build());
+        model1.addBusinessObject(bo1);
+        model2.addBusinessObject(bo2);
+        model2.addBusinessObject(bo3);
         new SmartImportBDMOperation(null, null).performImport(model1, model2);
         assertThat(model1.getBusinessObjects()).containsExactly(bo1, bo2);
     }
