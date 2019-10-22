d54a3b7dd0ed0ccdd823e38394db3d895fa7c2f4
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-10-22 13:46:42 +0200

    feat(menu) move build bar action to file menu (#1963)
    Closes BST-624

--- tests/org.bonitasoft.studio.tests/src/org/bonitasoft/studio/tests/bar/BarExporterTest.java
+++ tests/org.bonitasoft.studio.tests/src/org/bonitasoft/studio/tests/bar/BarExporterTest.java
@@ -87,7 +87,7 @@ public class BarExporterTest {
         bot.waitUntil(BonitaBPMConditions.noPopupActive());
 
         // Menu Server > Build...
-        bot.menu("Server").menu("Build...").click();
+        bot.menu("File").menu("Export as").menu("Business Archive (.bar)...").click();
 
         // shell 'Build'
         final SWTBotShell shell = bot.shell(Messages.buildTitle);
