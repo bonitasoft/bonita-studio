3610e155c5b6f09eb225085566c266d678418889
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-11-25 10:39:07 +0100

    feat(groovyTemplating) add groovy template project (#2024)
    * Template engine using MarkupTemplateEngine
    * Project model inject in template
    * Master template
    * AsciidocTemplate extension
    * Unit tests

---
+++ bundles/plugins/bonita-asciidoc-templating/src/main/groovy/org/bonitasoft/asciidoc/templating/model/Project.groovy
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2019 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.asciidoc.templating.model
+
+import groovy.transform.Canonical
+import groovy.transform.Immutable
+import groovy.transform.builder.Builder
+
+/**
+ * Root element of a Bonita Project. The Project model holds all injectable information
+ * that can be used when generating a project asciidoc document.
+ */
+@Canonical
+@Builder
+class Project {
+   
+   /**
+    * The name of the current project
+    */
+   String name
+   /**
+    * The version of the current project
+    */
+   String version
+   /**
+    * The bonita version used when generating the documentation
+    */
+   String bonitaVersion
+   
+}
