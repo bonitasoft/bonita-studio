8a82162721a0c06bf773a460a8d30c26da472ce5
Adrien <adrien.lachambre@bonitasoft.com>
2020-07-27 13:48:18 +0200

    feat(Intro): add new actions in community intro (#2371)
    * Multi repo actions
    * Git actions
    
    [STUDIO-3601](https://bonitasoft.atlassian.net/browse/STUDIO-3601)

--- bundles/plugins/org.bonitasoft.studio.intro/src/org/bonitasoft/studio/intro/i18n/Messages.java
+++ bundles/plugins/org.bonitasoft.studio.intro/src/org/bonitasoft/studio/intro/i18n/Messages.java
@@ -1,33 +1,29 @@
 /**
  * Copyright (C) 2010 BonitaSoft S.A.
  * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.intro.i18n;
 
 import org.eclipse.osgi.util.NLS;
 
-
 public class Messages extends NLS {
 
-	static {
-		initializeMessages("messages", Messages.class);
-	}
-	
-	public static String newProcess;
-	public static String openAProcess;
+    static {
+        initializeMessages("messages", Messages.class);
+    }
+
+    public static String newProcess;
+    public static String openAProcess;
     public static String videosAndPodcasts;
     public static String blog;
     public static String communityContributions;
@@ -48,5 +44,8 @@ public class Messages extends NLS {
     public static String importLabel;
     public static String reportAnIssue;
     public static String importUserApplication;
+    public static String newProject;
+    public static String clone;
+    public static String switchProject;
 
 }
