b852ae067ca236cf3f3918b2c35346fcb47cc349
Romain Bioteau <romain.bioteau@bonitasoft.com>
2020-10-14 11:39:40 +0200

    fix(bar builder): build bar without config (#2448)
    When 'None' is selected for the configuration, an empty configuration is
    created at build time and put into the bar.
    Previously, we used a null argument for the conf Id when none was
    selected. Now we use a dedicated none ID so it doesn't collude with the
    logic that handle a real null id.
    
    [STUDIO-3642](https://bonitasoft.atlassian.net/browse/STUDIO-3642)
    
    Co-authored-by: adrien <adrien.lachambre@bonitasoft.com>

--- bundles/plugins/org.bonitasoft.studio.configuration/src/org/bonitasoft/studio/configuration/preferences/ConfigurationPreferenceConstants.java
+++ bundles/plugins/org.bonitasoft.studio.configuration/src/org/bonitasoft/studio/configuration/preferences/ConfigurationPreferenceConstants.java
@@ -5,24 +5,22 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.studio.configuration.preferences;
 
 /**
  * @author Romain Bioteau
- *
  */
 public interface ConfigurationPreferenceConstants {
 
-	public static final String DEFAULT_CONFIGURATION = "defaultConfiguration" ;
-	public static final String LOCAL_CONFIGURAITON = "Local";
-	
+    public static final String DEFAULT_CONFIGURATION = "defaultConfiguration";
+    public static final String LOCAL_CONFIGURAITON = "Local";
+    public static final String NONE_CONFIGURAITON = "None";
+
 }
