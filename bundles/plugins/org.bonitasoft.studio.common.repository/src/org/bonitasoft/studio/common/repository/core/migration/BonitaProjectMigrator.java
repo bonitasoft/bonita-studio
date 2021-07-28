dbc2e6de0ce26e866d2a280715fae4df74495df0
Romain Bioteau <romain.bioteau@bonitasoft.com>
2021-07-28 10:05:30 +0200

    fix(maven-plugin): update bonita project plugin version (#2877)
    Closes STUDIO-4079

--- bundles/plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/core/migration/BonitaProjectMigrator.java
+++ bundles/plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/core/migration/BonitaProjectMigrator.java
@@ -20,6 +20,7 @@ import org.bonitasoft.studio.common.Strings;
 import org.bonitasoft.studio.common.repository.core.migration.report.MigrationReport;
 import org.bonitasoft.studio.common.repository.core.migration.step.CreatePomMigrationStep;
 import org.bonitasoft.studio.common.repository.core.migration.step.RemoveLegacyFolderStep;
+import org.bonitasoft.studio.common.repository.core.migration.step.UpdateBonitaProjectMavenPluginVersionInPomStep;
 import org.bonitasoft.studio.common.repository.core.migration.step.UpdateBonitaRuntimeVersionInPomStep;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
@@ -30,7 +31,8 @@ public class BonitaProjectMigrator {
     private static final List<MigrationStep> STEPS = List.of(
             new CreatePomMigrationStep(),
             new RemoveLegacyFolderStep(),
-            new UpdateBonitaRuntimeVersionInPomStep());
+            new UpdateBonitaRuntimeVersionInPomStep(),
+            new UpdateBonitaProjectMavenPluginVersionInPomStep());
 
     private IProject project;
 
