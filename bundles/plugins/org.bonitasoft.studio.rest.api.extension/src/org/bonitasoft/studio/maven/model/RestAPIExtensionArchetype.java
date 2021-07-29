3fc542bb44d5b71358495ae78bc72a354b374d43
Romain Bioteau <romain.bioteau@bonitasoft.com>
2021-07-29 13:26:35 +0200

    feat(connector): update version

--- bundles/plugins/org.bonitasoft.studio.rest.api.extension/src/org/bonitasoft/studio/maven/model/RestAPIExtensionArchetype.java
+++ bundles/plugins/org.bonitasoft.studio.rest.api.extension/src/org/bonitasoft/studio/maven/model/RestAPIExtensionArchetype.java
@@ -16,7 +16,7 @@ public class RestAPIExtensionArchetype {
 
     public static final String ARTIFACT_ID = "bonita-rest-api-extension-archetype";
     public static final String GROUP_ID = "org.bonitasoft.archetypes";
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "1.4.2";
     public static final Archetype INSTANCE = new Archetype();
 
     static {
