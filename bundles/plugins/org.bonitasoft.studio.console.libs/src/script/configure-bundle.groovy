b5976da64d4f63fe730fdc7fb6488b3bcd13309e
Romain Bioteau <romain.bioteau@bonitasoft.com>
2021-10-28 17:34:17 +0200

    disable logging.properties customization

--- bundles/plugins/org.bonitasoft.studio.console.libs/src/script/configure-bundle.groovy
+++ bundles/plugins/org.bonitasoft.studio.console.libs/src/script/configure-bundle.groovy
@@ -4,11 +4,11 @@ def tomcatBundleDir =  new File(properties['tomcat.bundle.dir'])
 
 def securityConfigFile = new File(tomcatBundleDir, 'setup/platform_conf/initial/tenant_template_portal/security-config.properties')
 def contextFile = new File(tomcatBundleDir, 'server/conf/context.xml')
-def loggingPropertiesFile = new File(tomcatBundleDir, 'server/conf/logging.properties')
+//def loggingPropertiesFile = new File(tomcatBundleDir, 'server/conf/logging.properties')
 
 assert securityConfigFile.exists() : 'Console Securtiy Configruation file not found'
 assert contextFile.exists() : 'Tomcat bundle context.xml file not found'
-assert loggingPropertiesFile.exists() : 'Tomcat bundle logging.properties file not found'
+//assert loggingPropertiesFile.exists() : 'Tomcat bundle logging.properties file not found'
 
 def securityConfig = new Properties() 
 securityConfigFile.withInputStream {
@@ -24,11 +24,11 @@ def contextNode = new XmlSlurper().parse(contextFile)
 contextNode.@sessionCookiePath = '/'
 contextFile.write(XmlUtil.serialize(contextNode))
 
-println "Set hibernate slow query log level to WARN"
-def loggingProperties = new Properties()
-loggingPropertiesFile.withInputStream {
-    loggingProperties.load(it)
-}
-assert loggingProperties.containsKey('org.hibernate.SQL_SLOW.level') : "org.hibernate.SQL_SLOW.level property not found in $loggingProperties"
-loggingProperties."org.hibernate.SQL_SLOW.level" = 'WARN'
-loggingProperties.store(loggingPropertiesFile.newWriter(), null)
+//println "Set hibernate slow query log level to WARN"
+//def loggingProperties = new Properties()
+//loggingPropertiesFile.withInputStream {
+//  loggingProperties.load(it)
+//}
+//assert loggingProperties.containsKey('org.hibernate.SQL_SLOW.level') : "org.hibernate.SQL_SLOW.level property not found in $loggingProperties"
+//loggingProperties."org.hibernate.SQL_SLOW.level" = 'WARN'
+//loggingProperties.store(loggingPropertiesFile.newWriter(), null)
