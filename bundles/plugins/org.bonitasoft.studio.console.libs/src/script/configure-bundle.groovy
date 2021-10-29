2eef106c6a28b030e6b328453eb8ba00fa2680e4
Romain Bioteau <romain.bioteau@bonitasoft.com>
2021-10-29 11:23:28 +0200

    fix(logger): configure log4j2 logger
    Set slow hibernate query log level to WARN

--- bundles/plugins/org.bonitasoft.studio.console.libs/src/script/configure-bundle.groovy
+++ bundles/plugins/org.bonitasoft.studio.console.libs/src/script/configure-bundle.groovy
@@ -4,11 +4,11 @@ def tomcatBundleDir =  new File(properties['tomcat.bundle.dir'])
 
 def securityConfigFile = new File(tomcatBundleDir, 'setup/platform_conf/initial/tenant_template_portal/security-config.properties')
 def contextFile = new File(tomcatBundleDir, 'server/conf/context.xml')
-//def loggingPropertiesFile = new File(tomcatBundleDir, 'server/conf/logging.properties')
+def loggerConfigFile = new File(tomcatBundleDir, 'server/conf/log4j2-loggers.xml')
 
 assert securityConfigFile.exists() : 'Console Securtiy Configruation file not found'
 assert contextFile.exists() : 'Tomcat bundle context.xml file not found'
-//assert loggingPropertiesFile.exists() : 'Tomcat bundle logging.properties file not found'
+assert loggerConfigFile.exists() : 'Tomcat bundle log4j2-loggers.xml file not found'
 
 def securityConfig = new Properties() 
 securityConfigFile.withInputStream {
@@ -24,11 +24,9 @@ def contextNode = new XmlSlurper().parse(contextFile)
 contextNode.@sessionCookiePath = '/'
 contextFile.write(XmlUtil.serialize(contextNode))
 
-//println "Set hibernate slow query log level to WARN"
-//def loggingProperties = new Properties()
-//loggingPropertiesFile.withInputStream {
-//  loggingProperties.load(it)
-//}
-//assert loggingProperties.containsKey('org.hibernate.SQL_SLOW.level') : "org.hibernate.SQL_SLOW.level property not found in $loggingProperties"
-//loggingProperties."org.hibernate.SQL_SLOW.level" = 'WARN'
-//loggingProperties.store(loggingPropertiesFile.newWriter(), null)
+println "Set hibernate slow query log level to WARN"
+def configurationNode = new XmlSlurper().parse(loggerConfigFile)
+def slowQueryLogger = cconfigurationNode.'Loggers'.'Logger'.find { it.'@name' == 'org.hibernate.SQL_SLOW.level'}
+assert slowQueryLogger : "org.hibernate.SQL_SLOW.level logger not found in $loggerConfigFile"
+slowQueryLogger.'@level' = 'WARN'
+loggerConfigFile.write(XmlUtil.serialize(configurationNode))
