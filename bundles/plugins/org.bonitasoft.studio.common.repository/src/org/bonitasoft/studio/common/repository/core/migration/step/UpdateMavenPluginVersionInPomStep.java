88447be28bc7bd0886160060ecb4608a94a8ee9e
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-06-01 10:00:45 +0200

    feat(project): update maven plugins versions (#3333)
    * Configure additional common plugins versions
    * Update versions of configured plugins
    
    Closes DEV-347

---
+++ bundles/plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/core/migration/step/UpdateMavenPluginVersionInPomStep.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2023 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.common.repository.core.migration.step;
+
+import java.nio.file.Path;
+import java.util.Properties;
+
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.bonitasoft.studio.common.ProductVersion;
+import org.bonitasoft.studio.common.repository.core.maven.model.ProjectDefaultConfiguration;
+import org.bonitasoft.studio.common.repository.core.migration.MigrationStep;
+import org.bonitasoft.studio.common.repository.core.migration.report.MigrationReport;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.osgi.framework.Version;
+
+public class UpdateMavenPluginVersionInPomStep implements MigrationStep {
+
+    @Override
+    public MigrationReport run(Path project, IProgressMonitor monitor) throws CoreException {
+        var model = loadParentMavenModel(project);
+        Properties properties = model.getProperties();
+        var report = new MigrationReport();
+        var defaultConfiguration = new ProjectDefaultConfiguration(ProductVersion.BONITA_RUNTIME_VERSION);
+        for (var plugin : defaultConfiguration.getPlugins()) {
+            var versionProperty = plugin.getVersionPropertyName();
+            var currentVersion = properties.getProperty(versionProperty);
+            if (properties.containsKey(versionProperty) &&
+                    new DefaultArtifactVersion(plugin.getVersion())
+                            .compareTo(new DefaultArtifactVersion(currentVersion)) > 0) {
+                properties.setProperty(versionProperty, plugin.getVersion());
+                report.updated(String.format("`%s` has been updated from `%s` to `%s`", versionProperty, currentVersion,
+                        plugin.getVersion()));
+            } else if (!properties.containsKey(versionProperty)) {
+                properties.setProperty(versionProperty, plugin.getVersion());
+                var pluginManagement = model.getBuild().getPluginManagement();
+                pluginManagement.addPlugin(plugin.toManagedPlugin());
+                report.added(String.format("`%s:%s` has been added to the managed plugins list.", plugin.getGroupId(),
+                        plugin.getArtifactId()));
+            }
+        }
+        if (report.hasUpdates() || report.hasAdditions()) {
+            saveMavenModel(model, project);
+        }
+        return report;
+    }
+
+    @Override
+    public boolean appliesTo(String sourceVersion) {
+        return Version.parseVersion(sourceVersion).compareTo(new Version("8.0.0")) >= 0;
+    }
+
+}
