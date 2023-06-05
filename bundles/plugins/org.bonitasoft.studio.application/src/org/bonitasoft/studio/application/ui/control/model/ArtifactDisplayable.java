2283e797a3675a37ee8457e62d96137bfc1f8b53
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-06-05 16:44:01 +0200

    fix(ui): Deploy wizard label provider adapters (#3334)
    After the adapter refactoring some icons where missing in the Deploy
    wizard in addition to some labels.

--- bundles/plugins/org.bonitasoft.studio.application/src/org/bonitasoft/studio/application/ui/control/model/ArtifactDisplayable.java
+++ bundles/plugins/org.bonitasoft.studio.application/src/org/bonitasoft/studio/application/ui/control/model/ArtifactDisplayable.java
@@ -2,6 +2,7 @@ package org.bonitasoft.studio.application.ui.control.model;
 
 import org.bonitasoft.studio.common.ui.IDisplayable;
 import org.bonitasoft.studio.model.edit.ProcessEditPlugin;
+import org.eclipse.core.runtime.Adapters;
 import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
 import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.swt.graphics.Image;
@@ -18,6 +19,12 @@ public class ArtifactDisplayable implements IDisplayable {
     public String getDisplayName() {
         if (artifact instanceof ProcessVersion) {
             return ((ProcessVersion) artifact).getVersion();
+        } else if (artifact instanceof FileStoreArtifact) {
+            var displayable = Adapters.adapt(((FileStoreArtifact) artifact).getFileStore(), IDisplayable.class);
+            if (displayable != null) {
+                return displayable.getDisplayName();
+            }
+            return artifact.getName();
         } else {
             return artifact.getName();
         }
@@ -33,6 +40,12 @@ public class ArtifactDisplayable implements IDisplayable {
             return ExtendedImageRegistry.INSTANCE
                     .getImage(ProcessEditPlugin.INSTANCE.getPluginResourceLocator().getImage("full/obj16/Pool"));
         }
+        if (artifact instanceof FileStoreArtifact) {
+            var displayable = Adapters.adapt(((FileStoreArtifact) artifact).getFileStore(), IDisplayable.class);
+            if (displayable != null) {
+                return displayable.getIcon();
+            }
+        }
         return null;
     }
 
@@ -46,12 +59,21 @@ public class ArtifactDisplayable implements IDisplayable {
         if (artifact instanceof ProcessVersion) {
             styledString.append("  " + ((ProcessVersion) artifact).getFileStore().getResource().getName(),
                     StyledString.DECORATIONS_STYLER);
+            return styledString;
         } else if (artifact instanceof ProcessArtifact) {
             if (((ProcessArtifact) artifact).hasSingleVersion()) {
                 ProcessVersion version = (ProcessVersion) ((ProcessArtifact) artifact).getLatestVersion();
                 styledString.append("  " + version.getFileStore().getResource().getName(),
                         StyledString.DECORATIONS_STYLER);
             }
+            return styledString;
+        }
+        if (artifact instanceof FileStoreArtifact) {
+            var displayable = Adapters.adapt(((FileStoreArtifact) artifact).getFileStore(), IDisplayable.class);
+            if (displayable != null) {
+                return displayable.getStyledString();
+            }
+            return styledString;
         }
         return styledString;
     }
