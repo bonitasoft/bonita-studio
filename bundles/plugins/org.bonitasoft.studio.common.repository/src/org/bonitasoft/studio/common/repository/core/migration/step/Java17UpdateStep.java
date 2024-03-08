06a36ee2d739195bcb4e5fddaad60dc5e8298a6e
Romain Bioteau <romain.bioteau@bonitasoft.com>
2024-03-08 14:06:50 +0100

    fix(migration): add Java 17 update step (#3480)

---
+++ bundles/plugins/org.bonitasoft.studio.common.repository/src/org/bonitasoft/studio/common/repository/core/migration/step/Java17UpdateStep.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2021 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.studio.common.repository.core.migration.step;
+
+import java.nio.file.Path;
+
+import org.bonitasoft.studio.common.repository.core.migration.MigrationStep;
+import org.bonitasoft.studio.common.repository.core.migration.report.MigrationReport;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.osgi.framework.Version;
+
+public class Java17UpdateStep implements MigrationStep {
+
+	@Override
+	public MigrationReport run(Path project, IProgressMonitor monitor) throws CoreException {
+		var report = new MigrationReport();
+		report.updated("Required Java version updated to `17`. Make sure that your third party dependencies are compliant with Java 17.");
+		return report;
+	}
+
+	@Override
+	public boolean appliesTo(String sourceVersion) {
+		return Version.parseVersion(sourceVersion).compareTo(new Version("10.0.0")) < 0;
+	}
+
+}
